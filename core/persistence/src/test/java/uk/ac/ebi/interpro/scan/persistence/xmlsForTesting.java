dc5f0de22766fbfd4e28625131740cd14c57d971
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-07-20 14:44:32 +0000

    Test XML files now in Spring config.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@44 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/test/java/uk/ac/ebi/interpro/scan/persistence/xmlsForTesting.java
@@ -0,0 +1,24 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+import java.util.List;
+
+/**
+ * Developed using IntelliJ IDEA.
+ * User: pjones
+ * Date: 20-Jul-2009
+ * Time: 15:29:15
+ *
+ * @author Phil Jones, EMBL-EBI
+ */
+public class xmlsForTesting {
+
+    private List<String> xmls;
+
+    public xmlsForTesting(List<String> xmls){
+        this.xmls = xmls;
+    }
+
+    public List<String> getXmls() {
+        return xmls;
+    }
+}
