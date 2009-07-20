6ac8ac6f9bff4e3947b932c39610ec46bcf5bd97
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-07-20 14:56:57 +0000

    git-svn-id: https://interproscan.googlecode.com/svn/trunk@47 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/FilteredFingerPrintsMatchDAO.java
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/FilteredFingerPrintsMatchDAO.java
@@ -2,7 +2,11 @@ package uk.ac.ebi.interpro.scan.persistence;
 import uk.ac.ebi.interpro.scan.model.Match;
 import uk.ac.ebi.interpro.scan.model.Protein;
 import uk.ac.ebi.interpro.scan.model.FilteredFingerPrintsMatch;
+import uk.ac.ebi.interpro.scan.model.Model;
+import uk.ac.ebi.interpro.scan.model.transactiontracking.RawTransactionSlice;
 import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,8 +15,14 @@ import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
  * Time: 15:13:18
  * To change this template use File | Settings | File Templates.
  */
-public interface FilteredFingerPrintsMatchDAO  extends GenericDAO<Protein, Long> {
+public interface FilteredFingerPrintsMatchDAO  extends GenericDAO<Match, Long> {
 
 
+           /**
+     * Retrieves a Protein object by primary key and also retrieves any associated cross references.
+     * @param m being the query match hitting proteins.
+     * @return The Protein List, with filtered finger print matches hitting the proteins
+     */
+    public List<Protein> getProteinsWithMatch (Match m);
 
 }
