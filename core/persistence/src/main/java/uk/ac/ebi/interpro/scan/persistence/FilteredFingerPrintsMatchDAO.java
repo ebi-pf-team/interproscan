95275a586b364a4b1df47ee9ae41329da7160b90
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-07-20 14:27:05 +0000

    git-svn-id: https://interproscan.googlecode.com/svn/trunk@42 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/FilteredFingerPrintsMatchDAO.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.interpro.scan.persistence;
+import uk.ac.ebi.interpro.scan.model.Match;
+import uk.ac.ebi.interpro.scan.model.Protein;
+import uk.ac.ebi.interpro.scan.model.FilteredFingerPrintsMatch;
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thimma
+ * Date: 20-Jul-2009
+ * Time: 15:13:18
+ * To change this template use File | Settings | File Templates.
+ */
+public interface FilteredFingerPrintsMatchDAO  extends GenericDAO<Protein, Long> {
+
+
+
+}
