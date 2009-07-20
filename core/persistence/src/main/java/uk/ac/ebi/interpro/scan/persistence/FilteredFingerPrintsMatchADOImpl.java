0eeb384cf7083cc3ed3c4203c86775fa76969141
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-07-20 14:57:20 +0000

    git-svn-id: https://interproscan.googlecode.com/svn/trunk@48 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/FilteredFingerPrintsMatchADOImpl.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thimma
+ * Date: 20-Jul-2009
+ * Time: 15:54:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class FilteredFingerPrintsMatchADOImpl {
+}
