a056f116e1f54602f1608c2026848fa373bc8e08
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-07-21 12:51:32 +0000

    git-svn-id: https://interproscan.googlecode.com/svn/trunk@59 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/BlastProdomLocationDAO.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thimma
+ * Date: 21-Jul-2009
+ * Time: 13:43:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class BlastProdomLocationDAO {
+}
