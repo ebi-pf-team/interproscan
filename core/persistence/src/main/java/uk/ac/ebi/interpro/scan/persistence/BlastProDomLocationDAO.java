d91ff37326ee8f99d1628b43e1b77bb50d92133a
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-07-21 13:43:48 +0000

    BlastProDomLocation DAO
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@62 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/BlastProDomLocationDAO.java
@@ -0,0 +1,31 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+import uk.ac.ebi.interpro.scan.model.BlastProDomLocation;
+import uk.ac.ebi.interpro.scan.model.Protein;
+import uk.ac.ebi.interpro.scan.model.transactiontracking.TransactionSlice;
+
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thimma
+ * Date: 21-Jul-2009
+ * Time: 13:43:42
+ * To change this template use File | Settings | File Templates.
+ */
+public interface BlastProDomLocationDAO extends GenericDAO<BlastProDomLocation, Long> {
+
+    /**
+     * Retrieves a Blast ProDom Location by score.
+     * @param score being the score value of one or more ProDom Location.
+     * @return The BlastProDomLocation, A list of BPL objects with a particular score.
+     * 
+     */
+     public List<BlastProDomLocation> getBlastProDomHitLocationByScore(Double score);
+
+
+
+
+}
