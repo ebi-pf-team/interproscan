f32bfb14f4758d7e2fe3de455cbac3804b2bd953
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-07-21 13:44:34 +0000

    BlastProDomLocation DAO Implementing class
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@63 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/BlastProDomLocationDAOImpl.java
@@ -0,0 +1,15 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+import uk.ac.ebi.interpro.scan.model.Protein;
+import uk.ac.ebi.interpro.scan.model.BlastProDomLocation;
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAOImpl;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thimma
+ * Date: 21-Jul-2009
+ * Time: 14:25:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class BlastProDomLocationDAOImpl extends GenericDAOImpl<BlastProDomLocation, Long> implements BlastProDomLocationDAO{
+}
