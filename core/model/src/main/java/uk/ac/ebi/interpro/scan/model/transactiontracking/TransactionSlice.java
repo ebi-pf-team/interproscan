d6cd435e739c0d23631717c2360865ee6c7c6f46
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-07-24 12:04:40 +0000

    Modified the TransactionSlice hierarchy - removed the TransactionSlice interface.  Replaced with the corresponding abstract classes, which have been renamed to the same as the intefaces they have replaced.  Cascaded these changes through the entire object map.
    All code compiles.  All tests complete successfully (except tests that have always failed anyway, annotated with @Ignore and explanatory comments).  Test in dependent modules work too.
    
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@106 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/transactiontracking/TransactionSlice.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/transactiontracking/TransactionSlice.java
@@ -1,55 +1,99 @@
 package uk.ac.ebi.interpro.scan.model.transactiontracking;
 
+import static uk.ac.ebi.interpro.scan.model.transactiontracking.TransactionState.*;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
- * This interface provides the data structure to allow management of
+ * provides the data structure to allow management of
  * match calculation transactions.  Each slice applies to a single
  * Model and indicates a range of Protein objects that are included in
  * the transaction for that Model.
  *
- * At any one time the Transaction 
- *
  * User: pjones
  * Date: 09-Jul-2009
- * Time: 15:08:58
+ * Time: 15:36:40
  *
  * @author Phil Jones, EMBL-EBI
  */
-public interface TransactionSlice {
+@MappedSuperclass
+public abstract class TransactionSlice {
+
+    @Column (name="status", nullable = false, updatable = true)
+    private TransactionState state;
+
+    @Column (nullable = false, updatable = false)
+    private Long bottom;
+
+    @Column (nullable = false, updatable = false)
+    private Long top;
+
+    public TransactionSlice(Long bottom, Long top){
+        if (top < bottom){
+            throw new IllegalArgumentException("Invalid Transaction Slice Initialisation - it makes no sense for the bottom of the slice to be a higher value than the top.");
+        }
+        this.bottom = bottom;
+        this.top = top;
+    }
+
+    protected TransactionSlice(){
+        
+    }
 
     /**
      * Returns the current status of this TransactionSlice.
      * @return the current status of this TransactionSlice.
      */
-    public TransactionState getState();
+    public TransactionState getState() {
+        return state;
+    }
 
     /**
      * Returns the bottom of the TransactionSlice.  This corresponds to the
      * lowest Protein primary key in this slice.
      * @return the bottom of the TransactionSlice.
      */
-    public Long getBottom();
+    public Long getBottom() {
+        return bottom;
+    }
 
     /**
      * Returns the top of the TransactionSlice.  This corresponds to the
      * lowest Protein primary key in this slice.
      * @return the bottom of the TransactionSlice.
      */
-    public Long getTop();
+    public Long getTop() {
+        return top;
+    }
 
     /**
      * Method called when the Transaction has been started.
      */
-    public void started();
+    public void started() {
+        if (state != NOT_STARTED){
+            throw new IllegalStateException ("The started method has been called on a transaction that has already started, existing state: "+ state);
+        }
+        this.state = RUNNING;
+    }
 
     /**
      * Method called when the Transaction has finished successfully
      */
-    public void finishedSuccessfully();
+    public void finishedSuccessfully() {
+        if (state != RUNNING){
+            throw new IllegalStateException("The finishedSuccessfully method has been called on a transaction that was not running, existing state: "+ state);
+        }
+        this.state = FINISHED_SUCCESSFULLY;
+    }
 
     /**
      * Method called with the Transaction has failed.
      */
-    public void failed();
-
-
+    public void failed() {
+        if (state != RUNNING){
+            throw new IllegalStateException("The failed method has been called on a transaction that was not running, existing state: "+ state);
+        }
+        this.state = FAILED;
+    }
 }
