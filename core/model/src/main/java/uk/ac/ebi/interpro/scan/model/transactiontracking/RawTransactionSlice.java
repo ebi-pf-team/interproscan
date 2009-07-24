b85f3900998f6c54c9d1adc87612e0e0451f6d15
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-07-24 12:04:40 +0000

    Modified the TransactionSlice hierarchy - removed the TransactionSlice interface.  Replaced with the corresponding abstract classes, which have been renamed to the same as the intefaces they have replaced.  Cascaded these changes through the entire object map.
    All code compiles.  All tests complete successfully (except tests that have always failed anyway, annotated with @Ignore and explanatory comments).  Test in dependent modules work too.
    
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@106 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/transactiontracking/RawTransactionSlice.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/transactiontracking/RawTransactionSlice.java
@@ -2,19 +2,44 @@ package uk.ac.ebi.interpro.scan.model.transactiontracking;
 
 import uk.ac.ebi.interpro.scan.model.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
 /**
  * Developed using IntelliJ IDEA.
  * User: pjones
  * Date: 09-Jul-2009
- * Time: 17:28:02
+ * Time: 15:36:16
  *
  * @author Phil Jones, EMBL-EBI
  */
-public interface RawTransactionSlice extends TransactionSlice{
+@Entity
+public class RawTransactionSlice extends TransactionSlice{
+
+    /**
+     * public constructor that takes the lowest and highest primary key values for the proteins
+     * in the slice as arguments.
+     * @param bottom the lowest primary key values for the proteins
+     * in the slice
+     * @param top the highest primary key values for the proteins
+     * in the slice
+     */
+    public RawTransactionSlice(long bottom, long top){
+        super(bottom, top);
+    }
+
+    protected RawTransactionSlice(){
+        super ();
+    }
+
+    @ManyToOne (optional = false)
+    private Model model;
 
     /**
      * Returns the Model to which this Slice applies.
      * @return the Model to which this Slice applies.
      */
-    public Model getModel();
+    public Model getModel(){
+        return model;
+    }
 }
