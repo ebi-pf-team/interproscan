a4311e1213a6d91857330f9e3cbaba3d20922384
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-10-22 15:44:27 +0000

    rawmatch key class for primary key in model changed for JPA annotation
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@129 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/HmmRawMatch.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/HmmRawMatch.java
@@ -1,5 +1,10 @@
 package uk.ac.ebi.interpro.scan.model.raw;
 
+import javax.persistence.Column;
+import javax.persistence.InheritanceType;
+import javax.persistence.Inheritance;
+import javax.persistence.Entity;
+
 /**
  * TODO: Add class description
  *
@@ -7,16 +12,27 @@ package uk.ac.ebi.interpro.scan.model.raw;
  * @version $Id$
  * @since 1.0
  */
-public final class HmmRawMatch extends RawMatch {
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class HmmRawMatch extends RawMatch {
 
     // TODO: evalue and score can be calculated -- do we need to store? Needs testing
+    @Column(name="EVALUE",nullable = false, updatable = false)
     private double evalue;
+
+    @Column(name="SCORE",nullable = false, updatable = false)
     private double score;
+    @Column(name="HMM_START")
     private long hmmStart;
+    @Column(name="HMM_END")
     private long hmmEnd;
-    private String hmmBounds;    
+    @Column(name="HMM_BOUNDS")
+    private String hmmBounds;
+    @Column(name="LOCATION_EVALUE")
     private double locationEvalue;
+    @Column(name="SEQ_SCORE")
     private double locationScore;
+    @Column(name="ALIGNMENT")
     private String alignment;   // CIGAR format
 
     public HmmRawMatch() { }
