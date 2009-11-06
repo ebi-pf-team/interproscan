906b825f596871982ca9065a97455b3b1ea6a090
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-11-06 17:18:46 +0000

    RawMatch, HmmRawMatch, Pfam, and BlastProdomMatch classes are modified and cleaned up
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@155 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/PfamDAO.java
@@ -0,0 +1,21 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
+import uk.ac.ebi.interpro.scan.model.raw.Pfam;
+//import uk.ac.ebi.interpro.scan.model.Protein;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thimma
+ * Date: 06-Nov-2009
+ * Time: 14:05:56
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PfamDAO extends GenericDAO<Pfam, Long> {
+
+        public Pfam getPfamMatchesByModel (String methodAc);
+
+        //public Pfam
+
+
+}
