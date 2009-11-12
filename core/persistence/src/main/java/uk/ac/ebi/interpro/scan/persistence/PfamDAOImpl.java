32169266f65d3a5009eacd6059979a59b319de7b
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-11-12 10:26:03 +0000

    RawMatch, HmmRawMatch, Pfam, and BlastProdomMatch classes are modified and cleaned up
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@173 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/PfamDAOImpl.java
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/PfamDAOImpl.java
@@ -1,7 +1,7 @@
 package uk.ac.ebi.interpro.scan.persistence;
 
 import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAOImpl;
-import uk.ac.ebi.interpro.scan.model.raw.Pfam;
+import uk.ac.ebi.interpro.scan.model.raw.PfamRawMatch;
 
 
 import javax.persistence.Query;
@@ -14,27 +14,27 @@ import javax.persistence.Query;
  * Time: 14:27:39
  * To change this template use File | Settings | File Templates.
  */
-public class PfamDAOImpl extends GenericDAOImpl<Pfam, Long> implements PfamDAO {
+public class PfamDAOImpl extends GenericDAOImpl<PfamRawMatch, Long> implements PfamDAO {
 
      /**
      * Calls the GenericDAOImpl constructor passing in Protein.class as
      * argument, so that this DAO is set up to handle the correct class of model.
      */
     public PfamDAOImpl(){
-        super(Pfam.class);
+        super(PfamRawMatch.class);
     }
 
-    public Pfam getPfamMatchesById(Long id) {
-        Query query = entityManager.createQuery("select p from Pfam p  where p.id = :id");
+    public PfamRawMatch getPfamMatchesById(Long id) {
+        Query query = entityManager.createQuery("select p from PfamRawMatch p  where p.id = :id");
         query.setParameter("id", id);
-        return (Pfam) query.getSingleResult();
+        return (PfamRawMatch) query.getSingleResult();
     }
 
-       public Pfam getPfamMatchesByModel(String methodAc) {
+       public PfamRawMatch getPfamMatchesByModel(String methodAc) {
           // Pfam p = null;
-            Query query = entityManager.createQuery("select p from Pfam p  where p.model = :methodAc");
+            Query query = entityManager.createQuery("select p from PfamRawMatch p  where p.model = :methodAc");
            query.setParameter("methodAc", methodAc);
-           return (Pfam) query.getSingleResult();
+           return (PfamRawMatch) query.getSingleResult();
            //return p;
        }
 }
