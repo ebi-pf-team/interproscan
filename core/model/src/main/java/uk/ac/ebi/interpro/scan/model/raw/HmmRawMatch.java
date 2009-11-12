ce3e39710008e7a856c3622db078deead333103c
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-11-12 09:59:15 +0000

    RawMatch, HmmRawMatch, Pfam, and BlastProdomMatch classes are modified and cleaned up
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@170 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/HmmRawMatch.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/HmmRawMatch.java
@@ -1,8 +1,8 @@
 package uk.ac.ebi.interpro.scan.model.raw;
 
-import javax.persistence.Column;
-import javax.persistence.InheritanceType;
-import javax.persistence.Inheritance;
+//import javax.persistence.Column;
+//import javax.persistence.InheritanceType;
+//import javax.persistence.Inheritance;
 import javax.persistence.Entity;
 
 /**
@@ -16,24 +16,87 @@ import javax.persistence.Entity;
 //@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class HmmRawMatch extends RawMatch {
 
-    // TODO: evalue and score can be calculated -- do we need to store? Needs testing
+   // TODO: evalue and score can be calculated -- do we need to store? Needs testing
     //@Column(name="EVALUE",nullable = false, updatable = false)
-    protected double evalue;
+    protected double fullSequenceEvalue;
 
-    //@Column(name="SCORE",nullable = false, updatable = false)
-    protected double score;
+   //@Column(name="SCORE",nullable = false, updatable = false)
+    protected double sequenceScore;
+
+    private Double fullSequenceBias;
+
+
+    /**
+    * Populated from the 'this domain' c-Evalue column
+    */
+   private double domainCEValue;
+
+   /**
+    * Populated from the 'this domain' i-Evalue column
+    */
+   private double domainIEValue;
+
+   /**
+    * Populated from the 'this domain' score column
+    * Called 'score' for consistency with the res t of Onion code.
+    */
+   private double score;
+
+   /**
+    * Populated from the 'this domain' bias column
+    */
+   private Double domainBias;
+
+
     //@Column(name="HMM_START")
     protected long hmmStart;
     //@Column(name="HMM_END")
     protected long hmmEnd;
     //@Column(name="HMM_BOUNDS")
     protected String hmmBounds;
+    /**
+        * Populated from the 'ali coord' from column
+        */
+       private int alignmentFrom;
+
+
+    /**
+        * Populated from the 'ali coord' to column
+        */
+       private int alignmentTo;
+
+       /**
+        * Populated from the 'env coord' from column
+        */
+       private int envelopeFrom;
+
+       /**
+        * Populated from the 'env coord' to column
+        */
+       private int envelopeTo;
+
+       /**
+        * Populated from the acc column
+        */
+       private double acc;
+
+       /**
+        * Populated from the description of target column
+        */
+       private String descriptionOfTarget;
+
+       private Integer releaseMajor;
+
+       private Integer releaseMinor;
+
+
+
     //@Column(name="LOCATION_EVALUE")
     protected double locationEvalue;
     //@Column(name="SEQ_SCORE")
    protected double locationScore;
     //@Column(name="ALIGNMENT")
-   protected String alignment;   // CIGAR format
+   protected String cigarAlignment;   // CIGAR format
 
     public HmmRawMatch() { }
     public HmmRawMatch(String seqIdentifier, String model,String dbname,String dbversion, String generator, long start, long end) {
@@ -43,29 +106,69 @@ public abstract class HmmRawMatch extends RawMatch {
         super(model);
     }
 
-    public String getAlignment() {
-        return alignment;
+    public String getCigarAlignment() {
+        return cigarAlignment;
     }
 
-    public void setAlignment(String alignment) {
-        this.alignment = alignment;
+    public void setCigarAlignment(String cigarAlignment) {
+        this.cigarAlignment = cigarAlignment;
     }
 
-    public double getEvalue() {
-        return evalue;
+     public double getFullSequenceEvalue() {
+        return fullSequenceEvalue;
     }
 
-    public void setEvalue(double evalue) {
-        this.evalue = evalue;
+    public void setFullSequenceEvalue(double fullSequenceEvalue) {
+        this.fullSequenceEvalue = fullSequenceEvalue;
     }
 
-    public double getScore() {
-        return score;
+    public double getSequenceScore() {
+        return sequenceScore;
     }
 
-    public void setScore(double score) {
-        this.score = score;
+    public void setSequenceScore(double sequenceScore) {
+        this.sequenceScore = sequenceScore;
     }
+    public Double getFullSequenceBias() {
+           return fullSequenceBias;
+       }
+
+       public void setFullSequenceBias(Double fullSequenceBias) {
+           this.fullSequenceBias = fullSequenceBias;
+       }
+
+       public Double getDomainBias() {
+           return domainBias;
+       }
+
+       public void setDomainBias(Double domainBias) {
+           this.domainBias = domainBias;
+       }
+
+       public double getDomainCEValue() {
+           return domainCEValue;
+       }
+
+       public void setDomainCEValue(double domainCEValue) {
+           this.domainCEValue = domainCEValue;
+       }
+
+       public double getDomainIEValue() {
+           return domainIEValue;
+       }
+
+       public void setDomainIEValue(double domainIEValue) {
+           this.domainIEValue = domainIEValue;
+       }
+
+       public double getScore() {
+           return score;
+       }
+
+       public void setScore(double score) {
+           this.score = score;
+       }
+    
 
     public long getHmmStart() {
         return hmmStart;
@@ -106,5 +209,69 @@ public abstract class HmmRawMatch extends RawMatch {
     public void setLocationScore(double locationScore) {
         this.locationScore = locationScore;
     }
+    public int getAlignmentFrom() {
+           return alignmentFrom;
+       }
+
+       public void setAlignmentFrom(int alignmentFrom) {
+           this.alignmentFrom = alignmentFrom;
+       }
+
+       public int getAlignmentTo() {
+           return alignmentTo;
+       }
+
+       public void setAlignmentTo(int alignmentTo) {
+           this.alignmentTo = alignmentTo;
+       }
+
+       public int getEnvelopeFrom() {
+           return envelopeFrom;
+       }
+
+       public void setEnvelopeFrom(int envelopeFrom) {
+           this.envelopeFrom = envelopeFrom;
+       }
+
+       public int getEnvelopeTo() {
+           return envelopeTo;
+       }
+
+       public void setEnvelopeTo(int envelopeTo) {
+           this.envelopeTo = envelopeTo;
+       }
+
+       public double getAcc() {
+           return acc;
+       }
+
+       public void setAcc(double acc) {
+           this.acc = acc;
+       }
+
+       public String getDescriptionOfTarget() {
+           return descriptionOfTarget;
+       }
+
+       public void setDescriptionOfTarget(String descriptionOfTarget) {
+           this.descriptionOfTarget = descriptionOfTarget;
+       }
+
+       public Integer getReleaseMajor() {
+           return releaseMajor;
+       }
+
+       public void setReleaseMajor(Integer releaseMajor) {
+           this.releaseMajor = releaseMajor;
+       }
+
+       public Integer getReleaseMinor() {
+           return releaseMinor;
+       }
+
+       public void setReleaseMinor(Integer releaseMinor) {
+           this.releaseMinor = releaseMinor;
+       }
+    
 
 }
