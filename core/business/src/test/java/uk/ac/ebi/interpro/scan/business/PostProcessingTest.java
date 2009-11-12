10d134fe095a344070c922aecb2c8c510131782b
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-11-12 18:56:33 +0000

    First commit of empty business logic module.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@198 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/business/src/test/java/uk/ac/ebi/interpro/scan/business/PostProcessingTest.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.interpro.scan.business;
+
+import org.junit.Test;
+
+/**
+ * TODO: Description
+ *
+ * @author Phil Jones
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class PostProcessingTest {
+
+    @Test
+    public void test(){}
+}
