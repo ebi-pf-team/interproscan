3caa8c577c2d11d1b7d061f164fb95c9efd79890
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-11-13 15:51:00 +0000

    Added additional DAO methods required for post-processing.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@208 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/raw/PfamHmmer3RawMatchDAO.java
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/raw/PfamHmmer3RawMatchDAO.java
@@ -3,14 +3,32 @@ package uk.ac.ebi.interpro.scan.persistence.raw;
 import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
 import uk.ac.ebi.interpro.scan.model.raw.PfamHmmer3RawMatch;
 
+import java.util.List;
+import java.util.Map;
+
 /**
- * TODO: Add class description
+ * DAO methods for PfamHmmer3RawMatchDAO objects.
  *
  * @author  Manjula Thimma
+ * @author Phil Jones
  * @version $Id$
  */
 public interface PfamHmmer3RawMatchDAO extends GenericDAO<PfamHmmer3RawMatch, Long> {
 
-     public PfamHmmer3RawMatch getPfamMatchesByModel (String methodAc);
+    public PfamHmmer3RawMatch getPfamMatchesByModel (String methodAc);
+
+    /**
+     * Returns a Map of sequence identifiers to a List of
+     * PfamHmmer3RawMatch objects for the protein IDs in the range
+     * specified (Database default String ordering)
+     *
+     * Essential for Pfam-A HMMER3 post processing.
+     * @param bottomId return protein IDs >= this String
+     * @param topId the return protein IDs <= this String
+     * @return a Map of sequence identifiers to a List of
+     * PfamHmmer3RawMatch objects for the protein IDs in the range
+     * specified (Database default String ordering)
+     */
+    public Map<String, List<PfamHmmer3RawMatch>> getMatchesForProteinIdsInRange (String bottomId, String topId);
 
 }
