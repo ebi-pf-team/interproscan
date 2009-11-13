45a97790d03bc187b42ee66c8b0d60a661d9aaeb
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-11-13 15:57:12 +0000

    Moved command line API into the interproscan-business module.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@210 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/business/src/main/java/uk/ac/ebi/interpro/scan/business/cli/FileIsNotADirectoryException.java
@@ -0,0 +1,20 @@
+package uk.ac.ebi.interpro.scan.business.cli;
+
+/**
+ * Exception to inform calling code that the path provided is not a directory.
+ *
+ * @author  Phil Jones, EMBL-EBI
+ * @version $Id: FileIsNotADirectoryException.java 19 2009-07-16 14:39:29Z aquinn.ebi $
+ * @since   1.0
+ */
+public class FileIsNotADirectoryException extends Throwable {
+
+    /**
+     * Constructor that requires an explanatory message.
+     * @param message explanation of the source of the Exception.
+     */
+    public FileIsNotADirectoryException(String message) {
+        super(message);
+    }
+
+}
