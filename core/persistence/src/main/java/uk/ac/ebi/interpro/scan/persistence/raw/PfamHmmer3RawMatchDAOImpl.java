75feb979c2748271a2f13686c6607dd06d82c552
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-11-13 15:51:00 +0000

    Added additional DAO methods required for post-processing.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@208 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/raw/PfamHmmer3RawMatchDAOImpl.java
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/raw/PfamHmmer3RawMatchDAOImpl.java
@@ -4,10 +4,14 @@ import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAOImpl;
 import uk.ac.ebi.interpro.scan.model.raw.PfamHmmer3RawMatch;
 
 import javax.persistence.Query;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 
 /**
- * TODO: Add class description
+ * DAO implementation for PfamHmmer3RawMatchDAO objects.
  *
  * @author  Manjula Thimma
  * @version $Id$
@@ -22,17 +26,41 @@ public class PfamHmmer3RawMatchDAOImpl extends GenericDAOImpl<PfamHmmer3RawMatch
         super(PfamHmmer3RawMatch.class);
     }
 
-    public PfamHmmer3RawMatch getPfamMatchesById(Long id) {
-        Query query = entityManager.createQuery("select p from PfamHmmer3RawMatch p  where p.id = :id");
-        query.setParameter("id", id);
-        return (PfamHmmer3RawMatch) query.getSingleResult();
-    }
-
     public PfamHmmer3RawMatch getPfamMatchesByModel(String methodAc) {
         // Pfam p = null;
         Query query = entityManager.createQuery("select p from PfamHmmer3RawMatch p  where p.model = :methodAc");
         query.setParameter("methodAc", methodAc);
         return (PfamHmmer3RawMatch) query.getSingleResult();
         //return p;
+    }
+
+    /**
+     * Returns a Map of sequence identifiers to a List of
+     * PfamHmmer3RawMatch objects for the protein IDs in the range
+     * specified (Database default String ordering)
+     *
+     * Essential for Pfam-A HMMER3 post processing.
+     * @param bottomId return protein IDs >= this String
+     * @param topId the return protein IDs <= this String
+     * @return a Map of sequence identifiers to a List of
+     * PfamHmmer3RawMatch objects for the protein IDs in the range
+     * specified (Database default String ordering)
+     */
+    @SuppressWarnings("unchecked")
+    public Map<String, List<PfamHmmer3RawMatch>> getMatchesForProteinIdsInRange(String bottomId, String topId) {
+        Map<String, List<PfamHmmer3RawMatch>> proteinIdToMatchMap = new HashMap<String, List<PfamHmmer3RawMatch>>();
+        Query query = entityManager.createQuery("select p from PfamHmmer3RawMatch p  where p.sequenceIdentifier >= :bottom and p.sequenceIdentifier <= :top");
+        query.setParameter("bottom", bottomId);
+        query.setParameter("top", topId);
+        List<PfamHmmer3RawMatch> resultList = query.getResultList();
+        for (PfamHmmer3RawMatch match : resultList){
+            List<PfamHmmer3RawMatch> matchList = proteinIdToMatchMap.get(match.getSequenceIdentifier());
+            if (matchList == null){
+                matchList = new ArrayList<PfamHmmer3RawMatch>();
+                proteinIdToMatchMap.put(match.getSequenceIdentifier(), matchList);
+            }
+            matchList.add(match);
+        }
+        return proteinIdToMatchMap;
     }
 }
