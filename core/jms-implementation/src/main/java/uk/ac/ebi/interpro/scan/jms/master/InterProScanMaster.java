bfba3cbe9dbabbf3152407ea48ee1d7280ac059e
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-11-16 00:49:02 +0000

    Added configuration to build of JMS project.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@241 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/master/InterProScanMaster.java
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/master/InterProScanMaster.java
@@ -1,12 +1,19 @@
 package uk.ac.ebi.interpro.scan.jms.master;
 
 import uk.ac.ebi.interpro.scan.jms.SessionHandler;
+import uk.ac.ebi.interpro.scan.management.model.Job;
+import uk.ac.ebi.interpro.scan.management.model.StepInstance;
+import uk.ac.ebi.interpro.scan.management.model.StepExecutionState;
+import uk.ac.ebi.interpro.scan.management.model.StepExecution;
 
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.TextMessage;
+import javax.jms.ObjectMessage;
 
 import org.springframework.beans.factory.annotation.Required;
+
+import java.util.List;
 
 /**
  * Pretending to be the InterProScan master application.
@@ -15,13 +22,15 @@ import org.springframework.beans.factory.annotation.Required;
  * @version $Id: TestMaster.java,v 1.4 2009/10/28 15:04:00 pjones Exp $
  * @since 1.0
  */
-public class TestMaster implements Master {
+public class InterProScanMaster implements Master {
 
     private SessionHandler sessionHandler;
 
     private String jobSubmissionQueueName;
 
     private ResponseMonitor responseMonitor;
+
+    private List<Job> jobs;
 
     private String managementRequestTopicName;
 
@@ -70,6 +79,15 @@ public class TestMaster implements Master {
         this.responseMonitor = responseMonitor;
     }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    @Required
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
     /**
      * Run the Master Application.
      */
@@ -82,10 +100,37 @@ public class TestMaster implements Master {
             // Initialise the sessionHandler for the master thread
             sessionHandler.init();
 
-            // And do some stuff.
-            for (int i = 0; i < 50; i++){
-                sendMessage(jobSubmissionQueueName, "Message number " + i);  // Send a message every second or so.
-                Thread.sleep(100);
+            List<StepInstance> stepInstances = buildStepInstancesTheStupidWay();
+
+            while(true){
+//                sendMessage(jobSubmissionQueueName, "Message number " + i);  // Send a message every second or so.
+
+
+                for (StepInstance stepInstance : stepInstances){
+                    if (stepInstance.getState() == StepExecutionState.NEW_STEP_EXECUTION){
+                        // Check if dependcies exist...
+                        boolean canRun = stepInstance.stepInstanceDependsUpon() == null;
+                        // If they do exist, check if they are (all) complete...
+                        if (! canRun){
+                            boolean allFinished = true;
+                            for (Object obj : stepInstance.stepInstanceDependsUpon()){
+                                 StepInstance preStep = (StepInstance) obj;
+                                if (preStep.getState() != StepExecutionState.STEP_EXECUTION_SUCCESSFUL){
+                                    allFinished = false;
+                                    break;
+                                }
+                            }
+                            canRun = allFinished;
+                        }
+                        if (canRun){
+                            StepExecution stepExecution = stepInstance.createStepExecution();
+//                            sendMessage(stepExecution.getStepInstance().getStep().getQueue().getName(), stepExecution);
+                            // TODO - for the moment, just sending to the defalt job submission queue.
+                            sendMessage(jobSubmissionQueueName, stepExecution);
+                        }
+                    }
+                }
+                Thread.sleep(2000);
             }
 
         } catch (JMSException e) {
@@ -104,16 +149,19 @@ public class TestMaster implements Master {
         }
     }
 
+    private List<StepInstance> buildStepInstancesTheStupidWay() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
     /**
      * Just creates simple text messages to be sent to Worker nodes.
-     * @param messageString being the String to send as a message
+     * @param stepExecution being the StepExecution to send as a message
      * @throws JMSException in the event of a failure sending the message to the JMS Broker.
      */
-    private void sendMessage(String destination, String messageString) throws JMSException {
+    private void sendMessage(String destination, StepExecution stepExecution) throws JMSException {
+        stepExecution.submit();
         MessageProducer producer = sessionHandler.getMessageProducer(destination);
-        TextMessage message = sessionHandler.createTextMessage(messageString);
-
+        ObjectMessage message = sessionHandler.createObjectMessage(stepExecution);
         producer.send(message);
-        System.out.println("Sent message: " + message.getText());
     }
 }
