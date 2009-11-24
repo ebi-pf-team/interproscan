dc9984aae73cdc4f4c57eb475d1c8f06a9709d2e
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2009-11-24 14:26:30 +0000

    Now refreshes the state of StepExecutions after they are returned from the Worker node.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@284 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/master/InterProScanHandlerImpl.java
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/master/InterProScanHandlerImpl.java
@@ -1,9 +1,16 @@
 package uk.ac.ebi.interpro.scan.jms.master;
 
+import uk.ac.ebi.interpro.scan.management.model.StepExecution;
+import uk.ac.ebi.interpro.scan.management.model.implementations.hmmer3.ParseHMMER3OutputStepExecution;
+import uk.ac.ebi.interpro.scan.model.raw.RawSequenceIdentifier;
+import uk.ac.ebi.interpro.scan.model.raw.RawMatch;
+
 import javax.jms.Message;
 import javax.jms.TextMessage;
 import javax.jms.ObjectMessage;
 import javax.jms.JMSException;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Test implementation of a ResponseHandler.  Just spits out the response to System.out.
@@ -13,6 +20,9 @@ import javax.jms.JMSException;
  * @since 1.0
  */
 public class InterProScanHandlerImpl implements ResponseHandler{
+
+
+    private volatile Map<String, StepExecution> stepExecutions;
     /**
      * Recieves a Message and does whatever is needed.
      *
@@ -23,11 +33,20 @@ public class InterProScanHandlerImpl implements ResponseHandler{
         try{
             if (message instanceof TextMessage){
                 TextMessage textMessage = (TextMessage) message;
-                System.out.println("Response message is: " + textMessage.getText());
+                System.out.println("Text Message Received: " + textMessage.getText());
             }
             else if (message instanceof ObjectMessage){
                 ObjectMessage objectMessage = (ObjectMessage) message;
-                System.out.println("Object message (.toString()) is: " + objectMessage.getObject().toString());
+                Object messageContents = objectMessage.getObject();
+                if (messageContents instanceof StepExecution){
+                    StepExecution freshStepExecution = (StepExecution) messageContents;
+                    // Some StepExecution responses require further action
+                    // (e.g. data persistence) before continuing, so deal with that here.
+                    processStepExecution(freshStepExecution);
+
+                    StepExecution dirtyStepExecution = stepExecutions.get(freshStepExecution.getId());
+                    dirtyStepExecution.refresh(freshStepExecution);
+                }
             }
             else {
                 throw new IllegalArgumentException("Don't know how to handle the message: " + message.toString());
@@ -37,5 +56,32 @@ public class InterProScanHandlerImpl implements ResponseHandler{
             e.printStackTrace();
         }
 
+    }
+
+    private void processStepExecution(StepExecution freshStepExecution) {
+        // Spit out raw match results.
+        if (freshStepExecution instanceof ParseHMMER3OutputStepExecution){
+            // TODO - Persist the raw matches.
+            ParseHMMER3OutputStepExecution parsedOutputExecution = (ParseHMMER3OutputStepExecution) freshStepExecution;
+            parsedOutputExecution.getParsedResults();
+            Set<RawSequenceIdentifier> rawMatches = parsedOutputExecution.getParsedResults();
+            for (RawSequenceIdentifier identifier : rawMatches){
+                for (RawMatch rawMatch : identifier.getMatches()){
+                    System.out.print("RAW: ");
+                    System.out.print(rawMatch.toString());
+                }
+            }
+        }
+    }
+
+    /**
+     * Sets a reference to the stepExecutions so that they can be
+     * updated from the response to the Master.
+     *
+     * @param stepExecutions that have been completed on remote nodes.
+     */
+    @Override
+    public void setStepExecutionMap(Map<String, StepExecution> stepExecutions) {
+        this.stepExecutions = stepExecutions;
     }
 }
