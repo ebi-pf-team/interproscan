d3b3ee68374e1ca127cf9752285672145aaee7df
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-01-04 16:30:46 +0000

    Removed any references to the transactiontracking package, which is now removed.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@371 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/BlastProDomLocationDAOImpl.java
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/BlastProDomLocationDAOImpl.java
@@ -1,10 +1,8 @@
 package uk.ac.ebi.interpro.scan.persistence;
 
-import uk.ac.ebi.interpro.scan.model.Protein;
-import uk.ac.ebi.interpro.scan.model.BlastProDomMatch;
-import uk.ac.ebi.interpro.scan.model.transactiontracking.TransactionSlice;
-import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAOImpl;
 import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAOImpl;
+import uk.ac.ebi.interpro.scan.model.BlastProDomMatch;
 
 import javax.persistence.Query;
 import java.util.List;
--- core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/FingerPrintsMatchDAO.java
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/FingerPrintsMatchDAO.java
@@ -1,10 +1,8 @@
 package uk.ac.ebi.interpro.scan.persistence;
+
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
 import uk.ac.ebi.interpro.scan.model.Match;
 import uk.ac.ebi.interpro.scan.model.Protein;
-import uk.ac.ebi.interpro.scan.model.FingerPrintsMatch;
-import uk.ac.ebi.interpro.scan.model.Model;
-import uk.ac.ebi.interpro.scan.model.transactiontracking.RawTransactionSlice;
-import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
 
 import java.util.List;
 
