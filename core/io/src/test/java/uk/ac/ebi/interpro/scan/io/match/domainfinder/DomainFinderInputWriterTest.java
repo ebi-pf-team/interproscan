084f4d4fbf2656d8e480d05fa3c2c87b1c13d533
manjulapramila <manjulapramila@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-01-22 14:13:55 +0000

    Unit test for Domain finder package created
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@404 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/io/src/test/java/uk/ac/ebi/interpro/scan/io/match/domainfinder/DomainFinderInputWriterTest.java
@@ -0,0 +1,34 @@
+package uk.ac.ebi.interpro.scan.io.match.domainfinder;
+
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.Ignore;
+
+import javax.annotation.Resource;
+import java.io.InputStream;
+import java.io.IOException;
+//import java.io.File;
+import java.util.*;
+
+import uk.ac.ebi.interpro.scan.io.ParseException;
+import uk.ac.ebi.interpro.scan.io.match.domainfinder.DomainFinderInputWriter;
+import uk.ac.ebi.interpro.scan.io.match.domainfinder.DomainFinderOutputParser;
+import uk.ac.ebi.interpro.scan.model.raw.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thimma
+ * Date: 22-Jan-2010
+ * Time: 13:22:35
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class DomainFinderInputWriterTest {
+
+}
