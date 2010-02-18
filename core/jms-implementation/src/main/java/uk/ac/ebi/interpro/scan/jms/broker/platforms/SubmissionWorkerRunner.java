b443e3da9c7d61e480c397e68e8dedb4977b6d9f
maslen.ebi <maslen.ebi@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-02-18 15:54:06 +0000

    changes made to implement LAN and SGE-mediated worker running, plus rewrite of LSF method
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@463 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/broker/platforms/SubmissionWorkerRunner.java
@@ -0,0 +1,94 @@
+package uk.ac.ebi.interpro.scan.jms.broker.platforms;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+
+/**
+ * User: maslen
+ * Date: 18-Feb-2010
+ * Time: 14:43:32
+ */
+
+public class SubmissionWorkerRunner implements WorkerRunner{
+
+    private String optionalPrefix;
+
+    private Resource submissionJobFile;
+
+    //Optional parameter so user can set shell to one of their own choosing.
+    //Default for both SGE and LSF is Bourne shell (/bin/sh).
+    private String shellPath;
+
+    //This is a required parameter and currently can have the values 'sge', 'lsf' or 'pbs'.
+    private String submissionType;
+
+    private Map<String,String> submissionCommand = popSubmissionKey.newMap();
+
+    public void setOptionalPrefix(String optionalPrefix) {
+        this.optionalPrefix = optionalPrefix;
+    }
+
+    @Required
+    public void setSubmissionJobFile(Resource submissionJobFile) {
+        this.submissionJobFile = submissionJobFile;
+    }
+
+    public void setShellPath(String shellPath) {
+        this.shellPath = shellPath;
+    }
+
+    @Required
+    public void setSubmissionType(String submissionType) {
+        this.submissionType = submissionType;
+    }
+
+    @Override
+    public void startupNewWorker() {
+
+        StringBuffer commandBuf = new StringBuffer();
+        if (optionalPrefix != null){
+            commandBuf.append(optionalPrefix).append(' ');
+        }
+
+        commandBuf.append(submissionCommand.get(submissionType)).append(' ');
+
+        if (shellPath != null && shellPath.length() > 0) {
+            commandBuf.append(submissionCommand.get(submissionType+"_sh")).append(' ').append(shellPath).append(' ');
+        }
+
+        if (submissionCommand.containsKey(submissionType+"_exec")) {
+            commandBuf.append(submissionCommand.get(submissionType+"_exec")).append(' ');
+        }
+
+        try{
+            commandBuf.append(submissionJobFile.getFile().getAbsolutePath());
+
+            System.out.println("Submitted Command:\t" + commandBuf.toString());
+
+            Runtime.getRuntime().exec(commandBuf.toString());
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot start the worker", e);
+        }
+    }
+
+    static class popSubmissionKey{
+        private popSubmissionKey(){
+        }
+        private static Map<String, String> newMap(){
+            Map<String, String> m = new java.util.HashMap<String,String>();
+            m.put("sge", "qsub");
+            m.put("sge_sh", "-S");
+            m.put("pbs", "qsub");
+            m.put("pbs_sh", "???");
+            m.put("lsf", "bsub");
+            m.put("lsf_sh", "-L");
+            m.put("lsf_exec", "<");
+            return Collections.unmodifiableMap(m);
+        }
+    }
+}
