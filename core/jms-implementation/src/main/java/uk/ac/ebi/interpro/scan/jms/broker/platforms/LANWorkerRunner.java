6f0b1ed9a2d73240c4d1c5e82c3a715d17f7b71d
maslen.ebi <maslen.ebi@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-02-18 15:54:06 +0000

    changes made to implement LAN and SGE-mediated worker running, plus rewrite of LSF method
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@463 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/broker/platforms/LANWorkerRunner.java
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/broker/platforms/LANWorkerRunner.java
@@ -2,41 +2,32 @@ package uk.ac.ebi.interpro.scan.jms.broker.platforms;
 
 import org.springframework.beans.factory.annotation.Required;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.List;
 
 /**
- * This WorkerRunner runs workers on named
- * hosts on the local area network.
+ * This WorkerRunner runs workers on named hosts on the local area network.
+ * <p/>
+ * It is configured with a hostname, and optionally (in the spring-config-servers.xml file) the number of
+ * JVMs to run on the host, allowing powerful / multiprocessor machines to run more than one Worker.
  *
- * It is configured with a map of host names to the number of
- * JVMs to run on the host, allowing powerful / multiprocessor machines
- * to run more than one Worker.
- *
- * @author Phil Jones
- * @version $Id: LANWorkerRunner.java,v 1.1.1.1 2009/10/07 13:37:52 pjones Exp $
+ * @author Phil Jones, John Maslen
+ * @version Id: LANWorkerRunner.java,v 1.1.1.1 2009/10/07 13:37:52 pjones Exp $
  * @since 1.0
  */
 public class LANWorkerRunner implements WorkerRunner {
-
-    Map<String, Integer> hostNameToJVMCount;
 
     String command;
 
     String accessPrefix;
 
-    /**
-     * a Map of hostnames (possibly including domain too) to the number of JVMs to run
-     * on the host.
-     * @param hostNameToJVMCount map of hostnames (possibly including domain too) to the number of JVMs to run
-     * on the host.
-     */
-    @Required
-    public void setHostNameToJVMCount(Map<String, Integer> hostNameToJVMCount) {
-        this.hostNameToJVMCount = hostNameToJVMCount;
-    }
+    LANServerSettings lanServerSettings;
+
+    LANServerListing lanServerListing;
 
     /**
      * The command to run on each host, to start the JVM / worker. (probably a call to the java executable)
+     *
      * @param command The command to run on each host, to start the JVM / worker. (probably a call to the java executable)
      */
     @Required
@@ -48,8 +39,9 @@ public class LANWorkerRunner implements WorkerRunner {
      * TODO - Really need to think about this one!
      * How to run the command on the remote host, e.g.
      * "ssh -x username@"
-     *
+     * <p/>
      * The hostname will be appended on to this.
+     *
      * @param accessPrefix What to stick in front of the hostname.
      */
     @Required
@@ -57,13 +49,46 @@ public class LANWorkerRunner implements WorkerRunner {
         this.accessPrefix = accessPrefix;
     }
 
+    public void setLanServerListing(LANServerListing lanServerListing) {
+        this.lanServerListing = lanServerListing;
+    }
+
+    public void setLanServerSettings(LANServerSettings lanServerSettings) {
+        this.lanServerSettings = lanServerSettings;
+    }
 
     /**
-     * Runs a new worker JVM, by whatever mechanism (e.g. LSF, PBS, SunGridEngine)
+     * According to the number of JVMs required, a new worker JVM is started.
      * Assumes that the jar being executed has a main class define in the MANIFEST.
      */
     @Override
     public void startupNewWorker() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        List<LANServerSettings> lanServerSettingsList = lanServerListing.getListServerSettings();
+        for (LANServerSettings serverSettings : lanServerSettingsList) {
+            setLanServerSettings(serverSettings);
+            for (int i = 0; i < lanServerSettings.getVmNumber(); i++) {
+                StringBuffer commandBuf = new StringBuffer();
+
+                commandBuf.append(accessPrefix);
+
+                if (lanServerSettings.getHostname() != null) {
+                    commandBuf.append('@').append(lanServerSettings.getHostname()).append(' ');
+                }
+
+                commandBuf.append(command);
+
+                if (lanServerSettings.getMemory() != null) {
+                    commandBuf.append(' ').append(lanServerSettings.getMemory());
+                }
+
+                System.out.println("LAN command: " + commandBuf.toString());
+
+                try {
+                    Runtime.getRuntime().exec(commandBuf.toString());
+                } catch (IOException e) {
+                    throw new IllegalStateException("Cannot start the worker", e);
+                }
+            }
+        }
     }
 }
