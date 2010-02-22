98e35e4039932f7c93fc8045e029d13a6c5f112a
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-02-22 15:42:14 +0000

    Added new XrefDAO interface and implementation.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@470 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/XrefDAO.java
@@ -0,0 +1,25 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
+import uk.ac.ebi.interpro.scan.model.Xref;
+
+/**
+ * DAO Interface for data access to the Xref table
+ * (which contains protein IDs).
+ *
+ * @author Phil Jones
+ * @author David Binns
+ * @version $Id$
+ * @since 1.0
+ */
+public interface XrefDAO extends GenericDAO<Xref, Long>  {
+
+    /**
+     * Method to return the maximum UPI stored in the database.
+     * Unlikely to be used outside the scope of the EBI.
+     *
+     * @return the maximum UPI in the Xref table.  Returns null if no UPI xref is present.
+     */
+    public String getMaxUniparcId ();
+
+}
