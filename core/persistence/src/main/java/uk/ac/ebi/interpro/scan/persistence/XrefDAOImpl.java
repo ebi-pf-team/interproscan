5e3052671263453c42c70a42f8ade81eb962e828
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-02-22 15:42:14 +0000

    Added new XrefDAO interface and implementation.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@470 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/XrefDAOImpl.java
@@ -0,0 +1,55 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAOImpl;
+import uk.ac.ebi.interpro.scan.model.Xref;
+
+import javax.persistence.Query;
+
+/**
+ * Implementation of DAO Interface for data access to the Xref table
+ * (which contains protein IDs).
+ *
+ * @author Phil Jones
+ * @author David Binns
+ * @version $Id$
+ * @since 1.0
+ */
+public class XrefDAOImpl extends GenericDAOImpl<Xref, Long> implements XrefDAO {
+
+    private static final String UPI_ZERO = "UPI0000000000";
+
+    /**
+     * Sets the class of the model that the DOA instance handles.
+     * Note that this has been set up to use constructor injection
+     * because it makes it easy to sub-class GenericDAOImpl in a robust
+     * manner.
+     * <p/>
+     * Model class specific sub-classes should define a no-argument constructor
+     * that calls this constructor with the appropriate class.
+     *
+     */
+    public XrefDAOImpl() {
+        super(Xref.class);
+    }
+
+    /**
+     * Method to return the maximum UPI stored in the database.
+     * Unlikely to be used outside the scope of the EBI.
+     *
+     * TODO - When database name is added to the Xref entity, use instead of the like clause.
+     *
+     * @return the maximum UPI in the Xref table.  Returns UPI0000000000 if no UPI xref is present.
+     */
+    @Transactional (readOnly = true)
+    public String getMaxUniparcId() {
+        Query query = entityManager.createQuery(
+                "select max(x.identifier) from Xref x where x.identifier like ('UPI__________') "
+        );
+        final String upi = (String) query.getSingleResult();
+        if (upi == null || upi.length() == 0){
+            return UPI_ZERO;
+        }
+        return upi;
+    }
+}
