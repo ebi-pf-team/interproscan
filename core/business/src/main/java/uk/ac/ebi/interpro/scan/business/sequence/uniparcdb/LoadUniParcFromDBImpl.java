40dd735d6286aa8ec928fb617d0848e86f5e382e
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-02-22 15:46:00 +0000

    Implemented mechanism for loading proteins from UniParc.  Refined fasta file loader.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@471 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/business/src/main/java/uk/ac/ebi/interpro/scan/business/sequence/uniparcdb/LoadUniParcFromDBImpl.java
@@ -0,0 +1,84 @@
+package uk.ac.ebi.interpro.scan.business.sequence.uniparcdb;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.interpro.scan.business.sequence.ProteinLoader;
+import uk.ac.ebi.interpro.scan.persistence.XrefDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Implementation of interface to load proteins
+ * from UniParc.
+ *
+ * @author Phil Jones
+ * @author David Binns
+ * @version $Id$
+ * @since 1.0
+ */
+public class LoadUniParcFromDBImpl implements LoadUniParcFromDB {
+
+        /**
+     * Define a static logger variable so that it references the
+     * Logger instance named "PeptideQuery".
+     */
+    private static Logger LOGGER = Logger.getLogger(LoadUniParcFromDBImpl.class);
+
+    /**
+     * Spring documentation claims that this object is thread safe (contains no state -
+     * singleton) so can be used in a multi-threaded environment.
+     */
+    private JdbcTemplate jdbcTemplate;
+
+    private UniParcDBRowCallbackHandler  rowCallbackHandlerTemplate;
+
+    private XrefDAO xrefDao;
+
+    private Integer maximumProteins;
+
+    @Required
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Required
+    public void setRowCallbackHandlerTemplate(UniParcDBRowCallbackHandler rowCallbackHandlerTemplate) {
+        this.rowCallbackHandlerTemplate = rowCallbackHandlerTemplate;
+    }
+
+    @Required
+    public void setXrefDao(XrefDAO xrefDao) {
+        this.xrefDao = xrefDao;
+    }
+
+    /**
+     * Sets the maximum number of proteins loaded
+     * from UniParc in a single transaction.
+     *
+     * @param maximumProteins the maximum number of proteins loaded
+     *                        from UniParc in a single transaction.
+     */
+    @Required
+    public void setMaximumProteins(Integer maximumProteins) {
+        this.maximumProteins = maximumProteins;
+    }
+
+    /**
+     * Check for new protein sequences in the UniParc
+     * database and load them (up to a maximum number)
+     */
+    @Transactional
+    public void loadNewSequences() {
+        // Get the 'high water mark' UPI in I5
+        String highWaterMark = xrefDao.getMaxUniparcId();
+        LOGGER.debug("The high water mark UPI is " + highWaterMark);
+        final String sql = "select upi, seq_short, seq_long from uniparc.protein where upi > ? and rownum <= ? order by upi ASC" ;
+        jdbcTemplate.query(sql , new Object[]{highWaterMark, maximumProteins}, rowCallbackHandlerTemplate);
+        rowCallbackHandlerTemplate.persist();
+    }
+}
---
+++ core/business/src/main/java/uk/ac/ebi/interpro/scan/business/sequence/uniparcdb/UniParcDBRowCallbackHandler.java
@@ -0,0 +1,72 @@
+package uk.ac.ebi.interpro.scan.business.sequence.uniparcdb;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import uk.ac.ebi.interpro.scan.business.sequence.ProteinLoader;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * RowCallbackHandler for one row of data from the UniParc database, containing
+ * a UPI and a sequence.  Uses the ProteinLoader object to insert this
+ * into the database, creating StepInstances as required.
+ *
+ * @author Phil Jones
+ * @author David Binns
+ * @version $Id$
+ * @since 1.0
+ */
+public class UniParcDBRowCallbackHandler implements RowCallbackHandler {
+
+    private static final Logger LOGGER = Logger.getLogger(UniParcDBRowCallbackHandler.class);
+
+    private ProteinLoader proteinLoader;
+
+    private int counter = 0;
+
+    /**
+     * Helper class that performs common protein
+     * loading functionality
+     *
+     * @param proteinLoader Helper class that performs common protein
+     *                      loading functionality
+     */
+    @Required
+    public void setProteinLoader(ProteinLoader proteinLoader) {
+        this.proteinLoader = proteinLoader;
+    }
+
+    /**
+     * Processes one row returned from:
+     * select upi, seq_short, seq_long from uniparc.protein and upi > ? and rownum <= ? order by upi ASC
+     *
+     * @param resultSet
+     * @throws SQLException
+     */
+    @Override
+    public void processRow(ResultSet resultSet) throws SQLException {
+        String upi = resultSet.getString(1);
+        String sequence = resultSet.getString(2);
+        if (sequence == null || sequence.length() == 0){
+            sequence = resultSet.getString(3);
+        }
+        if (sequence == null) {
+            throw new IllegalArgumentException ("Ugh - found a UPI with no associated sequence!");
+        }
+        if (LOGGER.isDebugEnabled()){
+            if (counter++ % 200 == 0){
+                LOGGER.debug("Storing " + upi + " with sequence length "+ sequence.length());
+            }
+        }
+        proteinLoader.store(sequence, upi);
+    }
+
+    /**
+     * Call persist on the underlying proteinLoader at the end of the transaction.
+     */
+    public void persist(){
+        proteinLoader.persist();
+    }
+}
