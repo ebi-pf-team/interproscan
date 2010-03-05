7fd68275a16258c87cfe56d5b7c6192e19bf2432
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-03-05 16:19:27 +0000

    Simplified the Master - the response monitor is now a MessageListener - lots of code and configuration removed - more to come.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@484 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/master/ResponseMonitorImpl.java
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/master/ResponseMonitorImpl.java
@@ -4,12 +4,10 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import uk.ac.ebi.interpro.scan.jms.SessionHandler;
+import uk.ac.ebi.interpro.scan.management.dao.StepExecutionDAO;
 import uk.ac.ebi.interpro.scan.management.model.StepExecution;
 
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
+import javax.jms.*;
 import java.util.Map;
 
 /**
@@ -20,112 +18,34 @@ import java.util.Map;
  * @version $Id: ResponseMonitorImpl.java,v 1.1.1.1 2009/10/07 13:37:52 pjones Exp $
  * @since 1.0
  */
-public class ResponseMonitorImpl implements ResponseMonitor{
+public class ResponseMonitorImpl implements MessageListener {
 
     private static final Logger LOGGER = Logger.getLogger(ResponseMonitorImpl.class);
 
-    /**
-     * Handles the response from the worker thread.
-     */
-    private ResponseHandler handler;
+    StepExecutionDAO stepExecutionDAO;
 
-    private String workerJobResponseQueueName;
-
-    private volatile boolean running = true;
-
-    private int monitorResponseTimeout;
-
-    private ConnectionFactory connectionFactory;
-
-    @Required
-    public void setConnectionFactory(ConnectionFactory connectionFactory) {
-        this.connectionFactory = connectionFactory;
-    }
-    /**
-     * Sets the ResponseHandler.  This is the implementation
-     * that knows how to deal with responses on the destinationResponseQueue.
-     * @param handler the implementation
-     * that knows how to deal with responses on the destinationResponseQueue.
-     */
-    @Required
-    public void setHandler(ResponseHandler handler) {
-        this.handler = handler;
+    public ResponseMonitorImpl(StepExecutionDAO stepExecutionDAO) {
+        this.stepExecutionDAO = stepExecutionDAO;
     }
 
-    /**
-     * Sets the name of the destinationResponseQueue.
-     * @param workerJobResponseQueueName the name of the destinationResponseQueue.
-     */
-    @Required
-    public void setWorkerJobResponseQueueName(String workerJobResponseQueueName) {
-        this.workerJobResponseQueueName = workerJobResponseQueueName;
-    }
-
-    @Required
-    public void setMonitorResponseTimeout(int monitorResponseTimeout) {
-        if (monitorResponseTimeout <= 0){
-            throw new IllegalArgumentException ("The timeout for the ResponseMonitor must be greater than 0.");
-        }
-        this.monitorResponseTimeout = monitorResponseTimeout;
-    }
-
-    /**
-     * Method to gracefully shut down the ResponseMonitor. (i.e. it will finish
-     * handling whatever response it is currently working on first.)
-     */
     @Override
-    public void shutDownMonitor() {
-        // Sets the setToRun flag to false.  The monitor will finish handling
-        // the current message prior to ending.
-        running = false;
-    }
-
-    /**
-     * Sets a reference to the Map of StepExecutions so that the
-     * ResponseMonitor can update their state.
-     * <p/>
-     * Temporary only - will eventually update the StepExecutions
-     * in the database.
-     *
-     * @param stepExecutions
-     */
-    @Override
-    public void setStepExecutionMap(Map<Long, StepExecution> stepExecutions) {
-        this.handler.setStepExecutionMap(stepExecutions);
-    }
-
-    /**
-     * This runnable monitors the destinationResponseQueue and then handles
-     * responses from Workers using the injected ResponseHandler
-     *
-     * @see Thread#run()
-     */
-    @Override
-    public void run() {
-        SessionHandler sessionHandler = null;
+    public void onMessage(Message message) {
         try{
-            sessionHandler = new SessionHandler(connectionFactory);
-            MessageConsumer messageConsumer = sessionHandler.getMessageConsumer(workerJobResponseQueueName);
-            while (running){
-                // Consume messages off the response queue and handle them.
-                sessionHandler.start();
-                Message message = messageConsumer.receive(monitorResponseTimeout);
-                if (message != null) {
-                    handler.handleResponse(message);
-                    message.acknowledge();
+            if (message instanceof ObjectMessage){
+                ObjectMessage objectMessage = (ObjectMessage) message;
+                Object messageContents = objectMessage.getObject();
+                if (messageContents instanceof StepExecution){
+                    StepExecution freshStepExecution = (StepExecution) messageContents;
+                    stepExecutionDAO.refreshStepExecution(freshStepExecution);
                 }
             }
-        } catch (JMSException e) {
-            LOGGER.error ("JMSException thrown by ResponseMonitorImpl", e);
+            else {
+                LOGGER.error ("Received message that I don't know how to handle.");
+                throw new IllegalArgumentException("Don't know how to handle the message: " + message.toString());
+            }
         }
-        finally {
-            try {
-                if (sessionHandler != null){
-                    sessionHandler.close();
-                }
-            } catch (JMSException e) {
-                LOGGER.error ("JMSException thrown by ResponseMonitorImpl when attempting to close JMS Session & Connection.", e);
-            }
+        catch (JMSException jmse){
+            LOGGER.error("JMSException thrown by Response Monitor on the Master", jmse);
         }
     }
 }
