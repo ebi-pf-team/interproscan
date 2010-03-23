64488904fcf7041d5aea25cf09888f25db2eeb4a
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-03-23 18:04:05 +0000

    Reinstated the embedded Broker (but properly this time).
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@519 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/broker/EmbeddedBroker.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.interpro.scan.jms.broker;
+
+/**
+ * An attempt to factor out the EmbeddedBroker concept, so there is no
+ * direct dependency upon HornetQ in the code.
+ *
+ * @author Phil Jones
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public interface EmbeddedBroker {
+
+    void runBroker();
+}
---
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/broker/EmbeddedHornetQBroker.java
@@ -0,0 +1,68 @@
+package uk.ac.ebi.interpro.scan.jms.broker;
+
+import org.apache.log4j.Logger;
+import org.hornetq.core.config.Configuration;
+import org.hornetq.core.server.HornetQServer;
+import org.hornetq.core.server.HornetQServers;
+import org.hornetq.jms.server.JMSServerManager;
+import org.hornetq.jms.server.impl.JMSServerManagerImpl;
+import org.springframework.beans.factory.annotation.Required;
+
+import javax.naming.Context;
+
+/**
+ * Simple embedded HornetQ Server.
+ *
+ * This is expected to be run from the Master, which will communicate
+ * with this broker using an in-jvm connection.
+ *
+ * @author Phil Jones
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class EmbeddedHornetQBroker implements EmbeddedBroker {
+
+    private static final Logger LOGGER = Logger.getLogger(EmbeddedHornetQBroker.class.getName());
+
+    private Configuration hornetQConfig;
+
+    private String jmsConfigFileName;
+
+    private Context context;
+
+    /**
+     * COnfiguration of the hornetQ server.
+     * @param hornetQConfig
+     */
+    @Required
+    public void setHornetQConfig(Configuration hornetQConfig) {
+        this.hornetQConfig = hornetQConfig;
+    }
+
+    @Required
+    public void setJmsConfigFileName(String jmsConfigFileName) {
+        this.jmsConfigFileName = jmsConfigFileName;
+    }
+
+    /**
+     * OPTIONAL JNDI Context for the JMS server.
+     * @param context OPTIONAL JNDI Context for the JMS server.
+     */
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void runBroker(){
+        try{
+            HornetQServer server = HornetQServers.newHornetQServer(hornetQConfig);
+            JMSServerManager jmsServerManager = new JMSServerManagerImpl(server, jmsConfigFileName);
+            jmsServerManager.setContext(context);
+            jmsServerManager.start();
+            LOGGER.info("STARTED::");
+        }
+        catch (Throwable e){
+            LOGGER.fatal("Failed to runBroker Broker.", e);
+        }
+    }
+}
