49b58e3a96500fe2f9108152df133958c0bb0f43
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-03-24 14:17:40 +0000

    Updated script to run the master on ebi-002.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@523 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/jms-implementation/EBI-LSF.sh
+++ core/jms-implementation/EBI-LSF.sh
@@ -4,8 +4,11 @@ cd "$(dirname "$0")"
 
 target="/homes/$USER"
 
-rsync -avz target/interproscan-5-dist.dir "ebi-004.ebi.ac.uk:$target"
+rsync -avz target/interproscan-5-dist "ebi-002.ebi.ac.uk:$target"
 
-ssh ebi-004.ebi.ac.uk /sw/arch/pkg/jdk1.6/bin/java -Dconfig=$target/interproscan-5.properties -jar "$target/interproscan-5-dist.dir/interproscan-5.jar"
+# Empty the JMS broker before starting up.
+ssh ebi-002.ebi.ac.uk rm -rf $target/i5_broker_data
+
+ssh ebi-002.ebi.ac.uk /sw/arch/pkg/jdk1.6/bin/java -Djava.util.logging.config.file=$target/interproscan-5-dist/scripts/hornetq.logging.properties -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -Xms512M -Xmx2048M -Dconfig=$target/interproscan-5.properties -jar "$target/interproscan-5-dist/interproscan-5.jar" master
 
 #end
