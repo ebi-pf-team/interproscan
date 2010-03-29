7903cf6d2bd3c9e0e906df2d4a8ab3ae65080018
dwbinns <dwbinns@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-03-29 13:36:26 +0000

    Cleaning before execution
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@527 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/jms-implementation/EBI-LSF.sh
+++ core/jms-implementation/EBI-LSF.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-
+distribution_folder=interproscan-5-dist.dir
+master_host=ebi-002.ebi.ac.uk
 target="/homes/$USER"
+broker_data_folder=$target/$distribution_folder/broker_data
+temporary_file_folder=$target/$distribution_folder/temp
 
-rsync -avz target/interproscan-5-dist "ebi-002.ebi.ac.uk:$target"
 
 # Empty the JMS broker before starting up.
-ssh ebi-002.ebi.ac.uk rm -rf $target/i5_broker_data
+ssh ${master_host} ps -o pid,cmd -u $USER \| grep interproscan \| cut -c 1-5 \| xargs kill
+ssh ${master_host} bkill -g /interproscan-test 0
+ssh ${master_host} rm -r $broker_data_folder
+ssh ${master_host} rm -r $temporary_file_folder
+ssh ${master_host} sqlplus /@ispro @/nfs/seqdb/production/interpro/production/interproscan-5/empty.sql
 
-ssh ebi-002.ebi.ac.uk /sw/arch/pkg/jdk1.6/bin/java -Djava.util.logging.config.file=$target/interproscan-5-dist/scripts/hornetq.logging.properties -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -Xms512M -Xmx2048M -Dconfig=$target/interproscan-5.properties -jar "$target/interproscan-5-dist/interproscan-5.jar" master
+rsync -avz target/${distribution_folder} "${master_host}:$target"
+
+ssh ${master_host} cd $target/$distribution_folder\;/sw/arch/pkg/jdk1.6/bin/java -Djava.util.logging.config.file=$target/${distribution_folder}/scripts/hornetq.logging.properties -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -Xms512M -Xmx2048M -Dconfig=$target/interproscan-5.properties -jar "interproscan-5.jar" "$@"
 
 #end
