086bdd720ad17a2166b0707985c76e814cbb7ec2
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-06-07 09:54:59 +0000

    Update to order output by Protein first.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@651 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/gene3d/DomainFinderResourceWriter.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/gene3d/DomainFinderResourceWriter.java
@@ -7,40 +7,47 @@ import java.util.*;
 /**
  * Writes DomainFinder3 output.
  *
- * @author  Antony Quinn
+ * @author Antony Quinn
  * @version $Id$
  */
 public final class DomainFinderResourceWriter extends AbstractResourceWriter<DomainFinderRecord> {
 
-    @Override protected String createLine(DomainFinderRecord record) {
+    @Override
+    protected String createLine(DomainFinderRecord record) {
         return DomainFinderRecord.toLine(record);
     }
 
     /**
      * Returns records sorted by sequence e-value, then model ID, and then by sequence start if model hits sequence more than once.
      *
-     * @param  records Collection to sort
+     * @param records Collection to sort
      * @return Records sorted by sequence e-value, then model ID, and then by sequence start if model hits sequence more than once.
      */
-    @Override protected Collection<DomainFinderRecord> sort(Collection<DomainFinderRecord> records) {
+    @Override
+    protected Collection<DomainFinderRecord> sort(Collection<DomainFinderRecord> records) {
         List<DomainFinderRecord> list = new ArrayList<DomainFinderRecord>(records);
         Collections.sort(list, new Comparator<DomainFinderRecord>() {
-                public int compare(DomainFinderRecord record1, DomainFinderRecord record2) {
-                    // Sort by sequence e-value
-                    int c = record1.getSequenceEvalue().compareTo(record2.getSequenceEvalue());
-                    // If e-values are the same, sort by model ID (alphabetically: a to z)
-                    if (c == 0) {
-                        c = record1.getModelId().compareTo(record2.getModelId());
-                    }
-                    // If model IDs are the same, sort by sequence start
-                    if (c == 0) {
-                        return record1.getSequenceStart().compareTo(record2.getSequenceStart());
-                    }
-                    else    {
-                        return c;
-                    }
+            public int compare(DomainFinderRecord record1, DomainFinderRecord record2) {
+
+                // Sort by sequence identifier.
+                int c = record1.getSequenceId().compareTo(record2.getSequenceId());
+
+                // Sort by sequence e-value
+                if (c == 0) {
+                    c = record1.getSequenceEvalue().compareTo(record2.getSequenceEvalue());
+                }
+                // If e-values are the same, sort by model ID (alphabetically: a to z)
+                if (c == 0) {
+                    c = record1.getModelId().compareTo(record2.getModelId());
+                }
+                // If model IDs are the same, sort by sequence start
+                if (c == 0) {
+                    return record1.getSequenceStart().compareTo(record2.getSequenceStart());
+                } else {
+                    return c;
                 }
             }
+        }
         );
         return list;
     }
