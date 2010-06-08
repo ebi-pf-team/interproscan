1e514998f32444abc9efc981710d5a8dc52f839b
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-06-08 12:57:48 +0000

    Update PvalParser to use the simple 1 signature <-> 1 model mechanism.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@659 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/prints/PvalParser.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/prints/PvalParser.java
@@ -12,7 +12,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.*;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Parser for pval files that contain PRINTS models. (Models in PRINTS parlance!)
@@ -31,8 +33,6 @@ public class PvalParser implements Serializable {
     private static final String LINE_SIG_ACCESSION = "gx";
     private static final String LINE_SIG_DESCRIPTION = "gi";
     private static final String LINE_SIG_MODEL_COUNT = "gn";
-    private static final String LINE_MODEL_ACCESSION = "mx";
-    private static final String LINE_MODEL_NAME = "mi";
 
     private String releaseVersion;
 
@@ -47,8 +47,6 @@ public class PvalParser implements Serializable {
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(new FileReader(printsPvalFilePath));
-            PartialModel partialModelUnderConstruction = null;
-            final List<PartialModel> models = new ArrayList<PartialModel>();
             String line, sigAcc = null, sigName = null, sigDescription = null;
             Integer modelCount = null;
             while ((line = reader.readLine()) != null) {
@@ -58,15 +56,13 @@ public class PvalParser implements Serializable {
                     }
                 }
                 if (line.startsWith(LINE_SIG_NAME)) {
-                    createSignature(release, kdatFileData, sigAcc, sigName, sigDescription, models, modelCount);
+                    createSignature(release, kdatFileData, sigAcc, sigName, sigDescription);
 
                     // Now clear / reset all local variables.
                     sigName = extractLineContent(line);
                     sigAcc = null;
                     sigDescription = null;
-                    partialModelUnderConstruction = null;
                     modelCount = null;
-                    models.clear();
                 } else if (line.startsWith(LINE_SIG_ACCESSION)) {
                     sigAcc = extractLineContent(line);
                 } else if (line.startsWith(LINE_SIG_MODEL_COUNT)) {
@@ -74,14 +70,9 @@ public class PvalParser implements Serializable {
                     modelCount = Integer.parseInt(modelCountString);
                 } else if (line.startsWith(LINE_SIG_DESCRIPTION)) {
                     sigDescription = extractLineContent(line);
-                } else if (line.startsWith(LINE_MODEL_ACCESSION)) {
-                    partialModelUnderConstruction = new PartialModel(extractLineContent(line));
-                    models.add(partialModelUnderConstruction);
-                } else if (line.startsWith(LINE_MODEL_NAME)) {
-                    partialModelUnderConstruction.setName(extractLineContent(line));
                 }
             }
-            createSignature(release, kdatFileData, sigAcc, sigName, sigDescription, models, modelCount);
+            createSignature(release, kdatFileData, sigAcc, sigName, sigDescription);
         }
         finally {
             if (reader != null) {
@@ -95,57 +86,21 @@ public class PvalParser implements Serializable {
         return line.substring(3).trim();
     }
 
-    private void createSignature(final SignatureLibraryRelease release, Map<String, String> kdatFileData, final String sigAcc, final String sigName, final String sigDescription, final List<PartialModel> partialModels, final Integer modelCount) {
+    private void createSignature(final SignatureLibraryRelease release, Map<String, String> kdatFileData, final String sigAcc, final String sigName, final String sigDescription) {
         if (sigName != null) {   // If sigName is null, this is the very first one in the file, so nothing to do.
-            if (modelCount == null || modelCount != partialModels.size()) {
-                throw new ParseException("The number of models found for " + sigAcc + " does not match the value given on the gn; line. (gn: " + modelCount + ", number of models: " + partialModels.size());
-            }
-
             final String sigAbstract = kdatFileData.get(sigAcc);
 
             if (sigAbstract == null) {
                 throw new ParseException("There is no corresponding entry in the kdat file for fingerprint " + sigAcc);
             }
 
-            final Set<Model> models = new HashSet<Model>();
-            // Build the Models
-            for (PartialModel partialModel : partialModels) {
-                models.add(new Model(partialModel.getAccession(), partialModel.getName(), null));
-            }
+            final Set<Model> model = Collections.singleton(
+                    new Model(sigAcc, sigName, sigDescription)
+            );
 
             release.addSignature(
-                    new Signature(sigAcc, sigName, null, sigDescription, sigAbstract, release, models)
+                    new Signature(sigAcc, sigName, null, sigDescription, sigAbstract, release, model)
             );
         }
     }
-
-    /**
-     * Simple bean to allow model data to be parsed out - temporary object.
-     */
-    private class PartialModel {
-
-        private final String accession;
-
-        private String name;
-
-        private PartialModel(String accession) {
-            this.accession = accession;
-        }
-
-        public String getAccession() {
-            return accession;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            if (this.name != null) {
-                throw new IllegalStateException("Attempting to set the name of this PartialModel (PRINTS parsing object), but this value has already been set.");
-            }
-            this.name = name;
-        }
-    }
-
 }
