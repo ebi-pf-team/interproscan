175731367348a07254f5f24d53314f04a5025e2b
maslen.ebi <maslen.ebi@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-06-24 14:09:09 +0000

    replacing PrintsProtein with slimmed down PrintsMotif
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@714 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/prints/parsemodel/PrintsMotif.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/prints/parsemodel/PrintsMotif.java
@@ -1,8 +1,8 @@
 package uk.ac.ebi.interpro.scan.io.match.prints.parsemodel;
 
 /**
- * Simple model class to hold details of PRINTS matches
- * during parsing.
+ * Simple model class to hold details of PRINTS motif details for the building of PrintsRawMatch objects where these details are added to with
+ * the required parameters for each individual motif.
  *
  * @author John Maslen
  * @version $Id$
@@ -14,7 +14,8 @@ package uk.ac.ebi.interpro.scan.io.match.prints.parsemodel;
 //Sn 			→		proteinID
 //1TBH 			→		motifName (1), eValue (2)#
 //2TBH/2TBN		→		motifName (1), eValue(9), graphScan (10)
-//3TBH/3TBN 		→		motifName (1), motifNo (2), motifTotal (4), idScore (5), pValue (7)*, length (9), position (11)**
+// NOT REQUIRED HERE:
+// 3TBH/3TBN 		→		motifName (1), motifNo (2), motifTotal (4), idScore (5), pValue (7)*, length (9), position (11)**
 //
 //# the  eValue is converted to log10 for storing in the database
 //      e.g. 	 eValue = Consts.log10(eValue);
@@ -40,48 +41,31 @@ package uk.ac.ebi.interpro.scan.io.match.prints.parsemodel;
 //2TBH GLU5KINASE      5  of  5  2.6e+02  52       2861       1.5e-55     1.1e-49     IIIII
 //2TBN ASNGLNASE       2  of  3  53.34    26.67    326        5.5e-06     15          I.i
 //2TBF
-//3TBS
-//3TBT MotifName       No.Mots   IdScore PfScore Pvalue    Sequence                                                Len  low  pos   high
-//3TBH GLU5KINASE      1  of  5  60.49   466     2.19e-09  GSDVVIVSSGAIAAG                                         15   0    52    0
-//….
-//3TBB
-//3TBN ASNGLNASE       1  of  3  32.54   196     1.91e-03  VVIVSSGAIAAG                                            12   0    55    0
-//3TBB
-//3TBF
 
 
-public class PrintsProtein {
+public class PrintsMotif {
 
     private String proteinIdentifier;
 
     private String motifName;
 
-    private int motifNo;
-
-    private int motifTotal;
-
-    //TODO -> Ascertain how release numbers are handled in I5
-
-    //private int dbRelNoMajor;
-
-    //private int dbRelNoMinor;
-
-    private float idScore;
-
-    private float pValue;
-
-    private int seqStartPos;
-
-    private int seqEndPos;
+    private String modelId;
 
     private String graphScan;
 
-    private Float eValue;
+    private Double eValue;
 
-    public PrintsProtein(String proteinIdentifier, String motifName, Float eValue) {
+    public PrintsMotif(String proteinIdentifier, String motifName, double eValue, String modelId) {
         this.proteinIdentifier = proteinIdentifier;
         this.motifName = motifName;
         this.eValue = eValue;
+        this.modelId = modelId;
+    }
+
+    public PrintsMotif(String proteinIdentifier, String motifName, double eValue) {
+        this.proteinIdentifier = proteinIdentifier;
+        this.motifName = motifName;
+        this.eValue = eValue;;
     }
 
     public String getProteinIdentifier() {
@@ -92,36 +76,20 @@ public class PrintsProtein {
         return motifName;
     }
 
-    public int getMotifNo() {
-        return motifNo;
-    }
-
-    public int getMotifTotal() {
-        return motifTotal;
-    }
-
-    public float getIdScore() {
-        return idScore;
-    }
-
-    public float getpValue() {
-        return pValue;
-    }
-
-    public int getSeqStartPos() {
-        return seqStartPos;
-    }
-
-    public int getSeqEndPos() {
-        return seqEndPos;
-    }
-
     public String getGraphScan() {
         return graphScan;
     }
 
-    public Float geteValue() {
+    public Double geteValue() {
         return eValue;
+    }
+
+    public String getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(String modelId) {
+        this.modelId = modelId;
     }
 
     public void setProteinIdentifier(String proteinIdentifier) {
@@ -132,36 +100,11 @@ public class PrintsProtein {
         this.motifName = motifName;
     }
 
-    public void setMotifNo(int motifNo) {
-        this.motifNo = motifNo;
-    }
-
-    public void setMotifTotal(int motifTotal) {
-        this.motifTotal = motifTotal;
-    }
-
-    public void setIdScore(float idScore) {
-        this.idScore = idScore;
-    }
-
-    public void setpValue(float pValue) {
-        this.pValue = pValue;
-    }
-
-    public void setSeqStartPos(int seqStartPos) {
-        this.seqStartPos = seqStartPos;
-    }
-
-    public void setSeqEndPos(int seqEndPos) {
-        this.seqEndPos = seqEndPos;
-    }
-
     public void setGraphScan(String graphScan) {
         this.graphScan = graphScan;
     }
 
-    public void seteValue(Float eValue) {
+    public void seteValue(double eValue) {
         this.eValue = eValue;
     }
-
 }
