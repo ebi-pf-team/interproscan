f2ae0b24890e91ac66f05c00b345a99e88cc4850
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-06-30 10:30:19 +0000

    Reverted previous model-breaking change - an alternative solution is called for.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@733 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PrintsRawMatch.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PrintsRawMatch.java
@@ -10,15 +10,12 @@ import javax.persistence.Table;
  * <a href="http://www.bioinf.manchester.ac.uk/dbbrowser/PRINTS/">PRINTS</a> raw match.
  *
  * @author Antony Quinn
- * @author Phil Jones
  * @version $Id$
  */
 @Entity
 @Table(name = "prints_raw_match")
-public class PrintsRawMatch extends RawMatch implements Comparable<PrintsRawMatch> {
+public class PrintsRawMatch extends RawMatch {
 
-    @Column
-    private String printsModelId;
     @Column
     private double evalue;
     @Column(length = 15)
@@ -37,14 +34,13 @@ public class PrintsRawMatch extends RawMatch implements Comparable<PrintsRawMatc
 
     /* TODO - Use setters instead of assigning directly? */
 
-    public PrintsRawMatch(String sequenceIdentifier, String modelAccession, String modelId,
+    public PrintsRawMatch(String sequenceIdentifier, String model,
                           String signatureLibraryRelease,
                           int locationStart, int locationEnd,
                           double evalue, String graphscan,
                           int motifCount, int motifNumber,
                           double pvalue, double score) {
-        super(sequenceIdentifier, modelAccession, SignatureLibrary.PRINTS, signatureLibraryRelease, locationStart, locationEnd);
-        this.printsModelId = modelId;
+        super(sequenceIdentifier, model, SignatureLibrary.PRINTS, signatureLibraryRelease, locationStart, locationEnd);
         this.evalue = evalue;
         this.graphscan = graphscan;
         this.motifCount = motifCount;
@@ -101,91 +97,4 @@ public class PrintsRawMatch extends RawMatch implements Comparable<PrintsRawMatc
         this.motifNumber = motifNumber;
     }
 
-    public String getPrintsModelId() {
-        return printsModelId;
-    }
-
-    public void setPrintsModelId(String printsModelId) {
-        this.printsModelId = printsModelId;
-    }
-
-    /**
-     * Compares this object with the specified object for order.  Returns a
-     * negative integer, zero, or a positive integer as this object is less
-     * than, equal to, or greater than the specified object.
-     * <p/>
-     * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
-     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
-     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
-     * <tt>y.compareTo(x)</tt> throws an exception.)
-     * <p/>
-     * <p>The implementor must also ensure that the relation is transitive:
-     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
-     * <tt>x.compareTo(z)&gt;0</tt>.
-     * <p/>
-     * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
-     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
-     * all <tt>z</tt>.
-     * <p/>
-     * <p>It is strongly recommended, but <i>not</i> strictly required that
-     * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
-     * class that implements the <tt>Comparable</tt> interface and violates
-     * this condition should clearly indicate this fact.  The recommended
-     * language is "Note: this class has a natural ordering that is
-     * inconsistent with equals."
-     * <p/>
-     * <p>In the foregoing description, the notation
-     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
-     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
-     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
-     * <i>expression</i> is negative, zero or positive.
-     *
-     * @param that the object to be compared.
-     * @return a negative integer, zero, or a positive integer as this object
-     *         is less than, equal to, or greater than the specified object.
-     * @throws ClassCastException if the specified object's type prevents it
-     *                            from being compared to this object.
-     */
-    @Override
-    public int compareTo(PrintsRawMatch that) {
-        int comparison = this.getSequenceIdentifier().compareTo(that.getSequenceIdentifier());
-        if (comparison == 0) {
-            if (this.getEvalue() < that.getEvalue()) comparison = -1;
-            else if (this.getEvalue() > that.getEvalue()) comparison = 1;
-        }
-        if (comparison == 0) {
-            comparison = this.getModel().compareTo(that.getModel());
-        }
-        if (comparison == 0) {
-            if (this.getLocationStart() < that.getLocationStart()) comparison = -1;
-            else if (this.getLocationStart() > that.getLocationStart()) comparison = 1;
-        }
-        if (comparison == 0) {
-            if (this.getLocationEnd() < that.getLocationEnd()) comparison = -1;
-            else if (this.getLocationEnd() > that.getLocationEnd()) comparison = 1;
-        }
-        return comparison;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PrintsRawMatch)) return false;
-        if (!super.equals(o)) return false;
-
-        PrintsRawMatch that = (PrintsRawMatch) o;
-
-        if (Double.compare(that.evalue, evalue) != 0) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        long temp;
-        temp = evalue != +0.0d ? Double.doubleToLongBits(evalue) : 0L;
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
 }
