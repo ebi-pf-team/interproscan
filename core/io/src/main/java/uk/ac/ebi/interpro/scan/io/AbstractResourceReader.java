b9b00042c5794688dd590583c019dafdd4c7e0c1
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-07-29 09:20:43 +0000

    Corrected use of while(reader.ready()).  Changed to while ((line = reader.readLine()) != null) as reader.ready() can block and return false before the end of a file.  reader.readLine() only returns null when EOF is reached.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@753 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/AbstractResourceReader.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/AbstractResourceReader.java
@@ -2,48 +2,53 @@ package uk.ac.ebi.interpro.scan.io;
 
 import org.springframework.core.io.Resource;
 
-import java.util.*;
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Default implementation.
  *
- * @author  Antony Quinn
+ * @author Antony Quinn
  * @version $Id$
  */
 public abstract class AbstractResourceReader<T> implements ResourceReader<T> {
 
-    @Override public Collection<T> read(Resource resource) throws IOException {
+    @Override
+    public Collection<T> read(Resource resource) throws IOException {
         if (resource == null) {
             throw new NullPointerException("Resource is null");
         }
-        if (!resource.exists())  {
+        if (!resource.exists()) {
             throw new IllegalStateException(resource.getFilename() + " does not exist");
         }
-        if (!resource.isReadable())  {
+        if (!resource.isReadable()) {
             throw new IllegalStateException(resource.getFilename() + " is not readable");
         }
         final List<T> records = new ArrayList<T>();
         BufferedReader reader = null;
-        try{
+        try {
             reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
-            while (reader.ready()) {
-                T record = createRecord(reader.readLine());
+            String line;
+            while ((line = reader.readLine()) != null) {
+                T record = createRecord(line);
                 if (record != null) {
                     records.add(record);
                 }
             }
         }
         finally {
-            if (reader != null){
+            if (reader != null) {
                 reader.close();
             }
         }
         return Collections.unmodifiableList(records);
     }
-    
+
     protected abstract T createRecord(String line);
 
 }
