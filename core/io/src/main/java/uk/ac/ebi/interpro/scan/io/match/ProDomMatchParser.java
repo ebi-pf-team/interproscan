3a25d38456a9f9a4a8f8d9877f20325a17530da5
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-08-02 17:02:49 +0000

    * Prosite parsing implemented
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@760 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/ProDomMatchParser.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/ProDomMatchParser.java
@@ -1,25 +1,25 @@
 package uk.ac.ebi.interpro.scan.io.match;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
 import uk.ac.ebi.interpro.scan.model.SignatureLibrary;
 import uk.ac.ebi.interpro.scan.model.raw.ProDomRawMatch;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Parser for the output from Prodom
  *
- * @author  Phil Jones
- * @author  Antony Quinn
+ * @author Phil Jones
+ * @author Antony Quinn
  * @version $Id$
  */
 public class ProDomMatchParser extends AbstractLineMatchParser<ProDomRawMatch> {
 
     /**
      * Pattern to match a line from the raw Prodom output, such as:
-     *
+     * <p/>
      * UPI0000009C90_status=active      4     61 //  pd_PD003417;sp_RL35_VIBPA_Q87Q69;       4     61 // S=296    E=3e-27  //  (157)  RIBOSOMAL L35 50S RIBONUCLEOPROTEIN CHLOROPLAST YNL122C SEQUENCING DIRECT CEREVISIAE P53921  	 Length = 58
-     *
+     * <p/>
      * Groups:
      * 1 Protein Accession (as in Fasta File) (String)
      * 2 Sequence match start coordinate (Integer)
@@ -30,7 +30,7 @@ public class ProDomMatchParser extends AbstractLineMatchParser<ProDomRawMatch> {
      * 7 Score (Integer????) TODO Check this - currently using regex for any real number - may be able to simplify
      * 11 E value (Floating point - could be exponent)
      * 15 Integer value in brackets before signature description !? TODO - Find out what this is!  Is it an integer?
-     *
+     * <p/>
      * This regex is currently not grouping the signature description or the "Length" value at the end of the line TODO - Check these are not required.
      */
     private static final Pattern PRODOM_LINE_PATTERN = Pattern.compile(
@@ -41,25 +41,24 @@ public class ProDomMatchParser extends AbstractLineMatchParser<ProDomRawMatch> {
         super(signatureLibrary, signatureLibraryRelease);
     }
 
-    @Override protected ProDomRawMatch createMatch(SignatureLibrary signatureLibrary,
-                                                   String signatureLibraryRelease,
-                                                   String line) {
+    @Override
+    protected ProDomRawMatch createMatch(String line) {
         Matcher matcher = PRODOM_LINE_PATTERN.matcher(line);
         if (!matcher.matches()) {
             //throw new IllegalStateException("Unrecognised line: " + line);
             return null;
         }
-        String sequenceIdentifier   = matcher.group(1);
-        int sequenceStart           = Integer.parseInt(matcher.group(2));
-        int sequenceEnd             = Integer.parseInt(matcher.group(3));
-        String model                = matcher.group(4);
-        int modelStart              = Integer.parseInt(matcher.group(5));
-        int modelEnd                = Integer.parseInt(matcher.group(6));
-        double score                = Double.parseDouble(matcher.group(7));
-        double evalue               = Double.parseDouble(matcher.group(11));
-        int unknown                 = Integer.parseInt(matcher.group(15));
-        return new ProDomRawMatch(sequenceIdentifier, model, signatureLibraryRelease,
-                                  sequenceStart, sequenceEnd, score);
+        String sequenceIdentifier = matcher.group(1);
+        int sequenceStart = Integer.parseInt(matcher.group(2));
+        int sequenceEnd = Integer.parseInt(matcher.group(3));
+        String model = matcher.group(4);
+        int modelStart = Integer.parseInt(matcher.group(5));
+        int modelEnd = Integer.parseInt(matcher.group(6));
+        double score = Double.parseDouble(matcher.group(7));
+        double evalue = Double.parseDouble(matcher.group(11));
+        int unknown = Integer.parseInt(matcher.group(15));
+        return new ProDomRawMatch(sequenceIdentifier, model, this.getSignatureLibraryRelease(),
+                sequenceStart, sequenceEnd, score);
     }
 
 
--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/prosite/PrositeMatchParser.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/prosite/PrositeMatchParser.java
@@ -1,13 +1,15 @@
 package uk.ac.ebi.interpro.scan.io.match.prosite;
 
 import org.apache.log4j.Logger;
-import uk.ac.ebi.interpro.scan.model.raw.PfScanRawMatch;
-import uk.ac.ebi.interpro.scan.model.raw.RawProtein;
+import org.springframework.beans.factory.annotation.Required;
+import uk.ac.ebi.interpro.scan.io.match.AbstractLineMatchParser;
+import uk.ac.ebi.interpro.scan.model.PatternScanMatch;
+import uk.ac.ebi.interpro.scan.model.SignatureLibrary;
+import uk.ac.ebi.interpro.scan.model.raw.*;
+import uk.ac.ebi.interpro.scan.model.raw.alignment.CigarAlignmentEncoder;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Parses the GFF output format from ps_scan.pl
@@ -39,13 +41,113 @@ import java.util.Set;
  * @version $Id$
  * @since 1.0-SNAPSHOT
  */
-public class PrositeMatchParser<T extends PfScanRawMatch> {
+public class PrositeMatchParser extends AbstractLineMatchParser<PfScanRawMatch> {
 
     private static final Logger LOGGER = Logger.getLogger(PrositeMatchParser.class.getName());
 
-    public Set<RawProtein<T>> parse(InputStream is, String fileName, String signatureReleaseLibrary) throws IOException {
-        Set<RawProtein<T>> rawMatches = new HashSet<RawProtein<T>>();
+    private CigarAlignmentEncoder cigarEncoder;
 
-        return rawMatches;
+    protected PrositeMatchParser(SignatureLibrary signatureLibrary, String signatureLibraryRelease) {
+        super(signatureLibrary, signatureLibraryRelease);
+    }
+
+
+    @Required
+    public void setCigarEncoder(CigarAlignmentEncoder cigarEncoder) {
+        this.cigarEncoder = cigarEncoder;
+    }
+
+    /**
+     * Returns {@link uk.ac.ebi.interpro.scan.model.raw.RawMatch} instance using values from parameters.
+     *
+     * @param line Line read from input file.   @return {@link uk.ac.ebi.interpro.scan.model.raw.RawMatch} instance using values from parameters
+     */
+    @Override
+    protected PfScanRawMatch createMatch(String line) {
+        final String[] splitLine = line.split("\\t");
+        if (splitLine.length < 9) return null;
+        final String sequenceId = splitLine[0].trim();
+        final String model = splitLine[2].trim();
+        final int seqStart = Integer.parseInt(splitLine[3].trim());
+        int seqEnd = Integer.parseInt(splitLine[4].trim());
+        Double score = null;
+        final String scoreString = splitLine[5].trim();
+        if (scoreString.length() > 0 && !".".equals(scoreString)) {
+            score = new Double(scoreString);
+        }
+        Map<String, String> gffAttributes = extractAttributes(splitLine[8]);
+        final String cigarAlign = cigarEncoder.encode(gffAttributes.get("Sequence"));
+        final ProfileScanRawMatch.Level profileLevel = ProfileScanRawMatch.Level.byLevelString(gffAttributes.get("Level"));
+        final String patternLevelTag = gffAttributes.get("LevelTag");
+        final PatternScanMatch.PatternScanLocation.Level patternLevel = PatternScanMatch.PatternScanLocation.Level.getLevelByTag(patternLevelTag);
+
+        switch (this.getSignatureLibrary()) {
+            case HAMAP:
+                return new HamapRawMatch(
+                        sequenceId,
+                        model,
+                        this.getSignatureLibraryRelease(),
+                        seqStart,
+                        seqEnd,
+                        cigarAlign,
+                        score,
+                        profileLevel
+                );
+            case PROSITE_PROFILES:
+                return new ProSiteProfileRawMatch(
+                        sequenceId,
+                        model,
+                        this.getSignatureLibraryRelease(),
+                        seqStart,
+                        seqEnd,
+                        cigarAlign,
+                        score,
+                        profileLevel
+                );
+            case PROSITE_PATTERNS:
+                return new ProSitePatternRawMatch(
+                        sequenceId,
+                        model,
+                        this.getSignatureLibraryRelease(),
+                        seqStart,
+                        seqEnd,
+                        cigarAlign,
+                        patternLevel
+                );
+
+            default:
+                throw new IllegalStateException("The PrositeMatchParser currently only handles HAMAP and Prosite output.");
+        }
+    }
+
+    /**
+     * Helper method to grab the attributes from the end of the gff line.
+     * Pretty horrible format, hence pretty horrible code!
+     *
+     * @param attributes semi-colon separated attributes to be parsed into key-value pairs.
+     * @return a Map of key-value pairs extraced from the attributes String.
+     */
+    private Map<String, String> extractAttributes(String attributes) {
+        Map<String, String> attributeMap = new HashMap<String, String>();
+        String[] parts = attributes.split(";");
+        for (String part : parts) {
+            if (part != null) {
+                final String trimmedPart = part.trim();
+                final int firstSpaceIndex = trimmedPart.indexOf(' ');
+                String key = trimmedPart.substring(0, firstSpaceIndex);
+                String value = trimmedPart.substring(firstSpaceIndex + 1).trim();
+                if (value.startsWith("\"")) {
+                    value = value.substring(1);
+                }
+                if (value.endsWith("\"")) {
+                    value = value.substring(0, value.length() - 1);
+                }
+                if (key != null && value != null) {
+                    attributeMap.put(key, value);
+                }
+            }
+        }
+
+        return attributeMap;
     }
 }
