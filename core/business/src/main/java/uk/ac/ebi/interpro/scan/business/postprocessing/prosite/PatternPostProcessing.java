728d4e86f96a189934b0f1bc697e10b8f0367bb2
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-08-02 17:05:12 +0000

    Prosite Pattern & Profile post processing implemented.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@762 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/business/src/main/java/uk/ac/ebi/interpro/scan/business/postprocessing/prosite/PatternPostProcessing.java
@@ -0,0 +1,64 @@
+package uk.ac.ebi.interpro.scan.business.postprocessing.prosite;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+import uk.ac.ebi.interpro.scan.model.PatternScanMatch;
+import uk.ac.ebi.interpro.scan.model.raw.ProSitePatternRawMatch;
+import uk.ac.ebi.interpro.scan.model.raw.RawProtein;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Performs the simple post-processing required for Prosite Pattern based member
+ * databases - allows filtering based upon allowed levels (of match)
+ *
+ * @author Phil Jones, EMBL-EBI
+ * @version $Id$
+ * @since 1.0
+ */
+
+public class PatternPostProcessing implements Serializable {
+
+    private static final Logger LOGGER = Logger.getLogger(PatternPostProcessing.class.getName());
+
+    private List<PatternScanMatch.PatternScanLocation.Level> passLevels;
+
+    @Required
+    public void setPassLevels(List<PatternScanMatch.PatternScanLocation.Level> passLevels) {
+        this.passLevels = passLevels;
+    }
+
+    /**
+     * Performs post processing for Prosite Patterns.
+     * Very simple filter - if the Level of the match is included in the List of
+     * acceptable levels (passed in be the Bean setter) then the match passes.
+     *
+     * @param proteinIdToRawMatchMap being the Collection of unfiltered matches.
+     * @return a Map of filtered matches
+     */
+    public Map<String, RawProtein<ProSitePatternRawMatch>> process(Map<String, RawProtein<ProSitePatternRawMatch>> proteinIdToRawMatchMap) {
+        if (passLevels == null) {
+            throw new IllegalStateException("The ProfilePostProcessing class has not been correctly initialised. A List of acceptable Levels must be passed in.");
+        }
+        if (passLevels.size() == 0) {
+            LOGGER.warn("The ProfilePostProcessing class has been initialised such that NO matches will pass. (The list of acceptable levels is empty).");
+        }
+        Map<String, RawProtein<ProSitePatternRawMatch>> filteredMatches = new HashMap<String, RawProtein<ProSitePatternRawMatch>>();
+        for (String candidateProteinId : proteinIdToRawMatchMap.keySet()) {
+            RawProtein<ProSitePatternRawMatch> candidateRawProtein = proteinIdToRawMatchMap.get(candidateProteinId);
+            RawProtein<ProSitePatternRawMatch> filteredProtein = new RawProtein<ProSitePatternRawMatch>(candidateRawProtein.getProteinIdentifier());
+            for (ProSitePatternRawMatch rawMatch : candidateRawProtein.getMatches()) {
+                if (passLevels.contains(rawMatch.getPatternLevel())) {
+                    filteredProtein.addMatch(rawMatch);
+                }
+            }
+            if (filteredProtein.getMatches() != null && filteredProtein.getMatches().size() > 0) {
+                filteredMatches.put(candidateProteinId, filteredProtein);
+            }
+        }
+        return filteredMatches;
+    }
+}
