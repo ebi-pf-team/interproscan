a5c7e304c8fadb8c245493deca216e4cd603ba27
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-09-17 09:23:07 +0000

    TIGRfam "post processing" (no-op) completed.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@789 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/TigrFamFilteredMatchDAOImpl.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+import uk.ac.ebi.interpro.scan.model.raw.TigrFamRawMatch;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: maslen
+ * Date: Sep 16, 2010
+ */
+
+public class TigrFamFilteredMatchDAOImpl extends Hmmer2FilteredMatchDAO<TigrFamRawMatch> {
+
+}
