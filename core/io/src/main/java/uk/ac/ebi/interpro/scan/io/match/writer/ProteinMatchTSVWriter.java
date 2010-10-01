e80028acffbd258990827d74fc0a328d6e84d0ff
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-10-01 12:38:07 +0000

    Implemented necessary IO to parse InterPro Entry mappings and GO mappings from the InterPro XML format.  Also added a generic object marshaller / unmarshaller.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@793 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/writer/ProteinMatchTSVWriter.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/writer/ProteinMatchTSVWriter.java
@@ -1,63 +1,126 @@
 package uk.ac.ebi.interpro.scan.io.match.writer;
 
 import uk.ac.ebi.interpro.scan.io.TSVWriter;
+import uk.ac.ebi.interpro.scan.io.unmarshal.xml.interpro.GoTerm;
+import uk.ac.ebi.interpro.scan.io.unmarshal.xml.interpro.InterProEntry;
+import uk.ac.ebi.interpro.scan.io.unmarshal.xml.interpro.SignatureLibraryIntegratedMethods;
 import uk.ac.ebi.interpro.scan.model.*;
-import uk.ac.ebi.interpro.scan.model.HmmerLocation;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Write matches as output for InterProScan user.
  */
 public class ProteinMatchTSVWriter implements ProteinWriter {
 
-    
+
     private TSVWriter tsvWriter;
 
-    DateFormat dmyFormat =new SimpleDateFormat("dd-MM-yyyy");
+    DateFormat dmyFormat = new SimpleDateFormat("dd-MM-yyyy");
+    private boolean mapToInterProEntries;
+    private boolean mapToGO;
+    private Map<SignatureLibrary, SignatureLibraryIntegratedMethods> interProGoMapping;
 
     public ProteinMatchTSVWriter(File file) throws IOException {
-        tsvWriter=new TSVWriter(new BufferedWriter(new FileWriter(file)));
+        tsvWriter = new TSVWriter(new BufferedWriter(new FileWriter(file)));
     }
 
 
     public void write(Protein protein) throws IOException {
 
         String proteinAc = makeProteinAc(protein);
-        int length=protein.getSequence().length();
-        String md5=protein.getMd5();
-        String date= dmyFormat.format(new Date());
+        int length = protein.getSequence().length();
+        String md5 = protein.getMd5();
+        String date = dmyFormat.format(new Date());
 
         Set<Match> matches = protein.getMatches();
         for (Match match : matches) {
-            String signatureAc=match.getSignature().getAccession();
-            String analysis=match.getSignature().getSignatureLibraryRelease().getLibrary().getName();
-            String description=match.getSignature().getDescription();
+            final String signatureAc = match.getSignature().getAccession();
+            final SignatureLibrary signatureLibrary = match.getSignature().getSignatureLibraryRelease().getLibrary();
+            final String analysis = signatureLibrary.getName();
+            final String description = match.getSignature().getDescription();
 
             Set<Location> locations = match.getLocations();
             for (Location location : locations) {
-                String score="-";
-                String status="T";
+                String score = "-";
+                String status = "T";
 
                 if (location instanceof HmmerLocation) {
-                    score=""+((HmmerLocation) location).getEvalue();
+                    score = Double.toString(((HmmerLocation) location).getEvalue());
                 }
 
-                tsvWriter.write(proteinAc,md5,""+length,analysis,signatureAc,description,""+location.getStart(),""+location.getEnd(),score,status,date);
+                final List<String> mappingFields = new ArrayList<String>();
+                mappingFields.add(proteinAc);
+                mappingFields.add(md5);
+                mappingFields.add(Integer.toString(length));
+                mappingFields.add(analysis);
+                mappingFields.add(signatureAc);
+                mappingFields.add(description);
+                mappingFields.add(Integer.toString(location.getStart()));
+                mappingFields.add(Integer.toString(location.getEnd()));
+                mappingFields.add(score);
+                mappingFields.add(status);
+                mappingFields.add(date);
+
+
+                if (mapToInterProEntries) {
+                    final SignatureLibraryIntegratedMethods methodMappings = interProGoMapping.get(signatureLibrary);
+                    if (methodMappings != null) {
+                        final InterProEntry entry = methodMappings.getEntryByMethodAccession(signatureAc);
+                        if (entry != null) {
+                            mappingFields.add(entry.getEntryAccession());
+                            mappingFields.add(entry.getDescription());
+                            if (mapToGO && entry.getGoTerms().size() > 0) {
+                                StringBuffer buf = new StringBuffer();
+                                for (GoTerm goTerm : entry.getGoTerms()) {
+                                    if (buf.length() > 0) {
+                                        buf.append(", ");
+                                    }
+                                    buf.append(goTerm.getRoot().getRootName())
+                                            .append(':')
+                                            .append(goTerm.getTermName())
+                                            .append(" (")
+                                            .append(goTerm.getAccession())
+                                            .append(")");
+                                }
+                                mappingFields.add(buf.toString());
+                            }
+                        }
+                    }
+                }
+
+                tsvWriter.write(mappingFields);
             }
         }
 
     }
 
+    @Override
+    public void setMapToInterProEntries(boolean mapToInterProEntries) {
+        this.mapToInterProEntries = mapToInterProEntries;
+    }
+
+    @Override
+    public void setMapToGo(boolean mapToGO) {
+        this.mapToGO = mapToGO;
+    }
+
+    @Override
+    public void setInterProGoMapping(Map<SignatureLibrary, SignatureLibraryIntegratedMethods> interProGoMapping) {
+        this.interProGoMapping = interProGoMapping;
+    }
+
     private String makeProteinAc(Protein protein) {
-        StringBuilder proteinXRef=new StringBuilder();
+        StringBuilder proteinXRef = new StringBuilder();
         Set<Xref> crossReferences = protein.getCrossReferences();
         for (Xref crossReference : crossReferences) {
-            if (proteinXRef.length()>0) proteinXRef.append("|");
+            if (proteinXRef.length() > 0) proteinXRef.append("|");
             proteinXRef.append(crossReference.getIdentifier());
         }
         return proteinXRef.toString();
--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/writer/ZipWriter.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/writer/ZipWriter.java
@@ -1,32 +1,37 @@
 package uk.ac.ebi.interpro.scan.io.match.writer;
 
 import org.apache.commons.lang.SerializationUtils;
+import uk.ac.ebi.interpro.scan.io.unmarshal.xml.interpro.SignatureLibraryIntegratedMethods;
 import uk.ac.ebi.interpro.scan.model.Protein;
+import uk.ac.ebi.interpro.scan.model.SignatureLibrary;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
-* Created by IntelliJ IDEA.
-* User: dbinns
-* Date: 21-Jun-2010
-* Time: 17:09:13
-* To change this template use File | Settings | File Templates.
-*/
+ * Created by IntelliJ IDEA.
+ * User: dbinns
+ * Date: 21-Jun-2010
+ * Time: 17:09:13
+ * To change this template use File | Settings | File Templates.
+ */
 public class ZipWriter implements ProteinWriter {
 
     ZipOutputStream os;
 
+    //TODO - This ZipWriter serializes out the Protein object.  This currently gives no opportunity to include mappings
+    //TODO to InterPro or GO - ....
+    private boolean mapToInterProEntries;
+    private boolean mapToGO;
+    private Map<SignatureLibrary, SignatureLibraryIntegratedMethods> interProGoMapping;
+
     public ZipWriter(File file) throws FileNotFoundException {
-
-
-
         os = new ZipOutputStream(new FileOutputStream(file));
-
     }
 
     @Override
@@ -34,6 +39,21 @@ public class ZipWriter implements ProteinWriter {
         os.putNextEntry(new ZipEntry(protein.getMd5()));
         os.write(SerializationUtils.serialize(protein));
         os.closeEntry();
+    }
+
+    @Override
+    public void setMapToInterProEntries(boolean mapToInterProEntries) {
+        this.mapToInterProEntries = mapToInterProEntries;
+    }
+
+    @Override
+    public void setMapToGo(boolean mapToGO) {
+        this.mapToGO = mapToGO;
+    }
+
+    @Override
+    public void setInterProGoMapping(Map<SignatureLibrary, SignatureLibraryIntegratedMethods> interProGoMapping) {
+        this.interProGoMapping = interProGoMapping;
     }
 
     @Override
