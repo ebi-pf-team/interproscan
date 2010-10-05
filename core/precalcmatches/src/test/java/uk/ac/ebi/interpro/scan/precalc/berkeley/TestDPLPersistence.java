e094b70767e803c5187a117e1a78a32dd39c5d1b
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-10-05 17:28:08 +0000

    New module for lookup / storage of match information using BerkeleyDB JE.  Currently in testing and development - looking at performance and scalability at the moment.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@802 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/precalcmatches/src/test/java/uk/ac/ebi/interpro/scan/precalc/berkeley/TestDPLPersistence.java
@@ -0,0 +1,149 @@
+package uk.ac.ebi.interpro.scan.precalc.berkeley;
+
+import com.sleepycat.je.DatabaseException;
+import com.sleepycat.je.Environment;
+import com.sleepycat.je.EnvironmentConfig;
+import com.sleepycat.persist.*;
+import uk.ac.ebi.interpro.scan.precalc.berkeley.model.BK_Location;
+import uk.ac.ebi.interpro.scan.precalc.berkeley.model.BK_Match;
+
+import java.io.File;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * TODO: Description
+ *
+ * @author Phil Jones
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class TestDPLPersistence {
+
+
+    private static final int MATCH_NUMBER = 1000 * 1000 * 182;
+
+    private static final int SELECT_INTERVAL = 100 * 1000;
+
+    private static final int MATCHES_PER_PROTEIN = 4;
+
+    private static final NumberFormat format = DecimalFormat.getInstance();
+
+    public static void main(String[] args) {
+
+
+        Environment myEnv = null;
+        EntityStore store = null;
+        EntityCursor<BK_Match> matchCursor = null;
+
+        try {
+            EnvironmentConfig myEnvConfig = new EnvironmentConfig();
+            StoreConfig storeConfig = new StoreConfig();
+
+            myEnvConfig.setAllowCreate(true);
+            storeConfig.setAllowCreate(true);
+            storeConfig.setTransactional(false);
+
+            File file = new File("./target/" + UUID.randomUUID().toString());
+            file.mkdirs();
+            // Open the environment and entity store
+            myEnv = new Environment(file, myEnvConfig);
+            store = new EntityStore(myEnv, "EntityStore", storeConfig);
+
+
+            PrimaryIndex<Long, BK_Match> primIDX = store.getPrimaryIndex(Long.class, BK_Match.class);
+            SecondaryIndex<String, Long, BK_Match> secIDX = store.getSecondaryIndex(primIDX, String.class, "proteinMD5");
+
+            String proteinMD5 = UUID.randomUUID().toString();
+
+            // Need to pick a random accession.
+
+            final Random randomizer = new Random();
+            int randomValue = randomizer.nextInt(SELECT_INTERVAL);
+            String randomlySelectedProteinMD5 = null;
+            // Create some random Matches
+            for (int i = 0; i < MATCH_NUMBER; i++) {
+                if (i % MATCHES_PER_PROTEIN == 0) {
+                    proteinMD5 = UUID.randomUUID().toString();
+//                    System.out.println(proteinMD5);
+                }
+                if (i == randomValue) {
+                    randomlySelectedProteinMD5 = proteinMD5;
+                }
+                BK_Match match = new BK_Match();
+                match.setProteinMD5(proteinMD5);
+                match.setSignatureAccession(UUID.randomUUID().toString().substring(0, 6));
+                match.setSequenceEValue(randomizer.nextDouble());
+                match.setSequenceScore(randomizer.nextDouble());
+                for (int j = 0; j < 4; j++) {
+                    BK_Location location = new BK_Location();
+                    if (i % 50 == 0) {
+                        location.setCigarAlignment(UUID.randomUUID().toString());
+                    }
+                    if (i % 2 == 0) {
+                        location.seteValue(randomizer.nextDouble());
+                    }
+                    Random rand = new Random();
+                    location.setStart(rand.nextInt());
+                    location.setEnd(rand.nextInt());
+                    match.addLocation(location);
+
+                }
+                primIDX.put(match);
+
+                if (i % SELECT_INTERVAL == 0 && i > 0) {
+                    System.out.println("Following storage of " + i + " matches:");
+                    long startTimeMilli = System.currentTimeMillis();
+
+                    matchCursor = secIDX.entities(randomlySelectedProteinMD5, true, randomlySelectedProteinMD5, true);
+                    BK_Match currentMatch;
+
+                    while ((currentMatch = matchCursor.next()) != null) {
+                        System.out.println("currentMatch = " + currentMatch);
+                    }
+
+                    long endTimeMilli = System.currentTimeMillis();
+                    System.out.println("Time for complete query & iteration: " + (endTimeMilli - startTimeMilli) + " ms.");
+                    matchCursor.close();
+                    // Pick a random protein out of the next lot being inserted.
+                    randomValue = randomizer.nextInt(SELECT_INTERVAL) + i;
+                }
+            }
+
+//            System.out.println(randomlySelectedProteinMD5 + " key found? " + secIDX.contains(randomlySelectedProteinMD5));
+
+//            System.out.println("secIDX.count() = " + secIDX.count());
+
+
+        } catch (DatabaseException dbe) {
+            System.err.println("Error opening environment and store: " +
+                    dbe.toString());
+            System.exit(-1);
+        }
+        finally {
+            if (store != null) {
+                try {
+                    store.close();
+                } catch (DatabaseException dbe) {
+                    System.err.println("Error closing store: " +
+                            dbe.toString());
+                    System.exit(-1);
+                }
+            }
+
+            if (myEnv != null) {
+                try {
+                    // Finally, close environment.
+                    myEnv.close();
+                } catch (DatabaseException dbe) {
+                    System.err.println("Error closing MyDbEnv: " +
+                            dbe.toString());
+                    System.exit(-1);
+                }
+            }
+        }
+
+    }
+}
