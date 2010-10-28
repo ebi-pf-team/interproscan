b243e5a496916fb0493fe681c0a2e5009153c7cb
philip.j.r.jones <philip.j.r.jones@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2010-10-28 10:35:03 +0000

    Fixed deliberately broken code.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@823 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/io/src/test/java/uk/ac/ebi/interpro/scan/io/unmarshal/xml/interpro/InterProXMLUnmarshallerTest.java
+++ core/io/src/test/java/uk/ac/ebi/interpro/scan/io/unmarshal/xml/interpro/InterProXMLUnmarshallerTest.java
@@ -29,7 +29,7 @@ import java.util.zip.ZipInputStream;
 public class InterProXMLUnmarshallerTest {
 
     @Resource
-    private org.springframework.core.io.PResource interproXmlFile;
+    private org.springframework.core.io.Resource interproXmlFile;
 
     @Resource
     private InterProXMLUnmarshaller unmarshaller;
