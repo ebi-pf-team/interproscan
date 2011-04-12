452d6c85c8a5f9569c620fda0b511faae826b9d0
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-04-12 11:28:14 +0000

    PIRSF member database implementation
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1026 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PIRSFHmmer2RawMatch.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PIRSFHmmer2RawMatch.java
@@ -9,22 +9,23 @@ import javax.persistence.Entity;
  * Specific raw match class for PIRSF.
  * Only required to ensure it gets it's own table.
  *
- * @author Phil Jones
+ * @author Matthew Fraser
+ * @author Maxim Scheremetjew
  * @version $Id$
  * @since 1.0-SNAPSHOT
  */
 @Entity
 @javax.persistence.Table(name = PIRSFHmmer2RawMatch.TABLE_NAME)
 @org.hibernate.annotations.Table(appliesTo = PIRSFHmmer2RawMatch.TABLE_NAME, indexes = {
-        @Index(name = "PIRSF_RW_SEQ_IDX", columnNames = {RawMatch.COL_NAME_SEQUENCE_IDENTIFIER}),
-        @Index(name = "PIRSF_RW_NUM_SEQ_IDX", columnNames = {RawMatch.COL_NAME_NUMERIC_SEQUENCE_ID}),
-        @Index(name = "PIRSF_RW_MODEL_IDX", columnNames = {RawMatch.COL_NAME_MODEL_ID}),
-        @Index(name = "PIRSF_RW_SIGLIB_IDX", columnNames = {RawMatch.COL_NAME_SIGNATURE_LIBRARY}),
-        @Index(name = "PIRSF_RW_SIGLIB_REL_IDX", columnNames = {RawMatch.COL_NAME_SIGNATURE_LIBRARY_RELEASE})
+        @Index(name = "PIRSF2_RW_SEQ_IDX", columnNames = {RawMatch.COL_NAME_SEQUENCE_IDENTIFIER}),
+        @Index(name = "PIRSF2_RW_NUM_SEQ_IDX", columnNames = {RawMatch.COL_NAME_NUMERIC_SEQUENCE_ID}),
+        @Index(name = "PIRSF2_RW_MODEL_IDX", columnNames = {RawMatch.COL_NAME_MODEL_ID}),
+        @Index(name = "PIRSF2_RW_SIGLIB_IDX", columnNames = {RawMatch.COL_NAME_SIGNATURE_LIBRARY}),
+        @Index(name = "PIRSF2_RW_SIGLIB_REL_IDX", columnNames = {RawMatch.COL_NAME_SIGNATURE_LIBRARY_RELEASE})
 })
 public class PIRSFHmmer2RawMatch extends Hmmer2RawMatch {
 
-    public static final String TABLE_NAME = "PIRSF_H2_RAW_MATCH";
+    public static final String TABLE_NAME = "PIRSF_HMMER2_RAW_MATCH";
 
     protected PIRSFHmmer2RawMatch() {
 
