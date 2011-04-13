772afbadfcef0feb109a576e6afc02136dfa8ac2
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-04-13 08:45:19 +0000

    PIRSF member database implementation
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1032 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/pirsf/ParsePIRSFHmmpfamOutputStep.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/pirsf/ParsePIRSFHmmpfamOutputStep.java
@@ -1,16 +1,16 @@
 package uk.ac.ebi.interpro.scan.management.model.implementations.pirsf;
 
 import uk.ac.ebi.interpro.scan.management.model.implementations.AbstractParseHmmpfamOutputStep;
-import uk.ac.ebi.interpro.scan.model.raw.TigrFamHmmer2RawMatch;
+import uk.ac.ebi.interpro.scan.model.raw.PIRSFHmmer2RawMatch;
 
 /**
- * This class parses and persists the output from hmmpfam for TIGRFAM
+ * This class parses and persists the output from hmmpfam for PIRSF.
  *
  * @author Matthew Fraser
  * @author Maxim Scheremetjew
  * @version $Id$
  * @since 1.0-SNAPSHOT
  */
-public class ParsePIRSFHmmpfamOutputStep extends AbstractParseHmmpfamOutputStep<TigrFamHmmer2RawMatch> {
+public class ParsePIRSFHmmpfamOutputStep extends AbstractParseHmmpfamOutputStep<PIRSFHmmer2RawMatch> {
 
 }
