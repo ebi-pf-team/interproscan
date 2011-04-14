0b41c3e25aa1d11cee82fd397ac2a05515dae2d0
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-04-14 15:45:29 +0000

    PIRSF member database implementation
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1037 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/business/src/main/java/uk/ac/ebi/interpro/scan/business/postprocessing/pirsf/PirsfPostProcessing.java
+++ core/business/src/main/java/uk/ac/ebi/interpro/scan/business/postprocessing/pirsf/PirsfPostProcessing.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.core.io.Resource;
 import uk.ac.ebi.interpro.scan.io.pirsf.PirsfDatFileParser;
 import uk.ac.ebi.interpro.scan.io.pirsf.PirsfDatRecord;
+import uk.ac.ebi.interpro.scan.io.pirsf.SfTbFileParser;
 import uk.ac.ebi.interpro.scan.model.raw.PIRSFHmmer2RawMatch;
 import uk.ac.ebi.interpro.scan.model.raw.RawProtein;
 
@@ -15,7 +16,11 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * PIRSF post-processing -> also requires a pirsf.dat file.
+ * PIRSF post-processing.
+ *
+ * Also requires extra information from:
+ * - pirsf.dat file.
+ * - sf.tb file.
  *
  * Algorithm:
  *
@@ -25,10 +30,10 @@ public class PirsfPostProcessing implements Serializable {
     private static final Logger LOGGER = Logger.getLogger(PirsfPostProcessing.class.getName());
 
     private PirsfDatFileParser pirsfDatFileParser;
-
     private Resource pirsfDatFileResource;
 
-    private Map<String, PirsfDatRecord> pirsfDatRecordMap;
+    private SfTbFileParser sfTbFileParser;
+    private Resource sfTbFileResource;
 
     private Map<String, RawProtein<PIRSFHmmer2RawMatch>> allFilteredMatches = new HashMap<String, RawProtein<PIRSFHmmer2RawMatch>>();
 
@@ -43,6 +48,16 @@ public class PirsfPostProcessing implements Serializable {
         this.pirsfDatFileResource = pirsfDatFileResource;
     }
 
+    @Required
+    public void setSfTbFileParser(SfTbFileParser sfTbFileParser) {
+        this.sfTbFileParser = sfTbFileParser;
+    }
+
+    @Required
+    public void setSfTbFileResource(Resource sfTbFileResource) {
+        this.sfTbFileResource = sfTbFileResource;
+    }
+
     /**
      * Perform post processing.
      *
@@ -52,8 +67,11 @@ public class PirsfPostProcessing implements Serializable {
      */
     public Map<String, RawProtein<PIRSFHmmer2RawMatch>> process(Set<RawProtein<PIRSFHmmer2RawMatch>> rawMatchesSet) throws IOException {
 
-        // Read in extra information from pirsf.dat file
-        pirsfDatRecordMap = pirsfDatFileParser.parse(pirsfDatFileResource);
+        // Read in pirsf.dat file
+        Map<String, PirsfDatRecord> pirsfDatRecordMap = pirsfDatFileParser.parse(pirsfDatFileResource);
+
+        // Read in sf.tb file
+        Map<String, Integer> sfTbMap = sfTbFileParser.parse(sfTbFileResource);
 
         // Loop through the proteins and see if the matches need to be excluded (filtered) or not
         for (RawProtein<PIRSFHmmer2RawMatch> rawMatch : rawMatchesSet) {
@@ -67,6 +85,13 @@ public class PirsfPostProcessing implements Serializable {
                                 Map<String, RawProtein<PIRSFHmmer2RawMatch>> filteredMatches) {
 
         for (PIRSFHmmer2RawMatch pirsfRawMatch : matchRawProtein.getMatches()) {
+
+            String modelId = pirsfRawMatch.getModelId();
+            int seqF = pirsfRawMatch.getLocationStart();
+            int seqT = pirsfRawMatch.getLocationEnd();
+            double score = pirsfRawMatch.getLocationScore();
+            double evalue = pirsfRawMatch.getLocationEvalue();
+
             String id = pirsfRawMatch.getSequenceIdentifier();
             RawProtein<PIRSFHmmer2RawMatch> p;
             if (filteredMatches.containsKey(id)) {
