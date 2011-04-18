7e149e132c9d070b6a6c13665c7235cd32f6a3c7
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-04-18 16:19:24 +0000

    PIRSF member database implementation
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1041 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/business/src/main/java/uk/ac/ebi/interpro/scan/business/postprocessing/pirsf/PirsfPostProcessing.java
+++ core/business/src/main/java/uk/ac/ebi/interpro/scan/business/postprocessing/pirsf/PirsfPostProcessing.java
@@ -63,7 +63,6 @@ public class PirsfPostProcessing implements Serializable {
 
     private Map<String, RawProtein<PIRSFHmmer2RawMatch>> allFilteredMatches = new HashMap<String, RawProtein<PIRSFHmmer2RawMatch>>();
 
-
     @Required
     public void setPirsfDatFileParser(PirsfDatFileParser pirsfDatFileParser) {
         this.pirsfDatFileParser = pirsfDatFileParser;
@@ -118,16 +117,14 @@ public class PirsfPostProcessing implements Serializable {
                                 Map<String, PirsfDatRecord> pirsfDatRecordMap,
                                 long proteinLength) {
 
+        // Used to find the model Id with the smallest mean e-value for this protein
+        Double minMeanEvalue = null;
+        String modelIdWithMinMeanEvalue = null;
+
         for (PIRSFHmmer2RawMatch pirsfRawMatch : matchRawProtein.getMatches()) {
 
             String sequenceId = pirsfRawMatch.getSequenceIdentifier();
-
-            // Data from HMMER2 raw output
             String modelId = pirsfRawMatch.getModelId();
-            int seqFrom = pirsfRawMatch.getLocationStart();
-            int seqTo = pirsfRawMatch.getLocationEnd();
-            double score = pirsfRawMatch.getLocationScore();
-            double evalue = pirsfRawMatch.getLocationEvalue();
 
             PirsfDatRecord pirsfDatRecord = pirsfDatRecordMap.get(modelId);
             if (pirsfDatRecord == null) {
@@ -136,54 +133,92 @@ public class PirsfPostProcessing implements Serializable {
                 continue;
             }
 
-            // Perform first filtering calculations
-            double overlap = (seqTo - seqFrom + 1) / proteinLength;
-            double meanSeqLen = pirsfDatRecord.getMeanSeqLen();
-            double minScore = pirsfDatRecord.getMinScore();
-            double stdDevSeqLen = pirsfDatRecord.getStdDevSeqLen();
-            double lenDifference = Math.abs(proteinLength - meanSeqLen);
-            if (overlap >= 0.8 && score >= minScore && (lenDifference < (3.5 * stdDevSeqLen) || lenDifference < 50)) {
-                // Match is still OK for now, not filtered out yet
-                if (LOGGER.isTraceEnabled()) {
-                    LOGGER.trace("Raw match passes initial filtering, still in with a chance: " + pirsfRawMatch);
-                }
-
-            }
-            else {
-                // This match won't be making it into the filtered list of matches, so now look at the next
-                if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("Raw match filtered out: " + pirsfRawMatch);
-                }
-                continue;
+            // Perform first filtering check
+            if (!doesPassInitialFilter(proteinLength, pirsfRawMatch, pirsfDatRecord)) {
+                continue; // Failed so move on to the next raw match
             }
 
-            // TODO Find the model Id with the smallest mean e-value in this protein
 
-            /*
-             * TODO Run blast (if necessary).
-             *
-             * NOTE 15/04/2011:
-             *
-             * Onion did this:
-             * /ebi/extserv/bin/ncbi-blast-2.2.6/blastall -p blastp -F F -e 0.0005 -b 300 -v 300 -d /ebi/sp/pro1/interpro/data/members/pirsf/274/sf.seq -m 8 -i  SEQUENCE FILE > OUTPUT_FILE
-             *
-             * PIRSF Perl script (pirsf.pl) did this:
-             * $blastall -a $cpu -p blastp -F F -e 0.0001 -b 10000 -d $sfseq -i $infile -m 8
-             *
-             * TODO Which is right?
-             */
-
-
-            RawProtein<PIRSFHmmer2RawMatch> p;
-            if (filteredMatches.containsKey(sequenceId)) {
-                p = filteredMatches.get(sequenceId);
-            } else {
-                p = new RawProtein<PIRSFHmmer2RawMatch>(sequenceId);
-                filteredMatches.put(sequenceId, p);
+            double evalue = pirsfRawMatch.getLocationEvalue();
+            if ((minMeanEvalue == null && modelIdWithMinMeanEvalue == null) || (evalue < minMeanEvalue)) {
+                // Can reduce the minimum mean e-value for this protein
+                minMeanEvalue = evalue;
+                modelIdWithMinMeanEvalue = modelId;
             }
-            p.addMatch(pirsfRawMatch);
+
         }
 
+        // Is blast required for our chosen model Id?
+        if (modelIdWithMinMeanEvalue != null) {
+            PirsfDatRecord pirsfDatRecord = pirsfDatRecordMap.get(modelIdWithMinMeanEvalue);
+            if (pirsfDatRecord == null) {
+                LOGGER.warn("Model Id " + modelIdWithMinMeanEvalue + " could not be found in pirsf.dat, rejected");
+                return;
+            }
+            if (pirsfDatRecord.isBlastReqd()) {
+               /*
+                * TODO Run blast.
+                *
+                * NOTE 15/04/2011:
+                *
+                * Onion did this:
+                * /ebi/extserv/bin/ncbi-blast-2.2.6/blastall -p blastp -F F -e 0.0005 -b 300 -v 300 -d /ebi/sp/pro1/interpro/data/members/pirsf/274/sf.seq -m 8 -i  SEQUENCE FILE > OUTPUT_FILE
+                *
+                * PIRSF Perl script (pirsf.pl) did this:
+                * $blastall -a $cpu -p blastp -F F -e 0.0001 -b 10000 -d $sfseq -i $infile -m 8
+                *
+                * TODO Which is right?
+                */
+
+            }
+        }
+
+//            RawProtein<PIRSFHmmer2RawMatch> p;
+//            if (filteredMatches.containsKey(sequenceId)) {
+//                p = filteredMatches.get(sequenceId);
+//            } else {
+//                p = new RawProtein<PIRSFHmmer2RawMatch>(sequenceId);
+//                filteredMatches.put(sequenceId, p);
+//            }
+//            p.addMatch(pirsfRawMatch);
+
+    }
+
+    /**
+     * Initial filter checks for a raw match:
+     * - Check that the model covers > 80% of sequence
+     * - Score is greater than cutoff
+     * - EITHER difference between model length and protein length is less than 3.5 times the standard deviation
+     *   OR is less than 50 amino acids
+     *
+     * @param proteinLength Length of the sequence
+     * @param pirsfRawMatch Raw match data
+     * @param pirsfDatRecord Data from pirsf.dat
+     * @return true if raw match passed the filter checks, otherwise false
+     */
+    private boolean doesPassInitialFilter(long proteinLength, PIRSFHmmer2RawMatch pirsfRawMatch, PirsfDatRecord pirsfDatRecord) {
+        // Data from HMMER2 raw output
+        int seqFrom = pirsfRawMatch.getLocationStart();
+        int seqTo = pirsfRawMatch.getLocationEnd();
+        double score = pirsfRawMatch.getLocationScore();
+
+        // Data from pirsf.dat file
+        double meanSeqLen = pirsfDatRecord.getMeanSeqLen();
+        double minScore = pirsfDatRecord.getMinScore();
+        double stdDevSeqLen = pirsfDatRecord.getStdDevSeqLen();
+
+        // Perform calculations
+        double overlap = (seqTo - seqFrom + 1) / proteinLength;
+        double lenDifference = Math.abs(proteinLength - meanSeqLen);
+        if (overlap >= 0.8 && score >= minScore && (lenDifference < (3.5 * stdDevSeqLen) || lenDifference < 50)) {
+            return true;
+        }
+        else {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Raw match filtered out: " + pirsfRawMatch);
+            }
+            return false;
+        }
     }
 
 }
