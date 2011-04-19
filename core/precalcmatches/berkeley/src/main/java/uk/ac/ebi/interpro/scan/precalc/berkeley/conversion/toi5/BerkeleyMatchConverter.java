54d1f9cbbfeb552beb3c9a2515c3c1f3aca24df7
philip.j.r.jones@gmail.com <philip.j.r.jones@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-04-19 13:59:11 +0000

    First commit of precalculated match lookup service.  Service now works - just need to build client.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1042 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/precalcmatches/berkeley/src/main/java/uk/ac/ebi/interpro/scan/precalc/berkeley/conversion/toi5/BerkeleyMatchConverter.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.interpro.scan.precalc.berkeley.conversion.toi5;
+
+import uk.ac.ebi.interpro.scan.model.Match;
+import uk.ac.ebi.interpro.scan.precalc.berkeley.model.BerkeleyMatch;
+
+/**
+ * Interface for class that converts BerkeleyMatches to I5 matches.
+ *
+ * @author Phil Jones
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public interface BerkeleyMatchConverter<T extends Match> {
+
+    T convertMatch(BerkeleyMatch berkeleyMatch);
+}
---
+++ core/precalcmatches/server/src/main/java/uk/ac/ebi/interpro/scan/precalc/server/web/MatchesController.java
@@ -0,0 +1,62 @@
+package uk.ac.ebi.interpro.scan.precalc.server.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import uk.ac.ebi.interpro.scan.precalc.berkeley.model.BerkeleyMatch;
+import uk.ac.ebi.interpro.scan.precalc.berkeley.model.BerkeleyMatchXML;
+import uk.ac.ebi.interpro.scan.precalc.server.service.MatchesService;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * MVC Controller for the Precalculated matches
+ * web service.
+ *
+ * @author Phil Jones
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+
+@Controller
+@RequestMapping("/matches")
+public class MatchesController {
+
+    private static final Logger LOGGER = Logger.getLogger(MatchesController.class.getName());
+
+    private MatchesService matchesService;
+
+    private Jaxb2Marshaller marshaller;
+
+    private static final String VIEW_NAME = "matches";
+
+    @Autowired
+    public MatchesController(MatchesService matchesService, Jaxb2Marshaller marshaller) {
+        Assert.notNull(matchesService, "'matchesService' must not be null");
+        Assert.notNull(marshaller, "'unmarshaller' must not be null");
+        this.matchesService = matchesService;
+        this.marshaller = marshaller;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public void getMatches(HttpServletResponse response,
+                           @RequestParam(value = "md5", required = true) String[] md5Array) {
+        List<BerkeleyMatch> matches = matchesService.getMatches(Arrays.asList(md5Array));
+        BerkeleyMatchXML matchXML = new BerkeleyMatchXML(matches);
+        response.setContentType("application/xml");
+        try {
+            marshaller.marshal(matchXML, new StreamResult(response.getWriter()));
+        } catch (IOException e) {
+            LOGGER.error("IOException thrown when attempting to output 'BerkeleyMatchXML' in response to query: ");
+        }
+    }
+}
