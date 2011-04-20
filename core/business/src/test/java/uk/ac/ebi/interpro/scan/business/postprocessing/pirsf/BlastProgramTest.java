e832bf589202b6c9f19683c240c6001c43e904c1
maxim.scheremetjew@gmail.com <maxim.scheremetjew@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-04-20 13:41:43 +0000

    Finished Blast tool test.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1056 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/business/src/test/java/uk/ac/ebi/interpro/scan/business/postprocessing/pirsf/BlastProgramTest.java
+++ core/business/src/test/java/uk/ac/ebi/interpro/scan/business/postprocessing/pirsf/BlastProgramTest.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.interpro.scan.business.postprocessing.pirsf;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,13 +13,11 @@ import uk.ac.ebi.interpro.scan.business.binary.BinaryRunner;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
- * Created by IntelliJ IDEA.
- * User: matthew
- * Date: 18-Apr-2011
- * Time: 15:18:35
- * To change this template use File | Settings | File Templates.
+ * @author Matthew Fraser
+ * @author Maxim Scheremetjew
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -27,13 +26,67 @@ public class BlastProgramTest extends TestCase {
     @javax.annotation.Resource
     private BinaryRunner binRunner;
 
+    @javax.annotation.Resource(name = "inputFile")
+    private Resource inputFileResource;
+
+    @javax.annotation.Resource(name = "blastDbResource")
+    private Resource blastDbResource;
+
+    @javax.annotation.Resource(name = "binaryPath")
+    private Resource binaryPath;
+
+    @Before
+    public void init() {
+        assertNotNull(binRunner);
+        binRunner.setBinaryPath(binaryPath);
+    }
+
+
     @Test
-    @Ignore
     public void testBlastProgram() {
         //TODO Finish this test!
 
+        File inputFile = null;
+        File blastDBFile = null;
+        try {
+            inputFile = inputFileResource.getFile();
+            blastDBFile = blastDbResource.getFile();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+
+        StringBuffer additionalArgs = null;
+        InputStream io = null;
+        if (inputFile != null) {
+            additionalArgs = new StringBuffer();
+            additionalArgs.append("-i " + inputFile.getAbsolutePath());
+            additionalArgs.append(" -d " + blastDBFile.getAbsolutePath());
+            additionalArgs.append(" -o /tmp/blast/test.out");
+
+            try {
+                io = binRunner.run(additionalArgs.toString());
+                Thread.sleep(1000L);
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            } catch (InterruptedException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            } finally {
+                try {
+                    if (io != null)
+                        io.close();
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+        }
+    }
+
+    @Ignore
+    @Test
+    public void testRunSimpleBlastProgram() {
+
 //        try {
-//            Resource inputFileResource = new ClassPathResource("test_proteins.fasta");
 //            File outFile = File.createTempFile("blastout-", ".out");
 //        } catch (IOException e) {
 //            throw new IllegalStateException(e);
