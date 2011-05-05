1cf7043c4d0e2587ca13fc28526c499ba6b0f9fc
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-05-05 14:25:28 +0000

    Unit test for blastMatches.out file parser
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1073 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/io/src/test/java/uk/ac/ebi/interpro/scan/io/pirsf/BlastMatchesParserTest.java
@@ -0,0 +1,51 @@
+package uk.ac.ebi.interpro.scan.io.pirsf;
+
+import junit.framework.TestCase;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Run tests for parsing the sf.tb file.
+ *
+ * @author Matthew Fraser
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class BlastMatchesParserTest extends TestCase {
+    private static final Logger LOGGER = Logger.getLogger(BlastMatchesParserTest.class.getName());
+
+    private static final String TEST_FILE_NAME = "io/src/test/resources/data/pirsf/blastMatches.out";
+
+    private static final String TEST_RELEASE_VERSION = "2.74";
+
+    @Test
+    public void testBlastMatchesParser() throws IOException {
+
+        // Setup expected result
+        Long proteinId1 = new Long(2);
+        Long proteinId2 = new Long(3);
+
+        Map<Long, String> expectedResult = new HashMap<Long, String>();
+        expectedResult.put(proteinId1, "PIRSF000729");
+        expectedResult.put(proteinId2, "PIRSF000089");
+
+        // Run test method
+        BlastMatchesFileParser parser = new BlastMatchesFileParser();
+//        Resource testFile = new ClassPathResource(TEST_FILE_NAME);
+        Map<Long, String> actualResult = parser.parse(TEST_FILE_NAME);
+
+        // Compare actual result with expected result
+        Assert.assertEquals(2, actualResult.size());
+        Assert.assertEquals(expectedResult.get(proteinId1), actualResult.get(proteinId1));
+        Assert.assertEquals(expectedResult.get(proteinId2), actualResult.get(proteinId2));
+    }
+
+
+}
