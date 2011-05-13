ad3e940a91bd76e14065fe8a1f78653c74b30582
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-05-13 15:16:32 +0000

    Made a start on ProDom implementation. Working towards writing the FASTA file and running the Perl script (but this is not finished)!
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1095 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/prodom/ProDomOutputFileParser.java
@@ -0,0 +1,73 @@
+package uk.ac.ebi.interpro.scan.io.match.prodom;
+
+import org.apache.log4j.Logger;
+import uk.ac.ebi.interpro.scan.io.match.AbstractLineMatchParser;
+import uk.ac.ebi.interpro.scan.io.match.MatchParser;
+import uk.ac.ebi.interpro.scan.model.SignatureLibrary;
+import uk.ac.ebi.interpro.scan.model.raw.PantherRawMatch;
+import uk.ac.ebi.interpro.scan.model.raw.ProDomRawMatch;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Parse the output from the ProDom perl script.
+ *
+ * @author Matthew Fraser
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class ProDomOutputFileParser {
+
+    private static final Logger LOGGER = Logger.getLogger(ProDomOutputFileParser.class.getName());
+
+    private static final Pattern PATTERN = Pattern.compile("^\\d+\\s++PIRSF\\d{6,}+$");
+
+
+    public static Map<Long, String> parse(String pathToFile) throws IOException {
+        File blastMatchesFile = new File(pathToFile);
+        if (blastMatchesFile == null) {
+            throw new NullPointerException("Blast matches file resource is null");
+        }
+        if (!blastMatchesFile.exists()) {
+            throw new IllegalStateException(blastMatchesFile.getName() + " does not exist");
+        }
+        if (!blastMatchesFile.canRead()) {
+            throw new IllegalStateException(blastMatchesFile.getName() + " is not readable");
+        }
+        final Map<Long, String> data = new HashMap<Long, String>();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(blastMatchesFile));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+//                Matcher modelStart = PATTERN.matcher(line);
+//                if (modelStart.find()) {
+//                    // New accession
+//                    String[] text = line.split("\\s++");
+//                    if (text != null && text.length != 2) {
+//                        LOGGER.warn("Unexpected line in blast matches file: " + line);
+//                    } else {
+//                        Long proteinId = Long.parseLong(text[0]);
+//                        String modelId = text[1];
+//                        data.put(proteinId, modelId);
+//                    }
+//                } else {
+//                    LOGGER.warn("Unexpected line in blast matches file: " + line);
+//                }
+            }
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        return data;
+    }
+
+}
---
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/prodom/RunProDomBlast3iStep.java
@@ -0,0 +1,84 @@
+package uk.ac.ebi.interpro.scan.management.model.implementations.prodom;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+import uk.ac.ebi.interpro.scan.management.model.StepInstance;
+import uk.ac.ebi.interpro.scan.management.model.implementations.RunBinaryStep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * This step defines running the ProDom Perl script.
+ *
+ * @author Matthew Fraser
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class RunProDomBlast3iStep extends RunBinaryStep {
+
+    private static final Logger LOGGER = Logger.getLogger(RunProDomBlast3iStep.class.getName());
+
+    private String fullPathToProDomBlast3iPerlScript;
+
+    private String fullPathToBlastBinary;
+
+    private String fastaFileNameTemplate;
+
+    private String fullPathToProDomIprFile;
+
+    private String fullPathToTempDirectory;
+
+    @Required
+    public void setFullPathToProDomBlast3iPerlScript(String fullPathToProDomBlast3iPerlScript) {
+        this.fullPathToProDomBlast3iPerlScript = fullPathToProDomBlast3iPerlScript;
+    }
+
+    @Required
+    public void setFastaFileNameTemplate(String fastaFilePathNameTemplate) {
+        this.fastaFileNameTemplate = fastaFilePathNameTemplate;
+    }
+
+    @Required
+    public void setFullPathToBlastBinary(String fullPathToBlastBinary) {
+        this.fullPathToBlastBinary = fullPathToBlastBinary;
+    }
+
+    @Required
+    public void setFullPathToProDomIprFile(String fullPathToProDomIprFile) {
+        this.fullPathToProDomIprFile = fullPathToProDomIprFile;
+    }
+
+    @Required
+    public void setFullPathToTempDirectory(String fullPathToTempDirectory) {
+        this.fullPathToTempDirectory = fullPathToTempDirectory;
+    }
+
+    /**
+     * Current command line from Onion:
+     * <p/>
+     * /ebi/sp/pro1/Onion/calcs/PRODOM/ProDomBlast3i.pl -P /ebi/production/interpro/Onion/blast/blast-2.2.19/bin/ -p blastp -d /ebi/production/interpro/Onion/prodom/2006.1/prodom.ipr -s SEQUENCE_FILE -t /tmp/ -h 0 -f  > OUTPUT_FILE
+     *
+     * @param stepInstance           containing the parameters for executing.
+     * @param temporaryFileDirectory is the relative path in which files are stored.
+     * @return
+     */
+
+    @Override
+    protected List<String> createCommand(StepInstance stepInstance, String temporaryFileDirectory) {
+        final String fastaFilePathName = stepInstance.buildFullyQualifiedFilePath(temporaryFileDirectory, this.fastaFileNameTemplate);
+        List<String> command = new ArrayList<String>();
+        command.add(this.fullPathToProDomBlast3iPerlScript);
+        command.add("-P");
+        command.add(this.fullPathToBlastBinary);
+        command.add("-d");
+        command.add(this.fullPathToProDomIprFile);
+//        command.add("-s");
+//        command.add(fastaFilePathName);
+        command.add("-t");
+        command.add(this.fullPathToTempDirectory);
+        command.addAll(this.getBinarySwitchesAsList());
+        return command;
+    }
+}
