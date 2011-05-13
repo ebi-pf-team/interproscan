716bf3e43f4caba5b8e5d2a9f7afd724fcfe5122
philip.j.r.jones@gmail.com <philip.j.r.jones@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-05-13 08:52:07 +0000

    Various changes:
    * Fixed bug PRINTS (reported start / stop coordinates)
    * Fixed bug PROSITE (cigar format field too small)
    * Fixed bug Gene3D (binary params incorrect & need to delete 1 amino acid long matches
    * Added code for precalculated match system.
    
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1085 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/precalcmatches/precalc-match-client/src/main/java/uk/ac/ebi/interpro/scan/precalc/client/MatchHttpClient.java
+++ core/precalcmatches/precalc-match-client/src/main/java/uk/ac/ebi/interpro/scan/precalc/client/MatchHttpClient.java
@@ -14,7 +14,9 @@ import uk.ac.ebi.interpro.scan.precalc.berkeley.model.BerkeleyMatchXML;
 
 import javax.xml.transform.stream.StreamSource;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +36,10 @@ public class MatchHttpClient {
 
     private Jaxb2Marshaller unmarshaller;
 
+    public static final String MATCH_SERVICE_PATH = "/matches";
+
+    public static final String PROTEINS_TO_ANALYSE_SERVICE_PATH = "/notCalculated";
+
     public MatchHttpClient(Jaxb2Marshaller unmarshaller) {
         this.unmarshaller = unmarshaller;
     }
@@ -47,7 +53,7 @@ public class MatchHttpClient {
         this.url = url;
     }
 
-    public BerkeleyMatchXML getMatches(List<String> md5s) throws IOException {
+    public BerkeleyMatchXML getMatches(String... md5s) throws IOException {
         if (url == null) {
             throw new IllegalStateException("The url must be set for the MatchHttpClient to function");
         }
@@ -60,7 +66,7 @@ public class MatchHttpClient {
         encodedParameterEntity = new UrlEncodedFormEntity(qparams, "UTF-8");
 
         // Using HttpPost to ensure no problems with long URLs.
-        HttpPost post = new HttpPost(url);
+        HttpPost post = new HttpPost(url + MATCH_SERVICE_PATH);
         post.setEntity(encodedParameterEntity);
 
 
@@ -82,6 +88,52 @@ public class MatchHttpClient {
                 } else {
                     return null;
                 }
+            }
+        };
+        return httpclient.execute(post, handler);
+    }
+
+    public List<String> getMD5sOfProteinsToAnalyse(String... md5s) throws IOException {
+        if (url == null) {
+            throw new IllegalStateException("The url must be set for the MatchHttpClient to function");
+        }
+        HttpClient httpclient = new DefaultHttpClient();
+        final List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+        for (String md5 : md5s) {
+            qparams.add(new BasicNameValuePair(MD5_PARAMETER, md5));
+        }
+        UrlEncodedFormEntity encodedParameterEntity;
+        encodedParameterEntity = new UrlEncodedFormEntity(qparams, "UTF-8");
+
+        // Using HttpPost to ensure no problems with long URLs.
+        HttpPost post = new HttpPost(url + PROTEINS_TO_ANALYSE_SERVICE_PATH);
+        post.setEntity(encodedParameterEntity);
+
+
+        ResponseHandler<List<String>> handler = new ResponseHandler<List<String>>() {
+            public List<String> handleResponse(
+                    HttpResponse response) throws IOException {
+                HttpEntity responseEntity = response.getEntity();
+                List<String> md5sToAnalyse = new ArrayList<String>();
+                if (responseEntity != null) {
+                    // Stream in the response
+                    BufferedReader reader = null;
+                    try {
+                        reader = new BufferedReader(new InputStreamReader(responseEntity.getContent()));
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            line = line.trim();
+                            if (!line.isEmpty()) {
+                                md5sToAnalyse.add(line);
+                            }
+                        }
+                    } finally {
+                        if (reader != null) {
+                            reader.close();
+                        }
+                    }
+                }
+                return md5sToAnalyse;
             }
         };
         return httpclient.execute(post, handler);
