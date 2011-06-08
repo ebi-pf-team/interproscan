36e57ce59f52253a395323f04c74a210de4b1e05
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-06-08 13:41:05 +0000

    Continue work with ProDom implementation - mostly concerning the parsing of the prodom.ipr model and ProDom match output files.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1102 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/io/src/test/java/uk/ac/ebi/interpro/scan/io/prodom/match/ProDomMatchParserTest.java
@@ -0,0 +1,35 @@
+package uk.ac.ebi.interpro.scan.io.prodom.match;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import uk.ac.ebi.interpro.scan.io.match.MatchParser;
+import uk.ac.ebi.interpro.scan.model.SignatureLibrary;
+import uk.ac.ebi.interpro.scan.model.raw.ProDomRawMatch;
+import uk.ac.ebi.interpro.scan.model.raw.RawProtein;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+
+/**
+ * Test the ProDom result output file parser.
+ *
+ * @author Matthew Fraser, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class ProDomMatchParserTest extends TestCase {
+
+    @Test
+    public void testParse() throws IOException {
+        InputStream is = getClass().getClassLoader().getResourceAsStream("data/prodom/prodom_output.txt");
+        MatchParser<ProDomRawMatch> parser = new ProDomMatchParser(SignatureLibrary.PRODOM, "2006.1");
+        Set<RawProtein<ProDomRawMatch>> proteins = parser.parse(is);
+        assertNotNull(proteins);
+        assertTrue(proteins.size() == 3);
+
+        for (RawProtein<ProDomRawMatch> protein : proteins) {
+            assertNotNull(protein.getMatches());
+        }
+    }
+}
