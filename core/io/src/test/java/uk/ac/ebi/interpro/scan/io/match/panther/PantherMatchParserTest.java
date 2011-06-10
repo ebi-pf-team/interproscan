567b5e8cd140c931483527f81498daf2594d8276
maxim.scheremetjew <maxim.scheremetjew@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-06-10 16:44:12 +0000

    Finished parsing of Panther raw results. Extended model, more precisely panther raw match model object.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1113 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/io/src/test/java/uk/ac/ebi/interpro/scan/io/match/panther/PantherMatchParserTest.java
@@ -0,0 +1,46 @@
+package uk.ac.ebi.interpro.scan.io.match.panther;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.ebi.interpro.scan.model.raw.PantherRawMatch;
+
+/**
+ * Tests createMatch method of PantherMatchParser class.
+ *
+ * @author Maxim Scheremetjew, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class PantherMatchParserTest extends TestCase {
+
+    private PantherMatchParser instance;
+
+    @Before
+    public void setUp() {
+        instance = new PantherMatchParser();
+    }
+
+
+    @Test
+    public void testCreateMatch() {
+        //Test of real Panther raw match line
+        String rawMatchLine = "tr|Q6ZSE3|Q6ZSE3_HUMAN\tPTHR10024:SF2\tGB DEF: HYPOTHETICAL PROTEIN FLJ45597\t2.3e-141\t480.5\t1-341";
+        PantherRawMatch result = instance.createMatch(rawMatchLine);
+        assertNotNull("CreateMatch method returned a NULL value!", result);
+        assertEquals("tr|Q6ZSE3|Q6ZSE3_HUMAN", result.getSequenceIdentifier());
+        assertEquals("PTHR10024:SF2", result.getModelId());
+        assertEquals("GB DEF: HYPOTHETICAL PROTEIN FLJ45597", result.getFamilyName());
+        assertEquals(new Double("2.3e-141"), result.getEvalue());
+        assertEquals(new Double("480.5"), result.getScore());
+        assertEquals(1, result.getLocationStart());
+        assertEquals(341, result.getLocationEnd());
+        //location start, end is missing
+        rawMatchLine = "tr|Q6ZSE3|Q6ZSE3_HUMAN\tPTHR10024:SF2\tGB DEF: HYPOTHETICAL PROTEIN FLJ45597\t2.3e-141\t480.5";
+        assertNull("Result of createMatch method should be NULL!", instance.createMatch(rawMatchLine));
+        //
+        assertNull("Result of createMatch method should be NULL!", instance.createMatch(null));
+        //
+        assertNull("Result of createMatch method should be NULL!", instance.createMatch(""));
+    }
+}
--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PantherRawMatch.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PantherRawMatch.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.interpro.scan.model.raw;
 
 import uk.ac.ebi.interpro.scan.model.SignatureLibrary;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -9,19 +10,58 @@ import javax.persistence.Table;
  * <a href="http://www.pantherdb.org/">PANTHER</a> raw match.
  *
  * @author Antony Quinn
+ * @author Maxim Scheremetjew
  * @version $Id$
  */
 @Entity
-@Table(name = "panther_raw_match")
+@Table(name = PantherRawMatch.TABLE_NAME)
 public class PantherRawMatch extends RawMatch {
+
+    public static final String TABLE_NAME = "PANTHER_RAW_MATCH";
+
+    @Column
+    private double evalue;
+
+    @Column
+    private String familyName;
+
+    @Column
+    private double score;
 
     protected PantherRawMatch() {
     }
 
-    protected PantherRawMatch(String sequenceIdentifier, String model,
-                              String signatureLibraryRelease,
-                              int locationStart, int locationEnd) {
+    public PantherRawMatch(String sequenceIdentifier, String model,
+                           String signatureLibraryRelease,
+                           int locationStart, int locationEnd,
+                           double evalue, double score, String familyName) {
         super(sequenceIdentifier, model, SignatureLibrary.PANTHER, signatureLibraryRelease, locationStart, locationEnd);
+        this.evalue = evalue;
+        this.score = score;
+        this.familyName = familyName;
     }
 
+    public double getEvalue() {
+        return evalue;
+    }
+
+    public void setEvalue(double evalue) {
+        this.evalue = evalue;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
 }
