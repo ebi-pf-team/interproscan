7e0f1eaf3f919c6f54c451888b219a69d6c98e67
maxim.scheremetjew <maxim.scheremetjew@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-06-21 09:21:58 +0000

    Added new entity Panther raw match to model.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1122 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PantherRawMatch.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PantherRawMatch.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.interpro.scan.model.raw;
 
+import org.hibernate.annotations.Index;
 import uk.ac.ebi.interpro.scan.model.SignatureLibrary;
 
 import javax.persistence.Column;
@@ -7,14 +8,22 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 /**
- * <a href="http://www.pantherdb.org/">PANTHER</a> raw match.
+ * <a href="http://www.pantherdb.org/">PANTHER</a> raw match. All attributes of a Panther
+ * raw match are stored, even if they are not required at the moment but maybe in the future.
  *
  * @author Antony Quinn
  * @author Maxim Scheremetjew
  * @version $Id$
  */
 @Entity
-@Table(name = PantherRawMatch.TABLE_NAME)
+@javax.persistence.Table(name = PantherRawMatch.TABLE_NAME)
+@org.hibernate.annotations.Table(appliesTo = PantherRawMatch.TABLE_NAME, indexes = {
+        @Index(name = "PANTHER_RW_SEQ_IDX", columnNames = {RawMatch.COL_NAME_SEQUENCE_IDENTIFIER}),
+        @Index(name = "PANTHER_RW_NUM_SEQ_IDX", columnNames = {RawMatch.COL_NAME_NUMERIC_SEQUENCE_ID}),
+        @Index(name = "PANTHER_RW_MODEL_IDX", columnNames = {RawMatch.COL_NAME_MODEL_ID}),
+        @Index(name = "PANTHER_RW_SIGLIB_IDX", columnNames = {RawMatch.COL_NAME_SIGNATURE_LIBRARY}),
+        @Index(name = "PANTHER_RW_SIGLIB_REL_IDX", columnNames = {RawMatch.COL_NAME_SIGNATURE_LIBRARY_RELEASE})
+})
 public class PantherRawMatch extends RawMatch {
 
     public static final String TABLE_NAME = "PANTHER_RAW_MATCH";
@@ -22,6 +31,7 @@ public class PantherRawMatch extends RawMatch {
     @Column
     private double evalue;
 
+    /* Name of the Panther family/subfamily for instance SYNAPTOTAGMIN */
     @Column
     private String familyName;
 
@@ -31,6 +41,11 @@ public class PantherRawMatch extends RawMatch {
     protected PantherRawMatch() {
     }
 
+    /**
+     * @param model      Model Id (Panther family Id) for instance PTHR10024 OR PTHR10024:SF2.
+     * @param score      Calculated by the Panther Perl algorithm/binary.
+     * @param familyName The name of the Panther family/subfamily (for instance SYNAPTOTAGMIN).
+     */
     public PantherRawMatch(String sequenceIdentifier, String model,
                            String signatureLibraryRelease,
                            int locationStart, int locationEnd,
