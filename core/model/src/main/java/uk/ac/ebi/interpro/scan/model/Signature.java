a8b38fe8104b282f5c86b1b5bb516bf9c7bff438
maxim.scheremetjew <maxim.scheremetjew@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-07-15 15:40:27 +0000

    Updated model annotations.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1241 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/Signature.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/Signature.java
@@ -129,8 +129,8 @@ public class Signature implements Serializable {
     @Column(nullable = true, name = "signature_comment")  // comment is an SQL reserved word.
     private String comment;
 
-    @Transient
-    //@ManyToOne(optional = true)
+//    @Transient
+    @ManyToOne(optional = true)
     private Entry entry;
 
     /**
