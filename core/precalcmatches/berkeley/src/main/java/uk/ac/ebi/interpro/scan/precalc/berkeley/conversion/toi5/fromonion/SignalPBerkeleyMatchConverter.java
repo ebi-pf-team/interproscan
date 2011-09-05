c4e5dee5dac15345a43ed71e13aecb08a32b103f
philip.j.r.jones@gmail.com <philip.j.r.jones@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-09-05 11:02:03 +0000

    Empty converter class - to transfer to home machine.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1395 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/precalcmatches/berkeley/src/main/java/uk/ac/ebi/interpro/scan/precalc/berkeley/conversion/toi5/fromonion/SignalPBerkeleyMatchConverter.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.interpro.scan.precalc.berkeley.conversion.toi5.fromonion;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pjones
+ * Date: 9/5/11
+ * Time: 11:53 AM
+ * Converts SignalP matches from Berkeley matches to be retrievable from I5.
+ * <p/>
+ * Note: Onion analysis types:
+ * <p/>
+ * 1	SIGNALP_EUK
+ * 2	SIGNALP_GRAM+
+ * 3	SIGNALP_GRAM-
+ */
+public class SignalPBerkeleyMatchConverter {
+}
