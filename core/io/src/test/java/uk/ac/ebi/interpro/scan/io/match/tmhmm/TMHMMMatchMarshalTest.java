05652759bbdef307a5a4917bc733639412f22ccf
maxim.scheremetjew <maxim.scheremetjew@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-09-13 16:36:40 +0000

    Various changes:
    #Implemented marshaller test for TMHMM and Phobius
    #Finished TMHMM XML output support
    #Finalised TMHMM job file
    
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1424 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/io/src/test/java/uk/ac/ebi/interpro/scan/io/match/tmhmm/TMHMMMatchMarshalTest.java
@@ -0,0 +1,82 @@
+package uk.ac.ebi.interpro.scan.io.match.tmhmm;
+
+import junit.framework.TestCase;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import uk.ac.ebi.interpro.scan.model.PhobiusMatch;
+import uk.ac.ebi.interpro.scan.model.Protein;
+import uk.ac.ebi.interpro.scan.model.Signature;
+import uk.ac.ebi.interpro.scan.model.TMHMMMatch;
+
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.*;
+
+/**
+ * Represents a marshal test for TMHMM matches.
+ *
+ * @author Maxim Scheremetjew, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class TMHMMMatchMarshalTest extends TestCase {
+
+    private static final Logger LOGGER = Logger.getLogger(TMHMMMatchMarshalTest.class.getName());
+
+    private Jaxb2Marshaller marshaller;
+
+    public void init() {
+        marshaller = new Jaxb2Marshaller();
+        marshaller.setClassesToBeBound(Protein.class);
+        Map<String, Boolean> properties = new HashMap<String, Boolean>();
+        properties.put("jaxb.formatted.output", true);
+        marshaller.setMarshallerProperties(properties);
+    }
+
+
+    @Test
+    public void testMarshalTMHMMMatch() {
+        init();
+        Set<TMHMMMatch.TMHMMLocation> locations = new HashSet<TMHMMMatch.TMHMMLocation>();
+        locations.add(new TMHMMMatch.TMHMMLocation(1, 2, "TMHelix", 1.0f));
+        final Signature signature = new Signature.Builder("TMhelix").build();
+        TMHMMMatch match = new TMHMMMatch(signature, locations);
+        Protein protein = new Protein("aaa");
+        protein.addMatch(match);
+        try {
+            String result = marshal(marshaller, protein);
+            if (LOGGER.isDebugEnabled())
+                LOGGER.debug(result);
+            assertNotNull("XML result shouldn't be null!", result);
+            assertTrue(result.contains("<matches>"));
+            assertTrue(result.contains("<tmhmm-match>"));
+            assertTrue(result.contains("<signature ac=\"TMhelix\"/>"));
+            assertTrue(result.contains("<locations>"));
+            assertTrue(result.contains("<tmhmm-location score=\"1.0\" prediction=\"TMHelix\" end=\"2\" start=\"1\"/>"));
+        } catch (IOException e) {
+            LOGGER.warn("Couldn't marshal protein object!", e);
+        }
+    }
+
+    /**
+     * Marshalls an object to an XML, returned as a String instance.
+     *
+     * @param marshaller to perform the object -> XML marshalling
+     * @param o          the object to marshall to XML
+     * @return the XML in a String
+     * @throws java.io.IOException in the event of an error writing out the XML to the StringWriter.
+     */
+    private String marshal(Marshaller marshaller, Object o) throws IOException {
+        Writer writer = new StringWriter();
+        marshaller.marshal(o, new StreamResult(writer));
+        String xml = writer.toString();
+        LOGGER.debug("\n" + xml);
+        return xml;
+    }
+}
--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/TMHMMSignature.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/TMHMMSignature.java
@@ -10,8 +10,8 @@ package uk.ac.ebi.interpro.scan.model;
  */
 public enum TMHMMSignature {
     //TODO:Find out what upper case O means in this context
-    INSIDE_CELL("inside", "inside cell region", null),
-    OUTSIDE_CELL("outside", "outside cell region", null),
+    INSIDE_CELL("inside", "inside cell region", "inside cell region"),
+    OUTSIDE_CELL("outside", "outside cell region", "outside cell region"),
     MEMBRANE("TMhelix", "transmembrane helix", "Region of a membrane-bound protein predicted to be embedded in the membrane."),
     OTHER("O", "O region", null);
 
@@ -42,5 +42,23 @@ public enum TMHMMSignature {
     @Override
     public String toString() {
         return accession;
+    }
+
+    /**
+     * Utility method to check that a given Signature is valid.
+     * This method is light-weight - it is NOT checking that the
+     * SignatureLibrary associated with the Signature is of the correct
+     * type.
+     *
+     * @param signature being checked.
+     * @return true of the Signature accession and name are recognised.
+     */
+    public static boolean isValidSignature(Signature signature) {
+        for (TMHMMSignature type : TMHMMSignature.values()) {
+            if (type.getAccession().equals(signature.getAccession())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
