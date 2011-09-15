8af4028a3a332039fc94af85c0db530ccd6f4709
aquinn.ebi <aquinn.ebi@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-09-15 21:42:34 +0000

    Added web module
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1434 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/web/src/test/java/uk/ac/ebi/interpro/scan/web/ProteinViewControllerTest.java
@@ -0,0 +1,24 @@
+package uk.ac.ebi.interpro.scan.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Tests for {@link ProteinViewController}
+ *
+ * @author  Antony Quinn
+ * @version $Id$
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class ProteinViewControllerTest {
+
+    @Test
+    public void testProtein()    {
+        ProteinViewController c = new ProteinViewController();
+        c.protein("PLCH2");
+    }
+
+}
