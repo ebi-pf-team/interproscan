05053ea04310b56f342177491de04b631c50ecad
aquinn.ebi <aquinn.ebi@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-09-30 14:29:28 +0000

    Use SimpleProtein in JSPs. Reverted back to Lists rather than Maps to enable sorting.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1443 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/web/src/main/java/uk/ac/ebi/interpro/scan/web/ProteinViewController.java
+++ core/web/src/main/java/uk/ac/ebi/interpro/scan/web/ProteinViewController.java
@@ -8,9 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 import uk.ac.ebi.interpro.scan.model.*;
 import uk.ac.ebi.interpro.scan.model.Protein;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Controller for InterPro protein view.
@@ -22,25 +20,43 @@ import java.util.Set;
 public class ProteinViewController {
 
     /**
+     * Returns protein page.
+     *
+     * @param  id   Protein accession or MD5 checksum, for example "P38398"
+     * @return Protein page
+     */
+    @RequestMapping(value = "/protein/{id}", method = RequestMethod.GET)
+    public ModelAndView protein(@PathVariable String id) {
+        return new ModelAndView("protein", "protein", retrieve(id));
+    }
+
+    /**
      * Returns protein features for inclusion in DBML
      *
-     * @param  ac   Protein accession, for example "P38398"
+     * @param  id   Protein accession or MD5 checksum, for example "P38398"
      * @return Protein features for inclusion in DBML
      */
-    @RequestMapping(value = "/protein-features/{ac}", method = RequestMethod.GET)
-    public ModelAndView proteinFeatures(@PathVariable String ac) {
-        return new ModelAndView("protein-features", "protein", retrieve(ac));
+    @RequestMapping(value = "/protein-features/{id}", method = RequestMethod.GET)
+    public ModelAndView proteinFeatures(@PathVariable String id) {
+        return new ModelAndView("protein-features", "protein", retrieve(id));
     }    
 
     /**
      * Returns main body of protein page for inclusion in DBML
      *
-     * @param  ac   Protein accession, for example "P38398"
+     * @param  id   Protein accession or MD5 checksum, for example "P38398"
      * @return Main body of protein page for inclusion in DBML
      */
-    @RequestMapping(value = "/protein-body/{ac}", method = RequestMethod.GET)
-    public ModelAndView proteinBody(@PathVariable String ac) {
-        return new ModelAndView("protein-body", "protein", retrieve(ac));
+    @RequestMapping(value = "/protein-body/{id}", method = RequestMethod.GET)
+    public ModelAndView proteinBody(@PathVariable String id) {
+        return new ModelAndView("protein-body", "protein", retrieve(id));
+    }
+
+    private SimpleProtein retrieve(String id) {
+        // TODO: Check if id is MD5 using regex (using Protein class code?)
+        SimpleProtein p = SimpleProtein.valueOf(retrieveProtein(id));
+        p.sort();
+        return p;
     }
 
     /**
@@ -49,170 +65,264 @@ public class ProteinViewController {
      * @param  ac   Protein accession, for example "P38398"
      * @return Protein for given accession
      */
-    private Protein retrieve(String ac) {
+    private Protein retrieveProtein(String ac) {
 
         // TODO: Get real data from Berkeley DB
 
         // Create protein
-        Protein p = new Protein.Builder("GYGDEYGDEEDEOIHOHDEUHWQJINIKNCD")
-                .crossReference(new ProteinXref("UniProt", "A0A314", "RR12_COFAR"))
+        Protein p = new Protein.Builder("MPTIKQLIRNARQPIRNVTKSPALRGCPQRRGTCTRVYTITPKKPNSALRKVARVRLTSG\n" +
+                                        "FEITAYIPGIGHNLQEHSVVLVRGGRVKDLPGVRYHIVRGTLDAVGVKDRQQGRSKYGVK\n" +
+                                        "KPK")
+                .crossReference(new ProteinXref("UniProt", "A0A314", "RR12_COFAR", "30S ribosomal protein S12, chloroplastic"))
                 .build();
 
         // Add matches
-        Signature signature = new Signature("G3DSA:2.40.50.140", "Nucleic acid-binding proteins");
-        Set<Hmmer3Match.Hmmer3Location> locations = new HashSet<Hmmer3Match.Hmmer3Location>();
-        locations.add(new Hmmer3Match.Hmmer3Location(74, 93, -8.9, 0.28, 63, 82, 114, 73, 94));
-        locations.add(new Hmmer3Match.Hmmer3Location(105, 189, -8.9, 0.28, 63, 82, 114, 73, 94));
-        p.addMatch(new Hmmer3Match(signature, -8.9, 0.28, locations));
+        Set<Hmmer3Match.Hmmer3Location> l1 = new HashSet<Hmmer3Match.Hmmer3Location>();
+        l1.add(new Hmmer3Match.Hmmer3Location(1, 123, -8.9, 0.28, 63, 82, 114, 73, 94));
+        p.addMatch(new Hmmer3Match(
+                new Signature.Builder("G3DSA:2.40.50.140")
+                    .name("Nucleic acid-binding proteins")
+                    .entry(new Entry.Builder("IPR012340")
+                        .description("Nucleic acid-binding, OB-fold")
+                        .type(EntryType.DOMAIN)
+                        .build())
+                    .build(),
+               -8.9, 0.28, l1));
+
+
+        Entry entry = new Entry.Builder("IPR016027")
+                        .description("Nucleic acid-binding, OB-fold-like")
+                        .type(EntryType.DOMAIN)
+                        .build();
+        Set<Hmmer3Match.Hmmer3Location> l2 = new HashSet<Hmmer3Match.Hmmer3Location>();
+        l2.add(new Hmmer3Match.Hmmer3Location(2, 123, -8.9, 0.28, 63, 82, 114, 73, 94));
+        p.addMatch(new Hmmer3Match(
+                new Signature.Builder("SSF50249")
+                    .name("Nucleic_acid_OB")
+                    .entry(entry)
+                    .build(),
+                -8.9, 0.28, l2));
+        p.addMatch(new Hmmer3Match(
+                new Signature.Builder("SSF50250")
+                    .name("Made up name")
+                    .entry(entry)
+                    .build(),
+                -8.9, 0.28, l2));
 
         return p;
 
     }
 
-    public static class SimpleProtein {
+    public final static class SimpleProtein {
 
-        private String ac;
-        private String name;
-        private List<SimpleEntry> entries;
+        private final String ac;      // eg. P38398
+        private final String id;      // BRCA1_HUMAN
+        private final String name;    // eg. Breast cancer type 1 susceptibility
+        private final int length;
+        private final List<SimpleEntry> entries;
 
-        public SimpleProtein(String ac, String name) {
-            this.ac = ac;
-            this.name = name;
+        public SimpleProtein(String ac, String id, String name, int length) {
+            this.ac         = ac;
+            this.id         = id;
+            this.name       = name;
+            this.length     = length;
+            this.entries    = new ArrayList<SimpleEntry>();
         }
 
         public String getAc() {
             return ac;
         }
 
-        public void setAc(String ac) {
-            this.ac = ac;
+        public String getId() {
+            return id;
         }
 
         public String getName() {
             return name;
         }
 
-        public void setName(String name) {
-            this.name = name;
+        public int getLength() {
+            return length;
         }
 
         public List<SimpleEntry> getEntries() {
             return entries;
         }
 
-        public void setEntries(List<SimpleEntry> entries) {
-            this.entries = entries;
+        /**
+         * Returns a {@link SimpleProtein} from a {@link Protein}
+         *
+         * @param p Protein
+         * @return A {@link SimpleProtein} from a {@link Protein}
+         */
+        public static SimpleProtein valueOf(Protein p) {
+            // Get protein info
+            String proteinAc   = "Unknown";
+            String proteinName = "Unknown";
+            String proteinDesc = "Unknown";
+            if (!p.getCrossReferences().isEmpty()) {
+                ProteinXref x = p.getCrossReferences().iterator().next();
+                proteinAc     = x.getIdentifier();
+                proteinName   = x.getName();
+                proteinDesc   = x.getDescription();
+            }
+            SimpleProtein sp = new SimpleProtein(proteinAc, proteinName, proteinDesc, p.getSequenceLength());
+            // Get entries and corresponding signatures
+            for (Match m : p.getMatches()) {
+                // Signature
+                Signature s = m.getSignature();
+                String signatureAc = s.getAccession();
+                SimpleSignature ss = new SimpleSignature(signatureAc, s.getName(), s.getType());
+                for (Object o : m.getLocations()) {
+                    Location l = (Location) o;
+                    ss.getLocations().add(new SimpleLocation(l.getStart(), l.getEnd()));
+                }
+                // Entry
+                Entry e = s.getEntry();
+                SimpleEntry se = new SimpleEntry(e.getAccession(), e.getDescription(), e.getType().getName());                
+                if (sp.getEntries().contains(se))    {
+                    // Entry already exists, so get it
+                    se = sp.getEntries().get(sp.getEntries().indexOf(se));                    
+                }
+                else {
+                    // Create new entry
+                    sp.getEntries().add(se);
+                }
+//                if (sp.getEntriesMap().containsKey(entryAc)) {
+//                    // Entry already exists
+//                    se = sp.getEntriesMap().get(entryAc);
+//                }
+//                else {
+//                    // Create new entry
+//                    Entry e = s.getEntry();
+//                    se = new SimpleEntry(entryAc, e.getDescription(), e.getType().getName());
+//                    // Add to protein
+//                    sp.getEntriesMap().put(entryAc, se);
+//                }
+                // Add signature to entry
+                se.getSignaturesMap().put(signatureAc, ss);
+            }
+            for (SimpleEntry se : sp.entries) {
+                // TODO: Calculate super-match start and end locations from signature matches
+                if (se.getAc().equals("IPR012340")) {
+                    se.getLocations().add(new SimpleLocation(1, 123));
+                }
+                else {
+                    se.getLocations().add(new SimpleLocation(10, 30));
+                    se.getLocations().add(new SimpleLocation(35, 60));
+                    se.getLocations().add(new SimpleLocation(80, 110));
+                }
+            }
+            return sp;
+        }
+
+        public void sort() {
+            // Sort by entry start, and then by signature start
+            Collections.sort(entries);
+            for (SimpleEntry e : entries) {
+                // TODO: Sort signatures
+                //Collections.sort(e.signatures);
+                for (SimpleSignature s : e.getSignatures()) {
+                    Collections.sort(s.locations);
+                }
+            }
         }
 
     }
 
-    public static class SimpleEntry {
+    public final static class SimpleEntry implements Comparable<SimpleEntry>  {
 
-        private String ac;
-        private String name;
-        private String type;
-        private List<SimpleLocation> locations; // super matches
-        private List<SimpleSignature> signatures;
+        private final String ac;
+        private final String name;
+        private final String type;
+        private final List<SimpleLocation> locations; // super matches
+        private final Map<String, SimpleSignature> signatures;
 
         public SimpleEntry(String ac, String name, String type) {
-            this.ac = ac;
-            this.name = name;
-            this.type = type;
+            this.ac         = ac;
+            this.name       = name;
+            this.type       = type;
+            this.locations  = new ArrayList<SimpleLocation>();
+            this.signatures = new HashMap<String, SimpleSignature>();
         }
 
         public String getAc() {
             return ac;
         }
 
-        public void setAc(String ac) {
-            this.ac = ac;
-        }
-
         public String getName() {
             return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
         }
 
         public String getType() {
             return type;
         }
 
-        public void setType(String type) {
-            this.type = type;
-        }
-
         public List<SimpleLocation> getLocations() {
             return locations;
         }
 
-        public void setLocations(List<SimpleLocation> locations) {
-            this.locations = locations;
+        public Collection<SimpleSignature> getSignatures() {
+            return signatures.values();
         }
 
-        public List<SimpleSignature> getSignatures() {
+        public Map<String, SimpleSignature> getSignaturesMap() {
             return signatures;
         }
 
-        public void setSignatures(List<SimpleSignature> signatures) {
-            this.signatures = signatures;
+        @Override public int compareTo(SimpleEntry that) {
+            if (this == that) {
+                return 0;
+            }
+            return Collections.min(this.locations).compareTo(Collections.min(that.locations));
         }
-        
+
+        @Override public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (!(o instanceof SimpleEntry))
+                return false;
+            return this.ac.equals(((SimpleEntry)o).ac);
+        }
+
     }
 
-    public static class SimpleSignature {
+    public final static class SimpleSignature {
 
-        private String ac;
-        private String name;
-        private String type;
-        private List<SimpleLocation> locations;
+        private final String ac;
+        private final String name;
+        private final String type;
+        private final List<SimpleLocation> locations;
 
         public SimpleSignature(String ac, String name, String type) {
             this.ac = ac;
             this.name = name;
             this.type = type;
+            this.locations  = new ArrayList<SimpleLocation>();
         }
 
         public String getAc() {
             return ac;
         }
 
-        public void setAc(String ac) {
-            this.ac = ac;
-        }
-
         public String getName() {
             return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
         }
 
         public String getType() {
             return type;
         }
 
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public List<SimpleLocation> getLocations() {
+        public List<SimpleLocation> getLocations() {        
             return locations;
-        }
-
-        public void setLocations(List<SimpleLocation> locations) {
-            this.locations = locations;
-        }
+        }    
 
     }
 
-    public static class SimpleLocation {
+    public final static class SimpleLocation implements Comparable<SimpleLocation> {
 
-        private int start;
-        private int end;
+        private final int start;
+        private final int end;
 
         public SimpleLocation(int start, int end) {
             this.start = start;
@@ -223,16 +333,29 @@ public class ProteinViewController {
             return start;
         }
 
-        public void setStart(int start) {
-            this.start = start;
-        }
-
         public int getEnd() {
             return end;
         }
 
-        public void setEnd(int end) {
-            this.end = end;
+        @Override public int compareTo(SimpleLocation that) {
+            // Equal
+            if (this == that || this.equals(that)) {
+                return 0;
+            }
+            // Before
+            if (this.getStart() < that.getStart()) {
+                return -1;
+            }
+            // After
+            if (this.getStart() > that.getStart()) {
+                return 1;
+            }
+            // If same start, show lowest first
+            if (this.getStart() == that.getStart()) {
+                return this.getEnd() - that.getEnd();
+            }
+            // Should never get here, but...
+            return this.getStart() - that.getEnd();
         }
 
     }
