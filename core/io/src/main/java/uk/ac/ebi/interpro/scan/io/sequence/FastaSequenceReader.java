aa9b1119f06aceb23ced7b0a2c308238f92bd55a
philip.j.r.jones@gmail.com <philip.j.r.jones@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-11-18 09:13:04 +0000

    Changes to ensure that if the InterProScan master is restarted, any Steps that are re-run that involve writing files are able to overwrite existing files by the same name (on the assumption that files from a previous run may be incomplete, if the Step transaction did not finish successfully - responding to the Broker and part of the transaction.)
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1523 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/sequence/FastaSequenceReader.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/sequence/FastaSequenceReader.java
@@ -4,16 +4,13 @@ import org.springframework.core.io.Resource;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.InputStream;
-
-import uk.ac.ebi.interpro.scan.io.sequence.SequenceReader;
-import uk.ac.ebi.interpro.scan.io.sequence.SequenceRecord;
+import java.io.InputStreamReader;
 
 /**
  * Reads FASTA file.
  *
- * @author  Antony Quinn
+ * @author Antony Quinn
  * @version $Id$
  */
 public final class FastaSequenceReader implements SequenceReader {
@@ -21,9 +18,9 @@ public final class FastaSequenceReader implements SequenceReader {
     // NOTE: Use Java NIO if profiling identifies I/O as bottleneck
     // NOTE: [http://java.sun.com/docs/books/tutorial/essential/io/file.html]    
 
-    private static final char RECORD_START    = '>';
+    private static final char RECORD_START = '>';
     private static final int RECORD_START_LEN = Character.valueOf(RECORD_START).toString().length();
-      
+
     private final SequenceReader.Listener listener;
 
     private FastaSequenceReader() {
@@ -31,35 +28,38 @@ public final class FastaSequenceReader implements SequenceReader {
     }
 
     public FastaSequenceReader(SequenceReader.Listener listener) {
-        if (listener == null)   {
+        if (listener == null) {
             throw new IllegalArgumentException("Listener must not be null");
         }
         this.listener = listener;
     }
 
-    @Override public void read(Resource resource) throws IOException {
+    @Override
+    public void read(Resource resource) throws IOException {
+        if (resource == null) {
+            throw new IllegalStateException("The Resource passed in to the FastaSequenceReader.read() method must not be null.");
+        }
         try {
             read(resource.getInputStream());
-        }
-        catch (IOException e)   {
+        } catch (IOException e) {
             throw (new IOException("Could not read " + resource.getDescription(), e));
         }
     }
 
-    @Override public void read(InputStream file) throws IOException {
+    @Override
+    public void read(InputStream file) throws IOException {
         String line, idLine = null;
         final StringBuilder sequence = new StringBuilder();
         BufferedReader reader = new BufferedReader(new InputStreamReader(file));
         while ((line = reader.readLine()) != null) {
-            if (line.charAt(0) == RECORD_START)   {
+            if (line.charAt(0) == RECORD_START) {
                 // If we have a sequence then we're not at the first ID line in the file
-                if (sequence.length() > 0)  {
+                if (sequence.length() > 0) {
                     listener.mapRecord(createRecord(idLine, sequence.toString()));
                 }
                 idLine = line;
                 sequence.delete(0, sequence.length());  // Reset
-            }
-            else    {
+            } else {
                 // Must be sequence
                 if (idLine == null) {
                     throw new IllegalStateException("No ID line found in input stream");
@@ -73,26 +73,26 @@ public final class FastaSequenceReader implements SequenceReader {
         }
     }
 
-    private SequenceRecord createRecord(String idLine, String sequence)   {
+    private SequenceRecord createRecord(String idLine, String sequence) {
         if (sequence.length() == 0) {
             throw new IllegalArgumentException("No sequence found for: '" + idLine + "'");
         }
         final char ID_LINE_SEP = '|';                   // ID line separator
         idLine = idLine.substring(RECORD_START_LEN);    // Get everything after '>'
-        if (idLine.indexOf(ID_LINE_SEP) > 0)   {
+        if (idLine.indexOf(ID_LINE_SEP) > 0) {
             // Assume we have a UniProt-like record, for example:
             // >sp|P38398|BRCA1_HUMAN Breast cancer type 1 susceptibility protein OS=Homo sapiens GN=BRCA1 PE=1 SV=2
-            final int POS_ID   = 1; // Position of ID, eg. "P38398" 
+            final int POS_ID = 1; // Position of ID, eg. "P38398"
             final int POS_DESC = 2; // Position of description, eg. "BRCA1_HUMAN Breast cancer ..."
             int pos = 0;    // separator position
             StringBuilder id = new StringBuilder();
             StringBuilder description = new StringBuilder();
             for (char c : idLine.toCharArray()) {
-                if (c == ID_LINE_SEP)    {
+                if (c == ID_LINE_SEP) {
                     pos++;
                 }
                 // Get ID, eg. "P38398"
-                if (pos == POS_ID && c != ID_LINE_SEP)   {
+                if (pos == POS_ID && c != ID_LINE_SEP) {
                     id.append(c);
                 }
                 // Get description, eg. "BRCA1_HUMAN Breast cancer ..."
@@ -101,12 +101,11 @@ public final class FastaSequenceReader implements SequenceReader {
                 }
             }
             return new SequenceRecord(id.toString(), description.toString(), sequence);
-        }
-        else    {
+        } else {
             // Assume the only thing on the idLine is the protein ID, for example:
             // >P38398
             return new SequenceRecord(idLine, sequence);
         }
     }
-       
+
 }
--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/sequence/FastaSequenceWriter.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/sequence/FastaSequenceWriter.java
@@ -1,17 +1,18 @@
 package uk.ac.ebi.interpro.scan.io.sequence;
 
+import org.springframework.core.io.Resource;
 import uk.ac.ebi.interpro.scan.model.Protein;
 import uk.ac.ebi.interpro.scan.model.ProteinXref;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
-
-import org.springframework.core.io.Resource;
 
 /**
  * Write FASTA file.
  *
- * @author  Antony Quinn
+ * @author Antony Quinn
  * @version $Id$
  */
 public final class FastaSequenceWriter implements SequenceWriter {
@@ -20,96 +21,96 @@ public final class FastaSequenceWriter implements SequenceWriter {
     // NOTE: [http://java.sun.com/docs/books/tutorial/essential/io/file.html]
 
     private static final String ID_LINE_SEPARATOR = "|";
-    private static final int DEFAULT_WIDTH        = 60;
+    private static final int DEFAULT_WIDTH = 60;
 
     private final int width;
     private final boolean idNullable;
     private final boolean addXrefs;
 
-    public FastaSequenceWriter()    {
+    public FastaSequenceWriter() {
         this(false, DEFAULT_WIDTH, false);
     }
 
-    public FastaSequenceWriter(int width)    {
+    public FastaSequenceWriter(int width) {
         this(false, width, false);
     }
 
-    public FastaSequenceWriter(boolean idNullable)    {
+    public FastaSequenceWriter(boolean idNullable) {
         this(false, DEFAULT_WIDTH, idNullable);
     }
 
-    public FastaSequenceWriter(boolean idNullable, int width, boolean addXrefs)    {
-        if (width < 1)  {
+    public FastaSequenceWriter(boolean idNullable, int width, boolean addXrefs) {
+        if (width < 1) {
             throw new IllegalArgumentException("Width must be greater than 0 (width: " + width + ")");
         }
-        this.width      = width;
+        this.width = width;
         this.idNullable = idNullable;
-        this.addXrefs   = addXrefs;
+        this.addXrefs = addXrefs;
     }
 
-    @Override public void write(Collection<Protein> proteins, Resource resource) throws IOException {
-        if (proteins == null)   {
-            throw new IllegalArgumentException("Proteins collection must not be null");    
+    @Override
+    public void write(Collection<Protein> proteins, Resource resource) throws IOException {
+        if (proteins == null) {
+            throw new IllegalArgumentException("Proteins collection must not be null");
         }
-        if (resource == null)   {
+        if (resource == null) {
             throw new IllegalArgumentException("Resource must not be null");
         }
-        if (resource.exists())  {
+        if (resource.exists()) {
             // TODO: Decide if we should delete existing file or throw exception
-            if (resource.isOpen())  {
+            if (resource.isOpen()) {
                 throw new IllegalStateException("Resource is already open: " + resource.getDescription());
             }
         }
         BufferedWriter writer = null;
         try {
-            writer = new BufferedWriter(new FileWriter(resource.getFile()));
-            for (Protein p : proteins)  {
+            writer = new BufferedWriter(new FileWriter(resource.getFile(), false));
+            for (Protein p : proteins) {
                 writer.write(createRecord(p).toString());
             }
-        }            
-        catch (IOException e)    {
+        } catch (IOException e) {
             throw new IOException("Could not write to resource: " + resource.getDescription(), e);
-        }
-        finally {
+        } finally {
             if (writer != null) {
                 writer.close();
             }
         }
     }
 
-    @Override public int getWidth() {
+    @Override
+    public int getWidth() {
         return width;
     }
 
-    @Override public boolean isIdNullable() {
+    @Override
+    public boolean isIdNullable() {
         return idNullable;
     }
 
-    @Override public boolean isAddXrefs() {
+    @Override
+    public boolean isAddXrefs() {
         return addXrefs;
     }
 
-    private StringBuilder createRecord(Protein p)    {
+    private StringBuilder createRecord(Protein p) {
         final String RECORD_START = ">";
-        final String NEW_LINE     = "\n";
+        final String NEW_LINE = "\n";
         StringBuilder record = new StringBuilder(RECORD_START);
         // Add ID        
-        if (p.getId() == null)  {
+        if (p.getId() == null) {
             if (idNullable) {
                 // Use MD5 instead of ID
                 record.append(p.getMd5());
-            }
-            else    {
+            } else {
                 throw new NullPointerException("Protein ID is null: " + p.toString());
             }
-        }
-        else    {
+        } else {
             record.append(p.getId().toString());
         }
         // Add xrefs        
-        if (addXrefs && p.getCrossReferences().size() > 0)  {
+        if (addXrefs && p.getCrossReferences().size() > 0) {
             record.append(ID_LINE_SEPARATOR);
-            for (ProteinXref xref : p.getCrossReferences())    {
+            for (ProteinXref xref : p.getCrossReferences()) {
                 record.append(xref.getIdentifier());
                 record.append(" ");
             }
@@ -119,20 +120,19 @@ public final class FastaSequenceWriter implements SequenceWriter {
         if (p.getSequence().length() > width) {
             int count = 0;
             boolean wrap = false;
-            for (char c : p.getSequence().toCharArray())    {
+            for (char c : p.getSequence().toCharArray()) {
                 record.append(c);
                 count++;
                 // Word wrap every N chars
                 wrap = (count % width == 0);
-                if (wrap)    {
+                if (wrap) {
                     record.append(NEW_LINE);
                 }
             }
-            if (!wrap)  {
+            if (!wrap) {
                 record.append(NEW_LINE);
             }
-        }
-        else    {
+        } else {
             record.append(p.getSequence());
             record.append(NEW_LINE);
         }
