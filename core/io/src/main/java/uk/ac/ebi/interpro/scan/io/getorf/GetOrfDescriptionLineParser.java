fe41735632f8feb541c04cb0fa20ab56163ebb9e
maxim.scheremetjew <maxim.scheremetjew@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-11-28 17:10:34 +0000

    Started integration of nucleotide acid trace back mechanism.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1532 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/getorf/GetOrfDescriptionLineParser.java
@@ -0,0 +1,69 @@
+package uk.ac.ebi.interpro.scan.io.getorf;
+
+import org.apache.log4j.Logger;
+import uk.ac.ebi.interpro.scan.model.NucleotideSequenceStrand;
+import uk.ac.ebi.interpro.scan.model.OpenReadingFrame;
+
+import java.util.regex.Pattern;
+
+/**
+ * Description line parser for GetOrf formatted FASTA files.
+ *
+ * @author Maxim Scheremetjew, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class GetOrfDescriptionLineParser {
+
+    private static final Logger LOGGER = Logger.getLogger(GetOrfDescriptionLineParser.class.getName());
+
+    private static final String WHITESPACE = " ";
+
+    private static final Pattern ACCESSION_EXTRACTOR_PATTERN = Pattern.compile("^\\#=GF\\s+[A-Z]{2}\\s+([A-Z0-9]+).*$");
+
+    /**
+     * Parses out start and end position as well as the strand from the description line.
+     *
+     * @param descLine Description line.
+     */
+    public OpenReadingFrame parseGetOrfDescriptionLine(String descLine) {
+        if (descLine == null) {
+            LOGGER.warn("The specified description line is NULL!");
+            return null;
+        }
+        //Check description line format using PATTERN class
+        //... e.g.
+        //        Matcher acLineMatcher = ACCESSION_EXTRACTOR_PATTERN.matcher(line);
+        //                    if (acLineMatcher.find()) {
+        //                        record.setModelAc(acLineMatcher.group(1));
+        //                    }
+        //OR
+//        if (!descLine.matches("")) {
+//            LOGGER.warn("The specified description line: " + descLine + " doesn't match the given regular expression!");
+//            return null;
+//        }
+        //Filter step
+        descLine = filterDescriptionLine(descLine);
+        //Split by white space
+        String[] chunks = descLine.trim().split(WHITESPACE);
+        //Get values by index
+        String startPosChunk = chunks[1];
+        String endPosChunk = chunks[3];
+        NucleotideSequenceStrand strand = NucleotideSequenceStrand.SENSE;
+        if (chunks[4].equalsIgnoreCase("REVERSE")) {
+            strand = NucleotideSequenceStrand.ANTISENSE;
+            startPosChunk = endPosChunk;
+            endPosChunk = chunks[1];
+        }
+        int start = Integer.parseInt(startPosChunk);
+        int end = Integer.parseInt(endPosChunk);
+
+        return new OpenReadingFrame(start, end, strand);
+    }
+
+    private String filterDescriptionLine(String descLine) {
+        descLine = descLine.replace("[", "");
+        descLine = descLine.replace("(", "");
+        return descLine.replace("]", "");
+    }
+}
