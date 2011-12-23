52e5d3bdda3b8dfae5f20898301c3913075446f1
Maxim.Scheremetjew@gmail.com <Maxim.Scheremetjew@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2011-12-23 15:02:55 +0000

    Minor changes.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1543 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/writer/ProteinMatchesGFFResultWriter.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/match/writer/ProteinMatchesGFFResultWriter.java
@@ -1,11 +1,14 @@
 package uk.ac.ebi.interpro.scan.io.match.writer;
 
 import uk.ac.ebi.interpro.scan.io.GFFWriter;
+import uk.ac.ebi.interpro.scan.model.Entry;
+import uk.ac.ebi.interpro.scan.model.GoXref;
+import uk.ac.ebi.interpro.scan.model.PathwayXref;
+import uk.ac.ebi.interpro.scan.model.Signature;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 /**
@@ -46,5 +49,60 @@ public abstract class ProteinMatchesGFFResultWriter extends ProteinMatchesResult
 
     public void writeFASTASequence(String identifier, String sequence) throws IOException {
         gffWriter.writeFASTASequence(identifier, sequence);
+    }
+
+    protected void addAdditionalAttr(Signature signature, final List<String> gffAttributes) {
+        Entry interProEntry = signature.getEntry();
+        if (interProEntry != null) {
+            gffAttributes.add("interPro_entry=" + interProEntry.getAccession());
+            gffAttributes.add("interPro_entry_desc=" + interProEntry.getDescription());
+            if (mapToGO) {
+                Collection<GoXref> goXRefs = interProEntry.getGoXRefs();
+                if (goXRefs != null && goXRefs.size() > 0) {
+                    StringBuffer sb = new StringBuffer();
+                    for (GoXref xref : goXRefs) {
+                        if (sb.length() > 0) {
+                            sb.append(VALUE_SEPARATOR);
+                        }
+                        sb.append(xref.getIdentifier()); // Just writeComment the GO identifier to the output
+                    }
+                    gffAttributes.add("go_entries=" + sb.toString());
+                }
+            }
+            if (mapToPathway) {
+                Collection<PathwayXref> pathwayXRefs = interProEntry.getPathwayXRefs();
+                if (pathwayXRefs != null && pathwayXRefs.size() > 0) {
+                    StringBuffer sb = new StringBuffer();
+                    for (PathwayXref xref : pathwayXRefs) {
+                        if (sb.length() > 0) {
+                            sb.append(VALUE_SEPARATOR);
+                        }
+                        sb.append(xref.getDatabaseName() + ": " + xref.getIdentifier());
+                    }
+                    gffAttributes.add("pathways=" + sb.toString());
+                }
+            }
+        }
+    }
+
+    /**
+     * Writes information about the target protein sequence (or reference sequence).
+     *
+     * @param accession
+     * @param sequenceLength
+     * @throws IOException
+     */
+    protected void writeReferenceLine(String accession, int sequenceLength) throws IOException {
+        final List<String> referenceLine = new ArrayList<String>();
+        referenceLine.add(accession);
+        referenceLine.add(".");
+        referenceLine.add("polypeptide");
+        referenceLine.add("1");
+        referenceLine.add("" + sequenceLength);
+        referenceLine.add(".");
+        referenceLine.add("+");
+        referenceLine.add("0");
+        referenceLine.add("ID=" + accession);
+        this.gffWriter.write(referenceLine);
     }
 }
