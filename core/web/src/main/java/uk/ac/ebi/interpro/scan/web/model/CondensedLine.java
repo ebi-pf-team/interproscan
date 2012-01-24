d00edafeb032b7d62b890eebe912275d95ede2e7
philip.j.r.jones@gmail.com <philip.j.r.jones@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2012-01-24 17:02:40 +0000

    Developing algorithm for "condensed view" for new protein page design.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1553 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/web/src/main/java/uk/ac/ebi/interpro/scan/web/model/CondensedLine.java
@@ -0,0 +1,100 @@
+package uk.ac.ebi.interpro.scan.web.model;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author Phil Jones
+ *         Date: 24/01/12
+ *         Time: 15:38
+ *         <p/>
+ *         Comprises the supermatches that will be displayed together on a single line.
+ */
+public class CondensedLine implements Comparable<CondensedLine> {
+
+    /**
+     * zero indexed line number.
+     */
+    private int lineNumber;
+
+    private Set<SimpleSuperMatch> superMatchList = new TreeSet<SimpleSuperMatch>();
+
+    public CondensedLine(int lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public Set<SimpleSuperMatch> getSuperMatchList() {
+        return superMatchList;
+    }
+
+    /**
+     * Comparator to get the lines in the right order.
+     *
+     * @param other to compare with
+     * @return negative number if this comes first, 0 if they are the same line number
+     *         or positive number if other comes first.
+     */
+    public int compareTo(CondensedLine other) {
+        if (this == other || this.equals(other)) {
+            return 0;
+        }
+        return this.lineNumber - other.lineNumber;
+    }
+
+    /**
+     * Tests to see if any of the matches that are tied together overlap with
+     * matches already on the line.  If there is no overlap, then the supermatches
+     * are added to the line.
+     *
+     * @param superMatchBucket
+     * @return
+     */
+    public boolean addSuperMatchesWithoutOverlap(final SuperMatchBucket superMatchBucket) {
+        for (SimpleSuperMatch candidate : superMatchBucket.getSupermatches()) {
+            for (SimpleSuperMatch existingMatch : superMatchList) {
+                if (matchesOverlap(candidate, existingMatch)) {
+                    return false;
+                }
+            }
+        }
+        superMatchList.addAll(superMatchBucket.getSupermatches());
+        return true;
+    }
+
+    /**
+     * Determines if two domains overlap.
+     *
+     * @param one domain match one.
+     * @param two domain match two.
+     * @return true if the two domain matches overlap.
+     */
+    private boolean matchesOverlap(SimpleSuperMatch one, SimpleSuperMatch two) {
+        return !
+                ((one.getLocation().getStart() > two.getLocation().getEnd()) ||
+                        (two.getLocation().getStart() > one.getLocation().getEnd()));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CondensedLine that = (CondensedLine) o;
+
+        if (lineNumber != that.lineNumber) return false;
+        if (!superMatchList.equals(that.superMatchList)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = lineNumber;
+        result = 31 * result + superMatchList.hashCode();
+        return result;
+    }
+}
