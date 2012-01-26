14d87fbb373524afd933accc9f5a30e476bbd595
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2012-01-26 17:24:30 +0000

    Remove code no longer required for latest protein page design (ready for CSS application)
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1606 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/web/src/main/webapp/WEB-INF/tags/signature.tag
+++ core/web/src/main/webapp/WEB-INF/tags/signature.tag
@@ -4,29 +4,12 @@
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <%@ attribute name="protein"   required="true" type="uk.ac.ebi.interpro.scan.web.model.SimpleProtein" %>
 <%@ attribute name="signature" required="true" type="uk.ac.ebi.interpro.scan.web.model.SimpleSignature" %>
-<%@ attribute name="entryTypeIcon"  required="false" %>
 <%@ attribute name="entryTypeTitle" required="false" %>
-<%@ attribute name="colourClass"    required="false" %>
+<%@ attribute name="colourClass"    required="true" %>
 
 <div>
     <p>
-        <c:if test="${not empty entryTypeIcon}">
-            <%--TODO: Make icon stuff a tag--%>
-            <img src="/interpro/images/ico_type_${entryTypeIcon}_small.png"
-                 alt="${entryTypeTitle}" title="${entryTypeTitle}"/>
-        </c:if>
-        <a href="ISignature?ac=${signature.ac}" title="${entryTypeTitle}">${signature.name}</a>
-        <c:set var="prefix">
-            <c:choose>
-                <c:when test="${not empty signature.databaseDescription}">
-                    <abbr title="${signature.databaseDescription}">${signature.database}</abbr>
-                </c:when>
-                <c:otherwise>
-                    ${signature.database}
-                </c:otherwise>
-            </c:choose>
-        </c:set>
-        (${prefix}: ${signature.ac})
+        ${signature.ac} (<a href="ISignature?ac=${signature.ac}" title="${entryTypeTitle} (${signature.database})">${signature.name}</a>)
     </p>
     <div class="match">
         <c:forEach var="location" items="${signature.locations}">
--- core/web/src/main/webapp/resources/protein.js
+++ core/web/src/main/webapp/resources/protein.js
@@ -5,13 +5,8 @@
  * @version $Id$
  */
 
-// Chevrons
-var SHOW_ICON = "\u00BB"; // »
-var HIDE_ICON = "\u00AB"; // «
 
 $(document).ready(function() {
-    
-    createAllEntriesShowHideButton();
 
     // Read colour preference from cookie (requires http://plugins.jquery.com/project/Cookie)
 //    if($.cookie("css")) {
@@ -27,7 +22,7 @@ $(document).ready(function() {
         // Save in cookie
         //$.cookie("colour-by-database", this.checked, {expires: 365, path: '/'});
     });
-    
+
 });
 
 // Disables or enables stylesheets for database colouring
@@ -35,59 +30,4 @@ function configureStylesheets(enable) {
     $("link.database").each(function(i) {
         this.disabled = (!enable);
     });
-}
-
-// Add button to show and hide all signatures
-function createAllEntriesShowHideButton(){
-
-    // TODO: This messes up the state of individual toggling, so may need to loop through each element and toggle?
-
-    var showText = "Show all signatures " + SHOW_ICON;
-    var hideText = "Hide all signatures " + HIDE_ICON;
-    var buttonId = "all-signatures-toggle";
-    var allSignaturesClass = ".entry-signatures";
-
-    // Add button to page
-    $(getButtonHtml(buttonId)).prependTo("#section-domains-sites");
-    createShowHideButton(buttonId, allSignaturesClass, showText, showText, hideText);
-
-    // Hide all signatures after 0 milliseconds -- leaves visible if JavaScript off or not available
-    $(allSignaturesClass).slideToggle(0);
-}
-
-// Add button to show and hide signatures for an individual entry
-function createSingleEntryShowHideButton(targetId){
-    var showText = "Show signatures " + SHOW_ICON;
-    var hideText = "Hide signatures " + HIDE_ICON;
-    var buttonId = targetId + "-toggle";
-    // Add button to page
-    document.write(getButtonHtml(buttonId));
-    createShowHideButton(buttonId, "#" + targetId, showText, showText, hideText);
-}
-
-function getButtonHtml(buttonId) {
-    return "<button id='" + buttonId + "'></button>";
-}
-
-function createShowHideButton(buttonId, target, initialText, showText, hideText) {
-
-    // console.log("createShowHideButton: " + buttonId);
-
-    // Set initial text
-    var button = $("#" + buttonId);
-    button.text(initialText);
-    
-    // Add show/hide behaviour
-    button.click(function () {
-        //console.log("clicked");
-        var delay = 400; // milliseconds
-        $(target).slideToggle(delay);
-        // Toggle
-        setTimeout(function() {
-            var s = (button.text() == showText ? hideText : showText);
-            button.text(s);
-        }, delay / 2);
-    });
-
-    return button;
 }
