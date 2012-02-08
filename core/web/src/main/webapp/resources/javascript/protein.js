829b4d61f033ab58a075c0d8dd5eac93bab37786
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2012-02-08 11:52:21 +0000

    Continue with match location popup tooltips
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1623 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/web/src/main/webapp/resources/javascript/protein.js
+++ core/web/src/main/webapp/resources/javascript/protein.js
@@ -5,25 +5,40 @@
  * @version $Id$
  */
 
+// Prepare protein match popup
+// E.g. Tie "match-location-1" span to "match-popup-1" hidden div content.
+function preparePopup(spanId) {
+    var searchString = 'location-';
+    var prefix = spanId.substring(0, spanId.indexOf(searchString));
+    var postfix = spanId.substring(prefix.length + searchString.length, spanId.length);
+    var popupId = prefix.concat('popup-', postfix);
+    //alert('spanId: '.concat(spanId, ', prefix: ', prefix, ', postfix: ', postfix, ', popupId: ', popupId));
 
-$(document).ready(function() {
-
-    // Read colour preference from cookie (requires http://plugins.jquery.com/project/Cookie)
-//    if($.cookie("css")) {
-//        var id = $.cookie("colour-by-database");
-//        ...
-//    }
-
-    // CSS switching
-    var checkbox = $("input[type=checkbox]");
-    configureStylesheets(checkbox.checked); // initialise
-    checkbox.click(function() {
-        configureStylesheets(this.checked);
-        // Save in cookie
-        //$.cookie("colour-by-database", this.checked, {expires: 365, path: '/'});
+    $('#'.concat(spanId)).qtip({
+        content: {
+            text: $('#'.concat(popupId)),
+            title: {
+                text: 'Location data',
+                button: true // Close button
+            }
+        },
+        position: {
+            my: 'top center',
+            at: 'bottom center',
+            viewport: $(window), // Keep the tooltip on-screen at all times
+            effect: true // Positioning animation
+        },
+        show: {
+            event: 'click',
+            solo: false // Show one tooltip at a time?
+        },
+        hide: 'close',
+        style: {
+            classes: 'ui-tooltip-wiki ui-tooltip-light ui-tooltip-shadow'
+        }
     });
+}
 
-});
 
 // Disables or enables stylesheets for database colouring
 function configureStylesheets(enable) {
