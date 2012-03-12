237ae989aec3c449392a75c8c146687a9efc2fbf
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2012-03-12 14:21:44 +0000

    Refactor javascript: - common.js = javascript common to all jsp pages; - protein.js = javascript used by the protein page (but not by the protein structure page); - protein-popups.js = javascript used to implement popups on the protein and protein structure pages;
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1694 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/web/src/main/webapp/resources/javascript/protein-popups.js
@@ -0,0 +1,48 @@
+// Prepare protein match/structural match popups
+// E.g. Tie "match-location-1" span to "match-popup-1" hidden div content.
+function preparePopup(spanId) {
+    var searchString = 'location-';
+    var prefix = spanId.substring(0, spanId.indexOf(searchString));
+    var postfix = spanId.substring(prefix.length + searchString.length, spanId.length);
+    var popupId = prefix.concat('popup-', postfix);
+    //alert('spanId: '.concat(spanId, ', prefix: ', prefix, ', postfix: ', postfix, ', popupId: ', popupId));
+
+    $('#'.concat(spanId)).qtip({
+        prerender: true, // Render all popups at page load time?
+        content: {
+            text: $('#'.concat(popupId)),
+            title: {
+                text: 'Location data', // Popup text in the title bar
+                button: true // Close button
+            }
+        },
+        position: {
+            my: 'bottom center',
+            at: 'top center',
+            viewport: $(window), // Keep the tooltip on-screen at all times
+            effect: true // Positioning animation
+        },
+        show: {
+            event: 'mouseenter',
+            solo: false, // Show one tooltip at a time?
+            delay: 250 // Avoid a mass of popups when moving mouse across the screen!
+        },
+        hide: {
+            fixed: true, // If the user mouses out of the span to the popup then keep the popup open
+            delay: 500
+        },
+        style: {
+            classes: 'ui-tooltip-wiki ui-tooltip-light ui-tooltip-shadow'
+        },
+        events: {
+            render: function(event, api) {
+                api.elements.target.bind('click', function() {
+                    api.set('hide.event', false);
+                });
+            },
+            hide: function(event, api) {
+                api.set('hide.event', 'mouseout');
+            }
+        }
+    });
+}
