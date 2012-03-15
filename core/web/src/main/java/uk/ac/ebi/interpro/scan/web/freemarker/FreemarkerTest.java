6e3adfdeab7403261e1dffa01c8449b394ce0df5
maxim.scheremetjew <maxim.scheremetjew@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2012-03-15 15:23:40 +0000

    Simple FreeMarker test file.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1726 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/web/src/main/java/uk/ac/ebi/interpro/scan/web/freemarker/FreemarkerTest.java
@@ -0,0 +1,87 @@
+package uk.ac.ebi.interpro.scan.web.freemarker;
+
+import freemarker.template.*;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import uk.ac.ebi.interpro.scan.model.Protein;
+import uk.ac.ebi.interpro.scan.web.ProteinViewHelper;
+import uk.ac.ebi.interpro.scan.web.io.CreateSimpleProteinFromMatchData;
+import uk.ac.ebi.interpro.scan.web.io.EntryHierarchy;
+import uk.ac.ebi.interpro.scan.web.model.CondensedView;
+import uk.ac.ebi.interpro.scan.web.model.SimpleProtein;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Simple main class which processes FreeMarker templates.
+ *
+ * @author Maxim Scheremetjew, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class FreemarkerTest {
+
+    public static void main(String[] args) throws Exception {
+        String directoryForTemplateLoading = "../WEB-INF/freemarker";
+        String pathToAppContextFile = "file:../WEB-INF/spring/app-config.xml";
+        String resultFilePath = "<home.dir>/test/freemarker.html";
+        String testView = "views/protein-structure.ftl";
+
+        Configuration cfg = new Configuration();
+        cfg.setDirectoryForTemplateLoading(
+                new File(directoryForTemplateLoading));
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("css_resource_jquery_qtip2", "resources/javascript/qtip2/jquery.qtip.css");
+        variables.put("css_resource_protein", "resources/css/protein.css");
+        variables.put("css_resource_type_colours", "resources/css/type_colours.css");
+        variables.put("css_resource_database", "resources/css/database.css");
+        variables.put("js_resource_jquery171", "resources/javascript/jquery/jquery-1.7.1.min.js");
+        variables.put("js_resource_jquery_qtip2", "resources/javascript/qtip2/jquery.qtip.min.js");
+        variables.put("js_resource_protein", "resources/javascript/protein.js");
+        variables.put("js_resource_jquery_jscroll", "resources/javascript/jquery/jquery.jscroll.min.js");
+        variables.put("img_resource_path", "resources");
+        variables.put("js_resource_common", "resources/javascript/common.js");
+        variables.put("js_resource_protein_popups", "resources/javascript/protein-popups.js");
+        cfg.setAllSharedVariables(new SimpleHash(variables, new DefaultObjectWrapper()));
+
+        final AbstractApplicationContext ctx = new FileSystemXmlApplicationContext(pathToAppContextFile);
+//        Get beans from context file
+        EntryHierarchy entryHierarchy = (EntryHierarchy) ctx.getBean("entryHierarchy");
+        Jaxb2Marshaller marshaller = (Jaxb2Marshaller) ctx.getBean("jaxb2");
+        CreateSimpleProteinFromMatchData matchData = (CreateSimpleProteinFromMatchData) ctx.getBean("matchData");
+
+        SimpleProtein simpleProtein = matchData.queryByAccession("P38398");
+
+        SimpleHash model = buildModelMap(simpleProtein, entryHierarchy);
+
+        Template temp = cfg.getTemplate(testView);
+        Writer out = new PrintWriter(new FileWriter(resultFilePath));
+        temp.process(model, out);
+        out.flush();
+    }
+
+    private static SimpleHash buildModelMap(SimpleProtein p, EntryHierarchy entryHierarchy) {
+        final int MAX_NUM_MATCH_DIAGRAM_SCALE_MARKERS = 10;
+        SimpleHash model = new SimpleHash();
+        if (p != null) {
+            model.put("protein", p);
+            model.put("condensedView", new CondensedView(p));
+            model.put("entryColours", entryHierarchy.getEntryColourMap());
+            model.put("scale", ProteinViewHelper.generateScaleMarkers(p.getLength(), MAX_NUM_MATCH_DIAGRAM_SCALE_MARKERS));
+        } // Else no match data was found for the protein therefore nothing to display
+        return model;
+    }
+
+    public static SimpleProtein getSimpleProteinFromXMLFile(Jaxb2Marshaller marshaller,
+                                                            EntryHierarchy entryHierarchy) throws IOException {
+        Resource resource = new ClassPathResource("protein-freemarker.xml");
+        Protein protein = (Protein) marshaller.unmarshal(new StreamSource(resource.getInputStream()));
+        return SimpleProtein.valueOf(protein, entryHierarchy);
+    }
+}
