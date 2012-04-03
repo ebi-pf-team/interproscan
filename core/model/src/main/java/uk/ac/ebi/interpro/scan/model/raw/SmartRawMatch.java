8d45ac6a771b4e6122dc68f80af5e966e3c73c4c
newboycraig@gmail.com <newboycraig@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2012-04-03 14:54:36 +0000

    Implemented compareTo method
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@1777 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/SmartRawMatch.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/SmartRawMatch.java
@@ -21,7 +21,7 @@ import javax.persistence.Entity;
         @Index(name = "SMART_RW_SIGLIB_IDX", columnNames = {RawMatch.COL_NAME_SIGNATURE_LIBRARY}),
         @Index(name = "SMART_RW_SIGLIB_REL_IDX", columnNames = {RawMatch.COL_NAME_SIGNATURE_LIBRARY_RELEASE})
 })
-public class SmartRawMatch extends Hmmer2RawMatch {
+public class SmartRawMatch extends Hmmer2RawMatch implements Comparable<SmartRawMatch> {
 
     public static final String TABLE_NAME = "SMART_RAW_MATCH";
 
@@ -31,5 +31,18 @@ public class SmartRawMatch extends Hmmer2RawMatch {
 
     public SmartRawMatch(String sequenceIdentifier, String model, SignatureLibrary signatureLibrary, String signatureLibraryRelease, int locationStart, int locationEnd, double evalue, double score, int hmmStart, int hmmEnd, String hmmBounds, double locationEvalue, double locationScore) {
         super(sequenceIdentifier, model, SignatureLibrary.SMART, signatureLibraryRelease, locationStart, locationEnd, evalue, score, hmmStart, hmmEnd, hmmBounds, locationEvalue, locationScore);
+    }
+
+    @Override
+    public int compareTo(SmartRawMatch that) {
+        if (this == that) return 0;
+        if (this.getEvalue() < that.getEvalue()) return -1;     // First, sort by ievalue ASC
+        if (this.getEvalue() > that.getEvalue()) return 1;
+        if (this.getScore() > that.getScore()) return -1;                     // then by score ASC
+        if (this.getScore() < that.getScore()) return 1;
+        if (this.hashCode() > that.hashCode())
+            return -1;                     // then by hashcode to be consistent with equals.
+        if (this.hashCode() < that.hashCode()) return 1;
+        return 0;
     }
 }
