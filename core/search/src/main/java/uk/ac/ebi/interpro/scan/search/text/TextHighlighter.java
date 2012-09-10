cd86bcb0e8f97b04e0acb4e38a28ea74365c184d
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2012-09-10 10:39:11 +0000

    Tweaks to search based on curator feedback
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@2223 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/search/src/main/java/uk/ac/ebi/interpro/scan/search/text/TextHighlighter.java
+++ core/search/src/main/java/uk/ac/ebi/interpro/scan/search/text/TextHighlighter.java
@@ -11,6 +11,7 @@ import org.apache.lucene.util.Version;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.regex.Pattern;
 
 /**
  * Highlight query terms in search results.
@@ -30,9 +31,10 @@ public final class TextHighlighter {
     private static final Formatter   TITLE_FORMATTER       = new SimpleHTMLFormatter("<span class='entry_title_high'>", "</span>");
     private static final Formatter   DESCRIPTION_FORMATTER = new SimpleHTMLFormatter("<span class='entry_sum_high'>", "</span>");
 
-    private static final int SNIPPET_MAX    = 5;
-    private static final int SNIPPET_LEN    = 35;
+    private static final int SNIPPET_MAX    = 3;
+    private static final int SNIPPET_LEN    = 60;
     private static final String SNIPPET_SEP = "...";
+    private static final Pattern MID_SENTANCE_PATTERN = Pattern.compile("^[a-z].*");
 
     private final Highlighter titleHighlighter;
     private final Highlighter descriptionHighlighter;
@@ -69,8 +71,10 @@ public final class TextHighlighter {
         if (description.equals("")) {
             return description;
         }
-        int len = Math.min(description.length(), 140);
-        return highlight(description, descriptionHighlighter, description.substring(0, len - 1) + SNIPPET_SEP);
+        int len = Math.min(description.length(), 180);
+        description = highlight(description, descriptionHighlighter, description.substring(0, len - 1) + SNIPPET_SEP);
+        return tidyFragments(description);
+
     }
 
     private String highlight(String text, Highlighter highlighter, String defaultText) {
@@ -86,6 +90,27 @@ public final class TextHighlighter {
             throw new IllegalStateException("Could not highlight " + text, e);
         }
         return fragments.length() == 0 ? defaultText : fragments;
+    }
+
+    /**
+     * Perform final tidying of the fragments text ready for display to the end user
+     * @param fragments Piece of text containing all fragments
+     * @return Tidied fragments text
+     */
+    private String tidyFragments(String fragments) {
+        if (fragments.startsWith(",")) {
+            fragments = fragments.substring(1);
+        }
+        fragments = fragments.trim();
+        if(MID_SENTANCE_PATTERN.matcher(fragments).matches()) {
+            // Not at the start of a sentence so manually add the "..."
+            fragments = "..." + fragments;
+        }
+        if (!fragments.endsWith(".")) {
+            // Not the end of a sentence so manually add the "..."
+            fragments = fragments + "...";
+        }
+        return fragments;
     }
 
 }
