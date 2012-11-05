417129706bec0284255c837cab0b8b625571ff3c
philip.j.r.jones@gmail.com <philip.j.r.jones@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2012-11-05 13:43:39 +0000

    Added new Condensed View modules / sub-projects.
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@2374 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/condensed-view/condensed-server/src/main/java/uk/ac/ebi/interpro/scan/condensed/server/web/PingController.java
@@ -0,0 +1,24 @@
+package uk.ac.ebi.interpro.scan.condensed.server.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pjones
+ * Date: 03/05/12
+ * Time: 11:31
+ */
+@Controller
+public class PingController {
+
+    @RequestMapping("/ping")
+    public void ping(HttpServletResponse resp) throws IOException {
+        final PrintWriter writer = resp.getWriter();
+        writer.println("ok");
+    }
+}
