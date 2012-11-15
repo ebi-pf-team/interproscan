2dbc40e4a0e9761474ee42a216c1e4bdb6c1fcab
mr.matthew.fraser <mr.matthew.fraser@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2012-11-15 16:45:54 +0000

    New unit test
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@2440 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

--- core/web/src/test/java/uk/ac/ebi/interpro/scan/web/io/AnalyseMatchDataResultTest.java
+++ core/web/src/test/java/uk/ac/ebi/interpro/scan/web/io/AnalyseMatchDataResultTest.java
@@ -4,13 +4,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.ac.ebi.interpro.scan.web.model.SimpleEntry;
 import uk.ac.ebi.interpro.scan.web.model.SimpleProtein;
+import uk.ac.ebi.interpro.scan.web.model.SimpleSignature;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Tests for {@link AnalyseMatchDataResult}
@@ -42,5 +47,55 @@ public class AnalyseMatchDataResultTest {
     @Test(expected = NullPointerException.class)
     public void testResourceNull() {
         parser.parseMatchDataOutput(null);
+    }
+
+    @Test
+    public void testCreateSimpleProtein() {
+        List<MatchDataRecord> records = new ArrayList<MatchDataRecord>();
+        records.add(new MatchDataRecord("A2AP18",
+                "PLCH2_MOUSE",
+                1501,
+                "6C2CA0E2864B327B",
+                "PTHR10336:SF21",
+                "PTHR10336:SF21",
+                "PANTHER",
+                29,
+                1489,
+                0.0,
+                "",
+                "",
+                "",
+                "",
+                10090,
+                "Mus musculus",
+                "Mus musculus (Mouse)",
+                false));
+        records.add(new MatchDataRecord("A2AP18",
+                "PLCH2_MOUSE",
+                1501,
+                "6C2CA0E2864B327B",
+                "PTHR10336",
+                "PTHR10336",
+                "PANTHER",
+                29,
+                1489,
+                0.0,
+                "",
+                "",
+                "",
+                "",
+                10090,
+                "Mus musculus",
+                "Mus musculus (Mouse)",
+                false));
+
+        SimpleProtein protein = parser.createSimpleProtein(records);
+        assertNotNull(protein);
+        List<SimpleEntry> entries = protein.getEntries();
+        assertNotNull(entries);
+        assertTrue(entries.size() == 0);
+        List<SimpleSignature> signatures = protein.getUnintegratedSignatures();
+        assertNotNull(signatures);
+        assertTrue(signatures.size() == 2);
     }
 }
