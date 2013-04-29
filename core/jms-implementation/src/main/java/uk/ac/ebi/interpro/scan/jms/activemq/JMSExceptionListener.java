55b2065c45df12419d3662ad3302b1f629e3955b
nuka.ebi@gmail.com <nuka.ebi@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2013-04-29 14:24:12 +0000

    Changed job priority for HAMAP and PROSITE jobs Updated Exception handling for TransportIO exceptions
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@3353 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0

---
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/activemq/JMSExceptionListener.java
@@ -0,0 +1,29 @@
+package uk.ac.ebi.interpro.scan.jms.activemq;
+
+import org.apache.activemq.transport.TransportListener;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import java.io.IOException;
+
+/**
+ * @author Gift Nuka
+ */
+
+@Component
+public class JMSExceptionListener implements ExceptionListener {
+
+    private static final Logger LOGGER = Logger.getLogger(JMSExceptionListener.class.getName());
+
+    public synchronized void onException(JMSException e) {
+
+        Exception ex = e.getLinkedException();
+        if (ex != null) {
+            LOGGER.debug("JMS Exception: ", e);
+        }
+
+    }
+
+}
