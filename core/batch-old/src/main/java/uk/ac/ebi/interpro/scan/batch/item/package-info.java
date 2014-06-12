66149d64cf9e7a514892d812fed5f48c1f5e891a
Mr.Matthew.Fraser@gmail.com <Mr.Matthew.Fraser@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2014-06-12 16:34:18 +0000

    Remove old modules
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@4522 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0
    
    Former-commit-id: 3b419c169a97c4d24d76cd32ac51428bfd831681

--- core/batch-old/src/main/java/uk/ac/ebi/interpro/scan/batch/item/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Provides implementations of the Spring Batch {@link org.springframework.batch.item.ItemReader} and
- * {@link org.springframework.batch.item.ItemWriter} interfaces.
- *
- * @since 1.0
- */
-package uk.ac.ebi.interpro.scan.batch.item;
--- core/batch-old/src/main/java/uk/ac/ebi/interpro/scan/batch/tasklet/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Provides implementations of the Spring Batch {@link org.springframework.batch.core.step.tasklet.Tasklet}
- * interface.
- *
- * @since 1.0
- */
-package uk.ac.ebi.interpro.scan.batch.tasklet;
