c8ebf3847aa27ee6a1b08fed14a5f48f8a4b72da
Mr.Matthew.Fraser@gmail.com <Mr.Matthew.Fraser@gmail.com@ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0>
2014-06-12 16:34:18 +0000

    Remove old modules
    git-svn-id: https://interproscan.googlecode.com/svn/trunk@4522 ea5da8b0-6d6b-11de-9de6-4d42d2ab87c0
    
    Former-commit-id: 3b419c169a97c4d24d76cd32ac51428bfd831681

--- core/batch-proactive/src/main/java/uk/ac/ebi/interpro/scan/batch/partition/proactive/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Provides implementations of the Spring Batch {@link org.springframework.batch.core.partition.PartitionHandler} 
- * interface for ProActive Parallel Suite.
- *
- * @see   <a href="http://proactive.inria.fr/">ProActive Parallel Suite</a>
- * @since 1.0
- */
-package uk.ac.ebi.interpro.scan.batch.partition.proactive;
--- core/batch-proactive/src/main/java/uk/ac/ebi/interpro/scan/batch/partition/remote/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides support classes for running partitioned steps remotely.
- *
- * @since 1.0
- */
-package uk.ac.ebi.interpro.scan.batch.partition.remote;
