7c28b0c5e5a23197a60b4e5eb2f6ddcf54a82172
nuka <nuka@ebi.ac.uk>
2015-04-01 10:45:50 +0100

    fixed the pfsearch fsata input format problem; updated the properties file to include hamap filter settings
    pfsearch doesnt raise an exception when a fasta input file has lines with length greater than 80.
    
    
    Former-commit-id: 8a5b309672bda10769721ce24fab1d4fde094483

--- core/jms-implementation/support-mini-x86-32/bin/prosite/pfsearch_wrapper.py
+++ core/jms-implementation/support-mini-x86-32/bin/prosite/pfsearch_wrapper.py
@@ -1,7 +1,8 @@
 #parse the hmmer3 tblout file
 #run the pfsearch binary
 #gift nuka
-#jan 2015
+#jan 2015, mar 2015
+
 
 import subprocess
 import sys
@@ -12,27 +13,22 @@ import time
 
 from tempfile import NamedTemporaryFile
 
-
 #reformat pfsearch alignment output to appear on one line
 def clean_output(lines):
   line_list = iter(lines.splitlines())
   formatted_lines  = ''
-  print lines
 
   for line in line_list:
     if not line.strip():
       continue
     if 'match_nb' in line:
       line = re.sub('match_nb=\S+\s+match_type=\S+\s+', '', line)
-      print line
       if 'match_nb' in line:
-        print 'regex failed ... ', line
+        #print 'regex failed ... ', line
         line = re.sub('match_nb=\S+\s+', '', line)
         line = re.sub('match_type=\S+\s+', '', line)
-        print 'regex may hav failed, print line again: ', line
-      else:
-	print 'regex worked ... ', line
-    #print line
+        #print 'regex may hav failed, print line again: ', line
+	#else print 'regex worked ... ', line
     if formatted_lines and len(line) > 0 and line[0] == '>':
       formatted_lines += '\n'
       formatted_lines += line
@@ -43,7 +39,7 @@ def clean_output(lines):
     formatted_lines = ''
   else:
     formatted_lines += '\n'
-  print formatted_lines
+  #print formatted_lines
   return formatted_lines
 
 ##get the hamap profiles from the
@@ -72,32 +68,36 @@ def get_hamap_profile(profiles_list_filename):
     return profiles
 
 def get_sequences(fasta_file):
-    fasta= open(fasta_file, 'r')
-    fasta_dict= {}
+    fasta = open(fasta_file, 'r')
+    fasta_dict = {}
     for line in fasta:
-        line= line.strip()
+        line = line.strip()
         if line == '':
             continue
         if line.startswith('>'):
-            seq_id= line.lstrip('>')
-            seq_id= re.sub('\..*', '', seq_id)
-            fasta_dict[seq_id]= ''
+            seq_id = line.lstrip('>')
+            seq_id = re.sub('\..*', '', seq_id)
+            fasta_dict[seq_id] = ''
         else:
-            fasta_dict[seq_id] += line
+            fasta_dict[seq_id] += line + '\n'
+            if len(line) > 80:
+                raise ValueError('Input fasta file format problem for pfsearch, line length greater than 80 ')
     fasta.close()
     return fasta_dict
 
 def get_sequences_for_profile(key_list, seqs_dict):
     sequences = ''
-    print len(key_list),':', key_list
+    #print len(key_list),':', key_list
     for key in key_list:
         if key in seqs_dict:
             #print "key found : ", key
             value = seqs_dict[key]
-            sequences += '>' + key + '\n' + value + '\n'
+            sequences += '>' + key + '\n' + value
+            if not value.endswith('\n'):
+                sequences += '\n'
 	    #print 'ok', sequences
         else:
-            print "key not found .. "
+            print "key not found : ", key
 
     #print 'ok', sequences
     return sequences
@@ -111,13 +111,10 @@ def write_to_file(filename, output):
     with open(filename, 'w') as seq_file:
         seq_file.write(output)  
 
-def run_pfsearch_binary():
-    print "run actual pfsearch command"
-
 def run_pfsearch_binary(arg_list, profiles, seqs_dict, stats_filename, command_index):
     count = 0
     temp_file_list = []
-    prf_half = int(len(profiles) / 2)
+    #prf_half = int(len(profiles) / 2)
     get_seq_time = 0
     for prf in profiles:
         sequence_ids = profiles[prf][1:]
@@ -125,11 +122,11 @@ def run_pfsearch_binary(arg_list, profiles, seqs_dict, stats_filename, command_i
         input_fasta_sequences = get_sequences_for_profile(sequence_ids, seqs_dict)
         #print input_fasta_sequences
         temp_file = create_temp_file(prf)
-        print 'temp file is ', temp_file
+        #print 'temp file is ', temp_file
         write_to_file(temp_file, input_fasta_sequences)
         get_seq_end_time = time.time()
         time_to_get_seqences = get_seq_end_time - get_seq_start_time
-        print 'time to get ', len(sequence_ids), 'sequences for ', prf, ': ', time_to_get_seqences
+        #print 'time to get ', len(sequence_ids), 'sequences for ', prf, ': ', time_to_get_seqences
         get_seq_time += time_to_get_seqences
         temp_file_list.append(temp_file)
 
@@ -138,7 +135,7 @@ def run_pfsearch_binary(arg_list, profiles, seqs_dict, stats_filename, command_i
         #comd_to_run.extend([profiles[prf][0],fasta_file])
         comd_to_run.extend([profiles[prf][0],temp_file])
         cmd_string = ' '.join(comd_to_run)
-        print "command to run: ",  cmd_string
+        #print "command to run: ",  cmd_string
         count = count  + 1
         output = subprocess.check_output(comd_to_run)
         output = clean_output(output)
@@ -149,7 +146,7 @@ def run_pfsearch_binary(arg_list, profiles, seqs_dict, stats_filename, command_i
         #    break
 
     for tempfile in temp_file_list:
-        print tempfile
+        #print tempfile
         os.unlink(tempfile)
 
     with open(stats_filename, 'w') as stats_file:
@@ -158,55 +155,47 @@ def run_pfsearch_binary(arg_list, profiles, seqs_dict, stats_filename, command_i
 
 if __name__ == "__main__":
 
+    if sys.version_info<(2,7,0):
+        sys.exit("Error: You need python 2.7 or later to run this script")
+
     #deal with arguments that are required
     if len(sys.argv) < 6:
         sys.exit("Error: expected more than 6 arguments, check your command again")
 
-    arg_list = sys.argv
-    profiles_list_filename = arg_list[1]
-    fasta_file = arg_list[2]
-    stats_filename = arg_list[3]
-    output_file = arg_list[4]
-    model_dir = arg_list[5]
-    command_index = 6
+    try:
+        arg_list = sys.argv
+        profiles_list_filename = arg_list[1]
+        fasta_file = arg_list[2]
+        stats_filename = arg_list[3]
+        output_file = arg_list[4]
+        model_dir = arg_list[5]
+        command_index = 6
 
-    pfsreach_cmd = ' '.join(sys.argv)
-    print pfsreach_cmd
+        pfsearch_cmd = ' '.join(sys.argv)
+        #print pfsearch_cmd
 
-    #create the output file in case we dont have any matches
-    open(output_file, 'a').close()
+        #create the output file in case we don't have any matches
+        open(output_file, 'a').close()
 
-    start_time = time.time()
-
-    #get the profiles
-    profiles =  get_hamap_profile(profiles_list_filename)
-    if len(profiles) > 1:
-        #get the protein sequences
-        seqs_dict = get_sequences(fasta_file)
-
-        end_time = time.time()
-        read_file_time = end_time - start_time
         start_time = time.time()
-	
-	##print some interesting stats
-        stats_filename = fasta_file + ".stats"
-        #run the pfsearch binary
-        pfsearch_cmd_run_count = run_pfsearch_binary(arg_list, profiles,seqs_dict, stats_filename, command_index)
 
-        # end_time = time.time()
-        #
-        # pfsearch_time = end_time - start_time
-        #
-        # if pfsearch_cmd_run_count:
-        #     avg_per_cmd = int(pfsearch_time * 1000 / pfsearch_cmd_run_count )
-        # else:
-        #     avg_per_cmd = 0
-        # with open(stats_filename, 'a') as stats_file:
-        #     stats_file.write("pfsearch cmd run count = " + str(pfsearch_cmd_run_count) + "\n")
-        #     stats_file.write("read file time =  " + str(read_file_time * 1000) + " ms \n")
-        #     stats_file.write("pfsearch_time = " +  str(pfsearch_time) + "\n")
-        #     stats_file.write("pfsearch time avg = " + str(avg_per_cmd) + " ms \n")
+        #get the profiles
+        profiles =  get_hamap_profile(profiles_list_filename)
+        if len(profiles) > 1:
+            #get the protein sequences
+            seqs_dict = get_sequences(fasta_file)
 
+            end_time = time.time()
+            read_file_time = end_time - start_time
+            start_time = time.time()
+
+            stats_filename = fasta_file + ".stats"
+            #run the pfsearch binary
+            pfsearch_cmd_run_count = run_pfsearch_binary(arg_list, profiles,seqs_dict, stats_filename, command_index)
+
+    except:
+        print (sys.version)
+        sys.exit("Unexpected error: " + sys.exc_info()[0])
 
 
 
