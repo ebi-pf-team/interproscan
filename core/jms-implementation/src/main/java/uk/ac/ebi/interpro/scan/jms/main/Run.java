26e9ab5415c307a08b20d64bc08f003238e6ded1
nuka <nuka@ebi.ac.uk>
2015-12-02 15:46:22 +0000

    add userdir to the properties of PantherBinaryRun, and use if tmp path is relative

--- core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/main/Run.java
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/main/Run.java
@@ -293,6 +293,10 @@ public class Run extends AbstractI5Runner {
 
                 }
 
+                //deal with panther
+                final PantherBinaryStep stepPantherRunBinary = (PantherBinaryStep) ctx.getBean("stepPantherRunBinary");
+                stepPantherRunBinary.setUserDir(parsedCommandLine.getOptionValue(I5Option.USER_DIR.getLongOpt()).trim());
+
                 System.out.println(Utilities.getTimeNow() + " Running InterProScan v5 in " + mode + " mode...");
 
                 runnable.run();
