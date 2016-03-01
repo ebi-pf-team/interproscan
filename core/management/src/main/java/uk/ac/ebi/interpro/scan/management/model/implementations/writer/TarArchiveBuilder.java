2d76533f5f4857a7ae62d2cd7c12de47b30510f1
matthew <matthew@ebi.ac.uk>
2016-03-01 14:58:54 +0000

    IBU-5025: Add some comments

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/writer/TarArchiveBuilder.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/writer/TarArchiveBuilder.java
@@ -45,6 +45,7 @@ public class TarArchiveBuilder {
     }
 
     public void buildTarArchive() throws IOException {
+        // E.g. for "-b OUT" tarArchive = "~/Projects/github-i5/interproscan/core/jms-implementation/target/interproscan-5-dist/OUT.html.tar.gz"
         if (tarArchive != null) {
             tarArchive.createNewFile();
         }
