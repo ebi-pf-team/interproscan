cf8ebcb9acec71cb58e46b7e12334ad455fe264e
matthew <matthew@ebi.ac.uk>
2016-03-08 14:36:39 +0000

    IBU-4628: CDD code and unit test review - tweaks

--- core/io/src/test/java/uk/ac/ebi/interpro/scan/io/cdd/CDDMatchParserTest.java
+++ core/io/src/test/java/uk/ac/ebi/interpro/scan/io/cdd/CDDMatchParserTest.java
@@ -1,20 +1,21 @@
 package uk.ac.ebi.interpro.scan.io.cdd;
 
-import junit.framework.TestCase;
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import uk.ac.ebi.interpro.scan.io.match.cdd.CDDMatchParser;
-import uk.ac.ebi.interpro.scan.io.match.panther.PantherMatchParser;
 import uk.ac.ebi.interpro.scan.model.raw.CDDRawMatch;
-import uk.ac.ebi.interpro.scan.model.raw.PantherRawMatch;
 import uk.ac.ebi.interpro.scan.model.raw.RawProtein;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests createMatch method of CDDMatchParser class.
@@ -23,7 +24,7 @@ import java.util.Set;
  * @version $Id$
  * @since 1.0-SNAPSHOT
  */
-public class CDDMatchParserTest extends TestCase {
+public class CDDMatchParserTest {
 
     private static final Logger LOGGER = Logger.getLogger(CDDMatchParserTest.class.getName());
 
@@ -38,8 +39,6 @@ public class CDDMatchParserTest extends TestCase {
 
     @Test
     public void testCreateMatch() throws IOException {
-        //Test of real CDD raw match line
-        String rawMatchLine = "1";
         Resource modelFileResource = new ClassPathResource(TEST_MODEL_FILE);
         InputStream is = modelFileResource.getInputStream();
 
@@ -47,21 +46,12 @@ public class CDDMatchParserTest extends TestCase {
         LOGGER.debug("result: " + result.toString());
         System.out.println("result: " + result);
         assertEquals(2, result.size());
-//        assertNotNull("CreateMatch method returned a NULL value!", result);
-//        assertEquals("tr|Q6ZSE3|Q6ZSE3_HUMAN", result.getSequenceIdentifier());
-//        assertEquals("PTHR10024:SF2", result.getModelId());
-//        assertEquals("GB DEF: HYPOTHETICAL PROTEIN FLJ45597", result.getFamilyName());
-//        assertEquals(new Double("2.3e-141"), result.getEvalue());
-//        assertEquals(new Double("480.5"), result.getScore());
-//        assertEquals(1, result.getLocationStart());
-//        assertEquals(341, result.getLocationEnd());
-//        //location start, end is missing
-//        rawMatchLine = "tr|Q6ZSE3|Q6ZSE3_HUMAN\tPTHR10024:SF2\tGB DEF: HYPOTHETICAL PROTEIN FLJ45597\t2.3e-141\t480.5";
-//        assertNull("Result of createMatch method should be NULL!", instance.createMatch(rawMatchLine));
-//        //
-//        assertNull("Result of createMatch method should be NULL!", instance.createMatch(null));
-//        //
-//        assertNull("Result of createMatch method should be NULL!", instance.createMatch(""));
-
+        for (final RawProtein<CDDRawMatch> rawProtein : result) {
+            final Collection<CDDRawMatch> rawMatches = rawProtein.getMatches();
+            assertEquals(1, rawMatches.size());
+            final CDDRawMatch rawMatch = rawMatches.iterator().next();
+            final String modelId = rawMatch.getModelId();
+            assertTrue(modelId.equals("cd07765") || modelId.equals("cd14735"));
+        }
     }
 }
