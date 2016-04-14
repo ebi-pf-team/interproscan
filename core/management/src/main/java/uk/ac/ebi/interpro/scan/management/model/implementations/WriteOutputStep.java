0c52867b917b73023acd92a4ba5932ac79d3512c
matthew <matthew@ebi.ac.uk>
2016-04-14 11:14:45 +0100

    Copied in the SVG output bug fix found in 5.18-57.0 release branch. Plus updated to new PFam website URL.

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/WriteOutputStep.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/WriteOutputStep.java
@@ -285,8 +285,12 @@ public class WriteOutputStep extends Step {
                 if (pathAvailable) {
                     try {
                         // Start creating the empty output file now, while the path is still available
-                        outputPath = Files.createFile(outputPath);
-                    } catch (IOException e) {
+                        if (outputFormat.equals(FileOutputFormat.SVG) && !archiveSVGOutput) {
+                            outputPath = Files.createDirectories(outputPath);
+                        }
+                        else {
+                            outputPath = Files.createFile(outputPath);
+                        }                    } catch (IOException e) {
                         pathAvailable = false; // Nope, that path has probably just been taken (e.g. by another copy of InterProScan writing to the same output directory)
                         if (LOGGER.isInfoEnabled()) {
                             LOGGER.info("Path " + candidateFileName.toString() + " was available for writing to, but I/O exception thrown");
