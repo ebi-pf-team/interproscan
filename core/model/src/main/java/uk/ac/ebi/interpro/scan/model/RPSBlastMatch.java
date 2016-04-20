f564f91fb29c83982f0752c2242b76616193b56e
nuka <nuka@ebi.ac.uk>
2016-04-20 10:00:03 +0100

    add the sites related classes

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/RPSBlastMatch.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/RPSBlastMatch.java
@@ -140,23 +140,16 @@ public class RPSBlastMatch extends Match<RPSBlastMatch.RPSBlastLocation> {
             return new RPSBlastLocation(this.getStart(), this.getEnd(), this.getScore(), this.getEvalue());
         }
 
-//        public static class RPSBlastSite extends Site {
-//
-//            @Column(nullable = false, name = "start")
-//            private int start;
-//
-//            @Column(nullable = false, name = "end")
-//            private int end;
-//
-//            private String residue;
-//
-//            protected RPSBlastSite() {
-//            }
-//
-//            public RPSBlastSite(String residue, int start, int end) {
-//                super(residue, start, end);
-//            }
-//        }
+        public static class RPSBlastSite extends Site {
+
+
+            protected RPSBlastSite() {
+            }
+
+            public RPSBlastSite(String residue, int start, int end) {
+                super(residue, start, end);
+            }
+        }
     }
 
 }
