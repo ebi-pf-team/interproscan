182009d259a1616955ef2c614685684839cce340
nuka <nuka@ebi.ac.uk>
2016-05-12 10:20:50 +0100

    refactor the residue annotation for cdd

---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/CDDFilteredMatch2DAO.java
@@ -0,0 +1,19 @@
+package uk.ac.ebi.interpro.scan.persistence;
+
+import uk.ac.ebi.interpro.scan.model.raw.CDDRawMatch;
+
+
+/**
+ * CDD filtered match data access object.
+ *
+ * @author Gift Nuka
+ * @version $id
+ * @since v5.16
+ */
+
+//<T extends RPSBlastRawMatch,  U extends RPSBlastMatch, R extends RawSite, S extends Site>
+
+public class CDDFilteredMatch2DAO
+    extends RPSBlastFilteredMatch2DAO<CDDRawMatch> {
+}
+
---
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/raw/RawSiteDAOImpl.java
@@ -0,0 +1,115 @@
+package uk.ac.ebi.interpro.scan.persistence.raw;
+
+import org.apache.log4j.Logger;
+import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAOImpl;
+import uk.ac.ebi.interpro.scan.model.raw.RawMatch;
+import uk.ac.ebi.interpro.scan.model.raw.RawProtein;
+import uk.ac.ebi.interpro.scan.model.raw.RawProteinSite;
+import uk.ac.ebi.interpro.scan.model.raw.RawSite;
+import uk.ac.ebi.interpro.scan.util.Utilities;
+
+import javax.persistence.Query;
+import java.util.*;
+
+/**
+ * Data access object methods for {@link RawSite}s.
+ *
+ * @author Gift Nuka
+ * @version $Id$
+ */
+public class RawSiteDAOImpl<T extends RawSite>
+        extends GenericDAOImpl<T, Long>
+        implements RawSiteDAO<T> {
+
+    private static final Logger LOGGER = Logger.getLogger(RawSiteDAOImpl.class.getName());
+
+    public RawSiteDAOImpl(Class<T> modelClass) {
+        super(modelClass);
+    }
+
+    @Transactional
+//    @Override
+    public void insertSites(Set<RawProteinSite<T>> rawSites) {
+        for (RawProteinSite<T> rawProteinSite : rawSites) {
+            LOGGER.debug("Persisting " + rawSites.size() + " raw sites.");
+            Utilities.verboseLog("Persisting " + rawSites.size() + " raw sites.");
+            insert(new HashSet<T>(rawProteinSite.getSites()));
+        }
+        entityManager.flush();
+
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public T getSitesByModel(String modelId) {
+        return readSpecific(modelId);
+    }
+
+    /**
+     * Returns sites within the given ID range.
+     *
+     * @param bottomId                 Lower bound (protein.id >= bottomId)
+     * @param topId                    Upper bound (protein.id <= topId)
+     * @param signatureDatabaseRelease Signature database release number.
+     * @return Proteins within the given ID range
+     */
+    @Transactional(readOnly = true)
+    @Override
+    public Set<T> getSitesByProteinIdRange(long bottomId, long topId,
+                                                   String signatureDatabaseRelease) {
+        // Get raw sites
+        Query query = entityManager
+                .createQuery(String.format("select p from %s p  " +
+                        "where p.numericSequenceId >= :bottom " +
+                        "and   p.numericSequenceId <= :top " +
+                        "and   p.signatureLibraryRelease = :sigLibRelease", unqualifiedModelClassName))
+                .setParameter("bottom", bottomId)
+                .setParameter("top", topId)
+                .setParameter("sigLibRelease", signatureDatabaseRelease);
+        @SuppressWarnings("unchecked") List<T> list = query.getResultList();
+        // Create raw proteins from raw sites
+        Set<T> rawSites = new HashSet<T>();
+        for (T rawSite : list) {
+            String id = rawSite.getSequenceIdentifier();
+
+            rawSites.add(rawSite);
+
+        }
+        return rawSites;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Transactional(readOnly = true)
+    @Override
+    public Set<T> getSitesByProteinsIds(Set<Long> proteinIds,
+                                               String signatureDatabaseRelease) {
+
+        Set<T> rawSites = new HashSet<T>();
+        if (proteinIds != null && proteinIds.size() >0) {
+            // Get raw matches
+            Query query = entityManager
+                    .createQuery(String.format("select p from %s p  " +
+                            "where p.numericSequenceId in(:ids)" +
+                            "and   p.signatureLibraryRelease = :sigLibRelease", unqualifiedModelClassName))
+                    .setParameter("ids", proteinIds)
+                    .setParameter("sigLibRelease", signatureDatabaseRelease);
+            @SuppressWarnings("unchecked") List<T> list = query.getResultList();
+            // Create raw proteins from raw matches
+
+            for (T rawSite : list) {
+                String id = rawSite.getSequenceIdentifier();
+
+                rawSites.add(rawSite);
+
+            }
+
+        }
+        return rawSites;
+    }
+
+
+
+}
