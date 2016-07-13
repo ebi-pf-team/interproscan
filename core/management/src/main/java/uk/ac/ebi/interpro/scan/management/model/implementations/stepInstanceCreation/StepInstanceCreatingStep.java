973e8d338df33454b197064ed0764dd5f9ae0f64
matthew <matthew@ebi.ac.uk>
2016-07-13 16:23:17 +0100

    IBU-5662: Remove unneccesary comments

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/stepInstanceCreation/StepInstanceCreatingStep.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/stepInstanceCreation/StepInstanceCreatingStep.java
@@ -10,11 +10,11 @@ import uk.ac.ebi.interpro.scan.management.model.Jobs;
  */
 public interface StepInstanceCreatingStep {
 
-    public static final String ANALYSIS_JOB_NAMES_KEY = "ANALYSIS_JOB_NAMES";
-    public static final String COMPLETION_JOB_NAME_KEY = "COMPLETION_JOB_NAME";
-    public static final String USE_MATCH_LOOKUP_SERVICE = "USE_MATCH_LOOKUP_SERVICE";
+    String ANALYSIS_JOB_NAMES_KEY = "ANALYSIS_JOB_NAMES";
+    String COMPLETION_JOB_NAME_KEY = "COMPLETION_JOB_NAME";
+    String USE_MATCH_LOOKUP_SERVICE = "USE_MATCH_LOOKUP_SERVICE";
 
-    public void setJobs(Jobs jobs);
+    void setJobs(Jobs jobs);
 
-    public void setStepInstanceDAO(StepInstanceDAO stepInstanceDAO);
+    void setStepInstanceDAO(StepInstanceDAO stepInstanceDAO);
 }
