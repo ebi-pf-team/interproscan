c51f7939bc2e422804cc7fb9e91c4ed09bd4bdad
matthew <matthew@ebi.ac.uk>
2016-08-12 11:16:37 +0100

    IBU-5628: Minor tweaks/bugfixes a part of SFLD code review

--- core/io/src/test/java/uk/ac/ebi/interpro/scan/io/match/hmmer/hmmer3/SFLDHmmer3MatchParserTest.java
+++ core/io/src/test/java/uk/ac/ebi/interpro/scan/io/match/hmmer/hmmer3/SFLDHmmer3MatchParserTest.java
@@ -17,6 +17,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -39,10 +40,6 @@ public class SFLDHmmer3MatchParserTest {
     @Resource
     private org.springframework.core.io.Resource sfldFile;
 
-    // Tests two matches at same location (see IBU-1133)
-    @Resource
-    private org.springframework.core.io.Resource highestScoringDomainFile;
-
     @Test
     public void testSFLDParser() throws IOException {
 
@@ -59,50 +56,10 @@ public class SFLDHmmer3MatchParserTest {
             final SFLDHmmer3RawMatch rawMatch = rawMatches.iterator().next();
             final String modelId = rawMatch.getModelId();
             assertTrue(rawMatches.size() > 0);
+            assertNotNull(modelId);
         }
         assertEquals(16, matchCount);
 
-    }
-
-    /*
-     unit test for site parsing
-     */
-    @Test
-    public void testSFLDMatchSite() throws IOException {
-
-        InputStream is = sfldFile.getInputStream();
-
-        //use matchdata that has sites
-        MatchSiteData matchSiteData = sfldParser.parse(is);
-
-        Set<RawProtein<SFLDHmmer3RawMatch>> proteins = sfldParser.parse(is);
-        LOGGER.debug("result: " + proteins.toString());
-        System.out.println("result: " + proteins);
-        assertEquals(5, proteins.size());
-        int matchCount = 0;
-        for (final RawProtein<SFLDHmmer3RawMatch> rawProtein : proteins) {
-            final Collection<SFLDHmmer3RawMatch> rawMatches = rawProtein.getMatches();
-            matchCount += rawMatches.size();
-            final SFLDHmmer3RawMatch rawMatch = rawMatches.iterator().next();
-            final String modelId = rawMatch.getModelId();
-            assertTrue(rawMatches.size() > 0);
-        }
-        assertEquals(16, matchCount);
-
-    }
-
-    private <T extends Hmmer3RawMatch> Set<RawProtein<T>> parse(SFLDHmmer3MatchParser<T> parser,
-                                                                InputStream is)
-            throws IOException {
-        Set<RawProtein<T>> proteins = null;
-        try {
-            proteins = parser.parse(is);
-            assertTrue("Must be at least one protein in collection", proteins.size() > 0);
-        }
-        finally {
-            is.close();
-        }
-        return proteins;
     }
 
 }
