bce2f09186608d25c536a68a90f73d8d7bafea14
matthew <matthew@ebi.ac.uk>
2016-10-26 15:22:52 +0100

    IBU-5980: Update web text files based on InterPro 60.0 data

--- core/web/src/test/java/uk/ac/ebi/interpro/scan/web/io/EntryHierarchyTest.java
+++ core/web/src/test/java/uk/ac/ebi/interpro/scan/web/io/EntryHierarchyTest.java
@@ -31,7 +31,7 @@ public class EntryHierarchyTest {
     public void testGetEntryColoursMap() {
         Map<String, Integer> entryColoursMap = entryHierarchy.getEntryColourMap();
         assertNotNull(entryColoursMap);
-        assertEquals(8978, entryColoursMap.size());
+        assertEquals(9064, entryColoursMap.size());
         assertFalse(entryColoursMap.containsKey("invalid"));
         entryColoursMap.put("IPR999999", 1);
     }
@@ -48,6 +48,6 @@ public class EntryHierarchyTest {
     public void testGetEntryHierarchyDataMap() {
         Map<String, EntryHierarchyData> data = entryHierarchy.getEntryHierarchyDataMap();
         assertNotNull(data);
-        assertEquals(8801, data.size());
+        assertEquals(8863, data.size());
     }
 }
