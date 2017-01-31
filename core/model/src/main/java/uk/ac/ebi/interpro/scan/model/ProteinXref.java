2a84b2b1ecb2c5d119c48a1e15bb781852c26f80
matthew <matthew@ebi.ac.uk>
2017-01-31 14:18:04 +0000

    IBU-6162: Tweak to protein ac JSON

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/ProteinXref.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/ProteinXref.java
@@ -17,7 +17,7 @@
 package uk.ac.ebi.interpro.scan.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonValue;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 import javax.persistence.*;
@@ -36,7 +36,6 @@ import java.io.Serializable;
 @Entity
 @XmlType(name = "ProteinXrefType")
 @Table(indexes = { @Index(columnList = "IDENTIFIER") })
-@JsonIgnoreProperties({"id", "description", "name", "databaseName"})
 public class ProteinXref extends Xref implements Serializable {
 
     @ManyToOne(optional = false)
@@ -63,6 +62,12 @@ public class ProteinXref extends Xref implements Serializable {
     public ProteinXref(String databaseName, String identifier, String name, String description) {
         super(databaseName, identifier, name);
         this.description = description;
+    }
+
+    @Override
+    @JsonValue
+    public String getIdentifier() {
+        return super.getIdentifier();
     }
 
     @XmlAttribute(name = "desc", required = false)
