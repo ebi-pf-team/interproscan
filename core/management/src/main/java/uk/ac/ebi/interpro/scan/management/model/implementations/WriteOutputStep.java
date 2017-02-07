1e72d7e7f8cf23137cdc36fbe48c8f2bc80ef0c2
Gift Nuka <nuka@ebi.ac.uk>
2017-02-07 14:05:49 +0000

    refactor the tsv-pro output format, remove unwanted fields

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/WriteOutputStep.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/WriteOutputStep.java
@@ -379,13 +379,18 @@ public class WriteOutputStep extends Step {
         //write the site tsv production output
         //only for CDD and SFLD
         final Map<String, String> parameters = stepInstance.getParameters();
-        final boolean excludeSites = Boolean.TRUE.toString().equals(parameters.get(StepInstanceCreatingStep.EXCLUDE_SITES));
-        if (! excludeSites) {
-            Path tsvProSitesPath = Paths.get(path.toString() + ".sites");
-            Utilities.verboseLog("tsv site path: " + tsvProSitesPath.getFileName().toString());
-            try (ProteinSiteMatchesTSVResultWriter tsvSitesWriter = new ProteinSiteMatchesTSVResultWriter(tsvProSitesPath)) {
+        String analysisJobNames = parameters.get(StepInstanceCreatingStep.ANALYSIS_JOB_NAMES_KEY);
+        if (analysisJobNames == null ||
+                analysisJobNames.toLowerCase().contains("cdd") ||
+                analysisJobNames.toLowerCase().contains("sfld")) {
+            final boolean excludeSites = Boolean.TRUE.toString().equals(parameters.get(StepInstanceCreatingStep.EXCLUDE_SITES));
+            if (!excludeSites) {
+                Path tsvProSitesPath = Paths.get(path.toString() + ".sites");
+                Utilities.verboseLog("tsv site path: " + tsvProSitesPath.getFileName().toString());
+                try (ProteinSiteMatchesTSVResultWriter tsvSitesWriter = new ProteinSiteMatchesTSVResultWriter(tsvProSitesPath)) {
 
-                writeProteinMatches(tsvSitesWriter, stepInstance, proteins);
+                    writeProteinMatches(tsvSitesWriter, stepInstance, proteins);
+                }
             }
         }
     }
