e0335700fe27a99695e825a7ae5fb833319de105
matthew <matthew@ebi.ac.uk>
2017-05-03 10:15:12 +0100

    IBU-6592: Update web text files for release 5.24-63.0

--- core/web/src/test/java/uk/ac/ebi/interpro/scan/web/io/EntryHierarchyTest.java
+++ core/web/src/test/java/uk/ac/ebi/interpro/scan/web/io/EntryHierarchyTest.java
@@ -31,7 +31,7 @@ public class EntryHierarchyTest {
     public void testGetEntryColoursMap() {
         Map<String, Integer> entryColoursMap = entryHierarchy.getEntryColourMap();
         assertNotNull(entryColoursMap);
-        assertEquals(9405, entryColoursMap.size());
+        assertEquals(9630, entryColoursMap.size());
         assertFalse(entryColoursMap.containsKey("invalid"));
         entryColoursMap.put("IPR999999", 1);
     }
@@ -48,6 +48,6 @@ public class EntryHierarchyTest {
     public void testGetEntryHierarchyDataMap() {
         Map<String, EntryHierarchyData> data = entryHierarchy.getEntryHierarchyDataMap();
         assertNotNull(data);
-        assertEquals(8826, data.size());
+        assertEquals(9250, data.size());
     }
 }
