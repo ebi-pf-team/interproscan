d5b1170813060e725a92f146bcebabd506037401
matthew <matthew@ebi.ac.uk>
2017-09-15 16:52:54 +0100

    IBU-7004: Update I5 version numbers

--- core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/main/Run.java
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/main/Run.java
@@ -139,7 +139,7 @@ public class Run extends AbstractI5Runner {
 
             ArrayList<String> analysesHelpInformation = new ArrayList<>();
 
-            String i5Version = "5.25-64.0";
+            String i5Version = "5.26-65.0";
             String i5BuildType = "64-Bit";
             //32bitMessage:i5BuildType = "32-Bit";
 
