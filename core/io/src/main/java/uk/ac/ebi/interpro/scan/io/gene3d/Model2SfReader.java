f4ff0509c8f5497af59bf720ad72611912f5171e
matthew <matthew@ebi.ac.uk>
2017-10-17 16:22:16 +0100

    Update installer parser for model_to_family_map.csv

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/gene3d/Model2SfReader.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/gene3d/Model2SfReader.java
@@ -75,16 +75,16 @@ public final class Model2SfReader extends AbstractModelFileParser {
                 while ((line = reader.readLine()) != null) {
                     String[] splitLine = line.split(",");
                     String model = splitLine[0];
-                    if (model != null && model.startsWith("\\") && model.endsWith("\\")) {
-                        model = model.substring(1, model.lastIndexOf("\\") - 1);
+                    if (model != null && model.startsWith("\"") && model.endsWith("\"")) {
+                        model = model.substring(1, model.lastIndexOf("\""));
                     }
                     else {
                         throw new IllegalStateException("Model in unexpected format on line: " + line + " (" + model + ")");
                     }
 
                     String signature = splitLine[1];
-                    if (signature != null && signature.startsWith("\\") && signature.endsWith("\\")) {
-                        signature = signature.substring(1, signature.lastIndexOf("\\") - 1);
+                    if (signature != null && signature.startsWith("\"") && signature.endsWith("\"")) {
+                        signature = signature.substring(1, signature.lastIndexOf("\""));
                     }
                     else {
                         throw new IllegalStateException("Signature in unexpected format on line: " + line + "  (" + signature + ")");
