53129d3cc00d0c5fd58b9281df7f0c431bb11cab
matthew <matthew@ebi.ac.uk>
2018-01-15 12:32:01 +0000

    IBU-7480: Berkeley tweaks

--- core/precalcmatches/berkeley/src/main/java/uk/ac/ebi/interpro/scan/precalc/berkeley/conversion/toi5/fromonion/Hmmer2BerkeleyMatchConverter.java
+++ core/precalcmatches/berkeley/src/main/java/uk/ac/ebi/interpro/scan/precalc/berkeley/conversion/toi5/fromonion/Hmmer2BerkeleyMatchConverter.java
@@ -2,12 +2,14 @@ package uk.ac.ebi.interpro.scan.precalc.berkeley.conversion.toi5.fromonion;
 
 import uk.ac.ebi.interpro.scan.model.HmmBounds;
 import uk.ac.ebi.interpro.scan.model.Hmmer2Match;
+import uk.ac.ebi.interpro.scan.model.Model;
 import uk.ac.ebi.interpro.scan.model.Signature;
 import uk.ac.ebi.interpro.scan.precalc.berkeley.conversion.toi5.BerkeleyMatchConverter;
 import uk.ac.ebi.interpro.scan.precalc.berkeley.model.BerkeleyLocation;
 import uk.ac.ebi.interpro.scan.precalc.berkeley.model.BerkeleyMatch;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -32,8 +34,6 @@ public class Hmmer2BerkeleyMatchConverter extends BerkeleyMatchConverter<Hmmer2M
                 bounds = HmmBounds.parseSymbol(location.getHmmBounds());
             }
 
-            Integer hmmLength = signature.getModels().get(0).getLength();//TODO hmmLength
-
             locations.add(new Hmmer2Match.Hmmer2Location(
                     valueOrZero(location.getStart()),
                     valueOrZero(location.getEnd()),
@@ -41,7 +41,7 @@ public class Hmmer2BerkeleyMatchConverter extends BerkeleyMatchConverter<Hmmer2M
                     valueOrZero(location.geteValue()),
                     valueOrZero(location.getHmmStart()),
                     valueOrZero(location.getHmmEnd()),
-                    valueOrZero(hmmLength),
+                    valueOrZero(location.getHmmLength()),
                     bounds
             ));
         }
