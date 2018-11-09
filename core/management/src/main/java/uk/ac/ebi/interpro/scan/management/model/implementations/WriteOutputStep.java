d3b3a3becf230aa5374640a5774014709d4efa76
matthew <matthew@ebi.ac.uk>
2018-11-09 11:41:38 +0000

    IBU-8071: Bug fix

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/WriteOutputStep.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/WriteOutputStep.java
@@ -418,7 +418,7 @@ public class WriteOutputStep extends Step {
         */
 
 //        // Include accompanying TSV version file? If filename already exists it will get replaced
-//        final Map<String, String> parameters = stepInstance.getParameters();
+        final Map<String, String> parameters = stepInstance.getParameters();
         final boolean inclTSVVersion = Boolean.TRUE.toString().equals(parameters.get(INCL_TSV_VERSION));
         if (inclTSVVersion) {
             final String tsvVersionFilename = path.toString() + ".version";
