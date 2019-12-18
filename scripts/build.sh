c35f2b0ee7271cfa223235e5ca074ce1caabec0b
Gift Nuka <nuka@ebi.ac.uk>
2019-12-18 16:42:34 +0000

    more tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ mvn -q clean package
 cd target/interproscan-5-dist 
 cd $TRAVIS_BUILD_DIR/core/jms-implementation/target/interproscan-5-dist
 df -h  
-./interproscan.sh -i test_proteins.fasta -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d
+./interproscan.sh -i test_proteins.fasta -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
+./interproscan.sh -i test_nt_seqs.fasta -t n -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 
 
