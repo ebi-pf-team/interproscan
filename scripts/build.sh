e64378fcd6593d911b92f30bdccde8bcbca45751
Gift Nuka <nuka@ebi.ac.uk>
2020-01-03 13:55:45 +0000

    refactor further travis test

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,9 @@ wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/bin/getorf.ubuntu18.04
 mv getorf.ubuntu18.04 bin/nucleotide/getorf
 chmod +x bin/nucleotide/getorf
 ./interproscan.sh -i test_proteins.fasta -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
+./interproscan.sh -i test_proteins.fasta -f tsv -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 ./interproscan.sh -i test_nt_seqs.fasta -t n -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
+./interproscan.sh -i test_nt_seqs.fasta -t n -f tsv -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
+./interproscan.sh -i test_all_appl.fasta -f tsv -dp
 
 
