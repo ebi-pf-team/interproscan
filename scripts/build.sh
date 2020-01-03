28f534ed2a2cf25dc8a9b6fac5bf02fb8dcc88d2
Gift Nuka <nuka@ebi.ac.uk>
2020-01-03 14:44:17 +0000

    add cdd binary download from ftp

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,6 +12,10 @@ cd src/coils/ncoils/2.2.1
 make
 cd ../../../../
 cp src/coils/ncoils/2.2.1/ncoils bin/ncoils/2.2.1/
+wget https://ftp.ncbi.nlm.nih.gov/pub/mmdb/cdd/rpsbproc/RpsbProc-x64-linux.tar.gz
+tar xzf RpsbProc-x64-linux.tar.gz 
+cp RpsbProc-x64-linux/rpsbproc bin/blast/ncbi-blast-2.9.0+/rpsbproc
+chmod +x bin/blast/ncbi-blast-2.9.0+/rpsbproc
 ./interproscan.sh -i test_proteins.fasta -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 ./interproscan.sh -i test_proteins.fasta -f tsv -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 ./interproscan.sh -i test_nt_seqs.fasta -t n -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
