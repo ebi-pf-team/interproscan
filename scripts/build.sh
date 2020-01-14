0ab92aebacff706567936da81e06fc321f0535a2
Gift Nuka <nuka@ebi.ac.uk>
2020-01-14 16:08:05 +0000

    minor test changes to travis config

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,6 +20,7 @@ chmod +x bin/blast/ncbi-blast-2.9.0+/rpsbproc
 ./interproscan.sh -i test_proteins.fasta -f tsv -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 ./interproscan.sh -i test_nt_seqs.fasta -t n -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 ./interproscan.sh -i test_nt_seqs.fasta -t n -f tsv -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
+./interproscan.sh -i test_nt_seqs.fasta -t n -f tsv -exclappl cdd
 ./interproscan.sh -i test_all_appl.fasta -f tsv -dp -exclappl cdd
 
 
