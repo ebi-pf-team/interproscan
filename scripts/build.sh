de8388dc146d923c72265377123cfbafe8753633
Gift Nuka <nuka@ebi.ac.uk>
2020-02-25 16:59:27 +0000

    add cdd to the tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,12 +9,13 @@ cd src/coils/ncoils/2.2.1
 make
 cd ../../../../
 cp src/coils/ncoils/2.2.1/ncoils bin/ncoils/2.2.1/
-wget https://ftp.ncbi.nlm.nih.gov/pub/mmdb/cdd/rpsbproc/RpsbProc-x64-linux.tar.gz
-tar xzf RpsbProc-x64-linux.tar.gz 
-cp RpsbProc-x64-linux/rpsbproc bin/blast/ncbi-blast-2.9.0+/rpsbproc
+wget ftp://ftp.ebi.ac.uk/pub/databases/interpro/iprscan/5/bin/centos7/rpsbproc.zip
+unzip rpsbproc.zip 
+cp rpsbproc bin/blast/ncbi-blast-2.9.0+/rpsbproc
 chmod +x bin/blast/ncbi-blast-2.9.0+/rpsbproc
 ./interproscan.sh -i test_proteins.fasta -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 ./interproscan.sh -i test_proteins.fasta -f tsv -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 ./interproscan.sh -i test_nt_seqs.fasta -t n -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 ./interproscan.sh -i test_nt_seqs.fasta -t n -f tsv -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
 ./interproscan.sh -i test_all_appl.fasta -f tsv -dp -exclappl cdd
+./interproscan.sh -i test_all_appl.fasta -f tsv -dp -appl cdd
