f8f06eb9262ed368ae718e8fccf0a083ffbd1389
Gift Nuka <nuka@ebi.ac.uk>
2020-02-27 12:32:14 +0000

    add install libdw to travis build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,9 +13,10 @@ wget ftp://ftp.ebi.ac.uk/pub/databases/interpro/iprscan/5/bin/centos7/rpsbproc.z
 unzip rpsbproc.zip 
 cp rpsbproc bin/blast/ncbi-blast-2.9.0+/rpsbproc
 chmod +x bin/blast/ncbi-blast-2.9.0+/rpsbproc
-if [${OS_TYPE} =  'ubuntu']; then
+if which apt >/dev/null; then
   sudo apt install libdw1 libdw-dev
-else
+fi
+if which yum >/dev/null; then
   yum install libdw1 libdw-dev
 fi
 ./interproscan.sh -i test_proteins.fasta -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
