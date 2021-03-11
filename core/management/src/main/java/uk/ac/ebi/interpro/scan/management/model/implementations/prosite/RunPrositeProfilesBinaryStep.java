e47fa3c15eabd0d6a98b5e43639c39a82b322d20
gift nuka <nuka@ebi.ac.uk>
2021-03-11 18:25:46 +0000

    refactor the prosite pipeline

---
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/prosite/RunPrositeProfilesBinaryStep.java
@@ -0,0 +1,156 @@
+package uk.ac.ebi.interpro.scan.management.model.implementations.prosite;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Required;
+import uk.ac.ebi.interpro.scan.management.model.StepInstance;
+import uk.ac.ebi.interpro.scan.management.model.implementations.RunBinaryStep;
+import uk.ac.ebi.interpro.scan.util.Utilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This step defines running the Prosite Perl script.
+ *
+ * @author Phil Jones
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class RunPrositeProfilesBinaryStep extends RunBinaryStep {
+
+    private static final Logger LOGGER = LogManager.getLogger(RunPrositeProfilesBinaryStep.class.getName());
+
+    private String fullPathToPsScanPerlScript;
+
+    private String fullPathToPfscanBinary;
+    private String fullPathToPfsearchBinary;
+   private String fullPathToRunPrositeBinary;
+
+    private String fastaFileNameTemplate;
+
+    private String fullPathToConfirmatoryProfiles;
+
+    private Boolean usePfsearch = false;
+
+    private String modelFile;
+
+   private String modelsDir;
+
+    public String getFullPathToPsScanPerlScript() {
+        return fullPathToPsScanPerlScript;
+    }
+
+    @Required
+    public void setFullPathToPsScanPerlScript(String fullPathToPsScanPerlScript) {
+        this.fullPathToPsScanPerlScript = fullPathToPsScanPerlScript;
+    }
+
+    public String getFastaFileNameTemplate() {
+        return fastaFileNameTemplate;
+    }
+
+    @Required
+    public void setFastaFileNameTemplate(String fastaFilePathNameTemplate) {
+        this.fastaFileNameTemplate = fastaFilePathNameTemplate;
+    }
+
+    public String getModelFile() {
+        return modelFile;
+    }
+
+    @Required
+    public void setModelFile(String modelFile) {
+        this.modelFile = modelFile;
+    }
+
+    public void setModelsDir(String modelsDir) {
+        this.modelsDir = modelsDir;
+    }
+
+   public String getModelsDir() {
+        return modelsDir;
+   }
+
+    public String getFullPathToPfscanBinary() {
+        return fullPathToPfscanBinary;
+    }
+
+    @Required
+    public void setFullPathToPfscanBinary(String fullPathToPfscanBinary) {
+        this.fullPathToPfscanBinary = fullPathToPfscanBinary;
+    }
+
+    //pfsearch
+    public String getFullPathToPfsearchBinary() {
+        return fullPathToPfsearchBinary;
+    }
+
+    @Required
+    public void setFullPathToPfsearchBinary(String fullPathToPfsearchBinary) {
+        this.fullPathToPfsearchBinary = fullPathToPfsearchBinary;
+    }
+
+
+    public void setFullPathToRunPrositeBinary(String fullPathToRunPrositeBinary) {
+        this.fullPathToRunPrositeBinary = fullPathToRunPrositeBinary;
+    }
+   public String getFullPathToRunPrositeBinary() {
+       return fullPathToRunPrositeBinary;
+   }
+
+    public String getFullPathToConfirmatoryProfiles() {
+        return fullPathToConfirmatoryProfiles;
+    }
+
+    public void setFullPathToConfirmatoryProfiles(String fullPathToConfirmatoryProfiles) {
+        this.fullPathToConfirmatoryProfiles = fullPathToConfirmatoryProfiles;
+    }
+
+    public void setUsePfsearch(Boolean usePfsearch) {
+        this.usePfsearch = usePfsearch;
+    }
+
+    /**
+     * current command lines from Onion:
+     * <p/>
+     * HAMAP:               /ebi/sp/pro1/interpro/binaries/scripts/ps_scan.pl -d /ebi/production/interpro/data/members/hamap/180510/hamap.prf --pfscan /ebi/sp/pro1/interpro/binaries/64_bit_Linux/pfscan -l -1 -o gff
+     * Prosite Profiles:    /ebi/sp/pro1/interpro/binaries/scripts/ps_scan.pl -d /ebi/sp/pro1/interpro/data/members/prosite/20.52/prosite.dat --pfscan /ebi/sp/pro1/interpro/binaries/64_bit_Linux/pfscan -s -m -o gff
+     * Prosite Patterns:    /ebi/sp/pro1/interpro/binaries/scripts/ps_scan.pl -d /ebi/sp/pro1/interpro/data/members/prosite/20.52/prosite.dat --pfscan  /ebi/sp/pro1/interpro/binaries/64_bit_Linux/pfscan -r -b /ebi/sp/pro1/interpro/data/members/prosite/20.52/evaluator.dat -s -o ipro
+     *
+     * @param stepInstance           containing the parameters for executing.
+     * @param temporaryFileDirectory is the relative path in which files are stored.
+     * @return
+     */
+
+    @Override
+    protected List<String> createCommand(StepInstance stepInstance, String temporaryFileDirectory) {
+        final String fastaFilePathName = stepInstance.buildFullyQualifiedFilePath(temporaryFileDirectory, this.getFastaFileNameTemplate());
+        final String outputFilePathName = stepInstance.buildFullyQualifiedFilePath(temporaryFileDirectory, this.getOutputFileNameTemplate()); 
+        List<String> command = new ArrayList<String>();
+        command.add("python3");
+        command.add(this.getFullPathToRunPrositeBinary());
+
+//        if (usePfsearch && this.fullPathToPfsearchBinary != null && !this.fullPathToPfsearchBinary.isEmpty()){
+//          command.add("-w");
+//          command.add(this.getFullPathToPfsearchBinary());
+//        }else{
+//          command.add("--pfscan");
+//          command.add(this.getFullPathToPfscanBinary());
+//        }
+//        if (this.getFullPathToConfirmatoryProfiles() != null && this.getFullPathToConfirmatoryProfiles().length() > 0) {
+//            command.add("-b");
+//            command.add(this.getFullPathToConfirmatoryProfiles());
+//        }
+        
+
+        command.add(this.getModelsDir());
+        command.add(fastaFilePathName);
+        command.add(outputFilePathName);
+        command.add(this.getFullPathToPfsearchBinary());
+        command.addAll(this.getBinarySwitchesAsList());
+
+        Utilities.verboseLog(30, "command: " + command);
+        return command;
+    }
+}
