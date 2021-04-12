bc77883a66063a6df81d5dc08f73160f13b4641f
Tiago Grego <tgrego@ebi.ac.uk>
2021-04-12 14:30:27 +0100

    updated version number to 5.51-85.0

--- core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/main/Run.java
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/main/Run.java
@@ -152,7 +152,7 @@ public class Run extends AbstractI5Runner {
 
             ArrayList<String> analysesHelpInformation = new ArrayList<>();
 
-            String i5Version = "5.51RC1-84.0";
+            String i5Version = "5.51-85.0";
             String i5BuildType = "64-Bit";
             //32bitMessage:i5BuildType = "32-Bit";
 
