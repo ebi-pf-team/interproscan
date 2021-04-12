6d87e6373753950115d9abceddde055a5d424362
gift nuka <nuka@ebi.ac.uk>
2021-04-12 12:55:27 +0100

    update travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,10 @@ mvn -q clean package
 cd target/interproscan-5-dist 
 cd $TRAVIS_BUILD_DIR/core/jms-implementation/target/interproscan-5-dist
 df -h  
-#cd src/coils/ncoils/2.2.1
-#make
-#cd ../../../../
-#cp src/coils/ncoils/2.2.1/ncoils bin/ncoils/2.2.1/
+cd src/coils/ncoils/2.2.1
+make
+cd ../../../../
+cp src/coils/ncoils/2.2.1/ncoils bin/ncoils/2.2.1/
 #wget ftp://ftp.ebi.ac.uk/pub/databases/interpro/iprscan/5/bin/centos7/rpsbproc.zip
 #unzip rpsbproc.zip 
 #cp rpsbproc bin/blast/ncbi-blast-2.9.0+/rpsbproc
@@ -24,11 +24,7 @@ df -h
 #fi
 python3 -V
 echo " checking python version"
-#cp -r data/hamap/2020_01 data/hamap/2020_05
-#cp -r data/cdd/3.17 data/cdd/3.18
 python3 initial_setup.py
+./interproscan.sh -i test_all_appl.fasta -f tsv -dp -appl pfam
 ./interproscan.sh -i test_proteins.fasta -f tsv -dp -appl sfld, prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
-./interproscan.sh -i test_all_appl.fasta -f tsv -dp -exclappl cdd, hamap
-./interproscan.sh -i test_all_appl.fasta -f tsv -dp -appl hamap
-./interproscan.sh -i test_all_appl.fasta -f tsv -dp -appl cdd
 ./interproscan.sh -i test_all_appl.fasta -f tsv -dp
