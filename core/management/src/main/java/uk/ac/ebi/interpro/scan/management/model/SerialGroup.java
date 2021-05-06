1e880b6919b23f156715ff0ebe08f34544abfb5d
gift nuka <nuka@ebi.ac.uk>
2021-05-06 10:56:14 +0100

    add panther binary serial group

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/SerialGroup.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/SerialGroup.java
@@ -9,6 +9,7 @@ package uk.ac.ebi.interpro.scan.management.model;
  * serial group depends upon database locking concerns.
  *
  * @author Phil Jones
+ * @author Gift Nuka
  * @version $Id$
  * @since 1.0
  */
@@ -24,6 +25,7 @@ public enum SerialGroup {
     LOAD_PROTEINS,
     LOAD_NUCLEIC_ACID,
     MATCH_LOOKUP,
+    PANTHER_BINARY,
     PARSE_GENE3D,
     PARSE_MOBIDB,
     PARSE_PFAM_A,
