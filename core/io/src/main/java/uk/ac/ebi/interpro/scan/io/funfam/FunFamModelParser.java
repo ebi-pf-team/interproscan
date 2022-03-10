0c55c86cf017720d7e96c908a179481bf123ceb4
Matthias Blum <mat.blum@gmail.com>
2022-03-10 11:13:03 +0000

    Add G3DSA prefix to FunFam signatures

--- core/io/src/main/java/uk/ac/ebi/interpro/scan/io/funfam/FunFamModelParser.java
+++ core/io/src/main/java/uk/ac/ebi/interpro/scan/io/funfam/FunFamModelParser.java
@@ -4,8 +4,8 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import uk.ac.ebi.interpro.scan.io.AbstractModelFileParser;
 import uk.ac.ebi.interpro.scan.io.model.HmmerModelParser;
+import uk.ac.ebi.interpro.scan.model.Model;
 import uk.ac.ebi.interpro.scan.model.Signature;
-import uk.ac.ebi.interpro.scan.model.SignatureLibrary;
 import uk.ac.ebi.interpro.scan.model.SignatureLibraryRelease;
 
 import java.io.IOException;
@@ -16,10 +16,17 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.SimpleFileVisitor;
 import java.util.*;
 
+/**
+ * Reads FunFam HMM files.
+ * @author Matthias Blum
+ */
+
 public class FunFamModelParser extends AbstractModelFileParser {
+    private static String prefix = "G3DSA:";
+
     @Override
     public SignatureLibraryRelease parse() throws IOException {
-        Set<Signature> signatures = new HashSet<>();
+        SignatureLibraryRelease release = new SignatureLibraryRelease(this.getSignatureLibrary(), this.getReleaseVersionNumber());
 
         for (Resource modelFile : modelFiles) {
             Files.walkFileTree(modelFile.getFile().toPath(), new SimpleFileVisitor<>() {
@@ -27,14 +34,23 @@ public class FunFamModelParser extends AbstractModelFileParser {
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                     HmmerModelParser parser = new HmmerModelParser();
                     parser.setModelFiles(new FileSystemResource(file.toFile()));
-                    signatures.addAll(parser.parse().getSignatures());
+
+                    for (Signature s: parser.parse().getSignatures()) {
+                        Set<Model> models = new HashSet<>();
+                        for (Model m: s.getModels().values()) {
+                            models.add(new Model(m.getAccession(), m.getName(), m.getDescription(), m.getLength()));
+                        }
+
+                        Signature ns = new Signature(FunFamModelParser.prefix + s.getAccession(), s.getName(),
+                                s.getType(), s.getDescription(), s.getAbstract(), release, models);
+                        release.addSignature(ns);
+                    }
+
                     return FileVisitResult.CONTINUE;
                 }
             });
         }
 
-        SignatureLibrary library = this.getSignatureLibrary();
-        String versionNumber = this.getReleaseVersionNumber();
-        return new SignatureLibraryRelease(library, versionNumber, signatures);
+        return release;
     }
 }
