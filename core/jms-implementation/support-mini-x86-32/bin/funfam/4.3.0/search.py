ff6d3d2b53e7914eb84524733979bc02cf9733e9
Matthias Blum <mat.blum@gmail.com>
2022-04-11 13:25:41 +0100

    Refactor FunFam search

--- core/jms-implementation/support-mini-x86-32/bin/funfam/4.3.0/search.py
+++ core/jms-implementation/support-mini-x86-32/bin/funfam/4.3.0/search.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import os
@@ -14,32 +14,38 @@ Filter and search FunFams with hmmsearch.
     """)
     parser.add_argument("seqfile", 
                         help="fasta file")
-    parser.add_argument("tabfile", 
+    parser.add_argument("tabfile",
                         help="file of assigned CATH superfamilies")
-    parser.add_argument("hmmdir", 
+    parser.add_argument("hmmdir",
                         help="FunFam data directory")
-    parser.add_argument("-o", "--output", default="-", 
+    parser.add_argument("hmmsearch_output",
+                        metavar="hmmsearch-output",
+                        help="hmmsearch output file")
+    parser.add_argument("cath_hits_output", metavar="cath-hits-output",
                         help="cath-resolve-hits output file")
-    parser.add_argument("--domtblout", 
-                        help="output hmmsearch domain hits file")
-    parser.add_argument("--hmmsearch", default="hmmsearch",
+    parser.add_argument("--hmmsearch", metavar="FILE",
+                        default="hmmsearch",
                         help="location of the hmmsearch binary")
-    parser.add_argument("--hmmsearch-flags", default="",
+    parser.add_argument("--hmmsearch-flags", metavar="FLAGS",
+                        default="",
                         help="additional hmmsearch options")
-    parser.add_argument("--cath-resolve-hits", default="cath-resolve-hits",
+    parser.add_argument("--cath-resolve-hits", metavar="FILE",
+                        default="cath-resolve-hits",
                         help="location of the cath-resolve-hits binary")
-    parser.add_argument("--cath-resolve-hits-flags", default="",
+    parser.add_argument("--cath-resolve-hits-flags", metavar="FLAGS",
+                        default="",
                         help="additional cath-resolve-hits options")
-    parser.add_argument("-T", dest="tempdir", metavar="DIR",
-                        help="create temporary files in DIR",
-                        default=tempfile.gettempdir())
+    parser.add_argument("-T", metavar="DIR",
+                        dest="tempdir", 
+                        default=tempfile.gettempdir(),
+                        help="create temporary files in DIR")
     args = parser.parse_args()
 
     seqfile = args.seqfile
     tabfile = args.tabfile
     hmmdir = args.hmmdir
-    output = args.output
-    domtblout = args.domtblout
+    hmmsearch_output = args.hmmsearch_output
+    cath_hits_output = args.cath_hits_output
     hmmsearch_binary = args.hmmsearch
     hmmsearch_flags = args.hmmsearch_flags.strip('"\'').split()
     cath_resolve_binary = args.cath_resolve_hits
@@ -62,24 +68,22 @@ Filter and search FunFams with hmmsearch.
                     families[cath_family_id] = hmmfile
 
     tmpoutput = os.path.join(tempdir, "output.tmp")
-    tmpdomtblout = os.path.join(tempdir, "domtblout.tmp")
     tmpresolved = os.path.join(tempdir, "resolved.tmp")
 
     # Base command for hmmsearch
     hmmsearch_cmd = [hmmsearch_binary] + hmmsearch_flags
     hmmsearch_cmd += ["-o", tmpoutput]
-    hmmsearch_cmd += ["--domtblout", tmpdomtblout]
 
     # Base command for cath-resolve-hits
     cath_resolve_cmd = [cath_resolve_binary] + cath_resolve_flags
     cath_resolve_cmd += ["--input-format=hmmsearch_out", 
                          "--hits-text-to-file", tmpresolved]
 
-    # cath-resolve-hits output
-    fh1 = sys.stdout if output == "-" else open(output, "wt")
+    # hmmsearch output
+    fh1 = open(hmmsearch_output, "wt")
 
-    # Save the per-domain output from hmmsearch
-    fh2 = open(domtblout, "wt") if domtblout else None
+    # cath-resolve-hits output
+    fh2 = open(cath_hits_output, "wt")
 
     try:
         for i, hmmfile in enumerate(families.values()):
@@ -93,10 +97,6 @@ Filter and search FunFams with hmmsearch.
                 raise RuntimeError("error while running hmmsearch: "
                                    "{}".format(' '.join(cmd)))
 
-            if fh2:
-                with open(tmpdomtblout, "rt") as fh3:
-                    fh2.write(fh3.read())
-
             # Resolve
             cmd = cath_resolve_cmd + [tmpoutput]
             exit_code = sp.call(cmd, stderr=sp.DEVNULL, stdout=sp.DEVNULL)
@@ -105,18 +105,17 @@ Filter and search FunFams with hmmsearch.
                 raise RuntimeError("error while running cath-resolve-hits: "
                                    "{}".format(' '.join(cmd)))
 
+            with open(tmpoutput, "rt") as fh3:
+                fh1.write(fh3.read())
+
             with open(tmpresolved, "rt") as fh3:
                 for line in fh3:
                     if line[0] != '#':
                         # query-id match-id score boundaries resolved aligned-regions cond-evalue indp-evalue
-                        fh1.write(line)
+                        fh2.write(line)
     finally:
-        if fh1 is not sys.stdout:
-            fh1.close()
-
-        if fh2:
-            fh2.close()
-
+        fh1.close()
+        fh2.close()
         shutil.rmtree(tempdir)
 
 
