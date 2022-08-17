a1e1b771c7048a03ff17f34233059b538e3f538e
Matthias Blum <mat.blum@gmail.com>
2022-08-17 16:48:22 +0100

    Get path to properties file from environment variable

--- core/jms-implementation/support-mini-x86-32/interproscan.sh
+++ core/jms-implementation/support-mini-x86-32/interproscan.sh
@@ -1,25 +1,36 @@
-#!/bin/bash
-#
-# This is InterProScan v5. Welcome.
-# Edit this script to suit your installation needs.
-#
+#!/usr/bin/env bash
+
+function get_bin_directory {
+    sed -n '/bin.directory=/p' "$1" | awk -F '=' '{print $2}'
+}
 
 USER_DIR=$PWD
-
 INSTALL_DIR="${BASH_SOURCE[0]}"
+
 while [ -h "$INSTALL_DIR" ]; do
   cd "$(dirname "$INSTALL_DIR")"
   INSTALL_DIR="$(readlink "$(basename "$INSTALL_DIR")")"
 done
-cd "$(dirname "$INSTALL_DIR")"
-INSTALL_DIR="$(pwd)/"
 
-# set environment variables for getorf
-export EMBOSS_ACDROOT=bin/nucleotide
-export EMBOSS_DATA=bin/nucleotide
+cd "$(dirname "$INSTALL_DIR")"
+
+BIN_DIR=$(get_bin_directory ./interproscan.properties)
+
+if [ -n "$INTERPROSCAN_CONF" ] && [ -f "$INTERPROSCAN_CONF" ]; then
+    PROPERTIES="$INTERPROSCAN_CONF"
+    PROPERTY="-Dsystem.interproscan.properties=${PROPERTIES}"
+    BIN_DIR="$(get_bin_directory "${PROPERTIES}")"
+else
+    PROPERTIES="./interproscan.properties"
+    PROPERTY=""
+fi
+
+# Set environment variables for getorf
+export EMBOSS_ACDROOT="$BIN_DIR"/nucleotide
+export EMBOSS_DATA="$BIN_DIR"/nucleotide
+
 
 # Check Java is installed
-
 JAVA=$(type -p java)
 if [[ "$JAVA" == "" ]]; then
     printf 'Java not found. Please install Java 11 and place it on your path,\n'
@@ -28,8 +39,6 @@ if [[ "$JAVA" == "" ]]; then
 fi
 
 # Check Java version is supported
-
-#JAVA_VERSION=$("$JAVA" -Xms32M -Xmx32M -version 2>&1 | head -n 1 | awk -F '"' '{print $2}' )
 JAVA_VERSION=$("$JAVA" -Xms32M -Xmx32M -version 2>&1 | sed -n '/version/p' | awk -F '"' '{print $2}' )
 JAVA_MAJOR_VERSION_FULL="$( cut -d ';' -f 1 <<< "$JAVA_VERSION" )"
 JAVA_MAJOR_VERSION="${JAVA_MAJOR_VERSION_FULL%%.*}"
@@ -41,11 +50,12 @@ then
     exit 1
 fi
 
+exit 0
+
 python3 initial_setup.py
 
 "$JAVA" \
  -XX:ParallelGCThreads=8 \
- -Xms2028M -Xmx9216M \
- -jar  interproscan-5.jar $@ -u $USER_DIR
-
-#end
+ -Xms2028M -Xmx4000M \
+ "$PROPERTY"
+ -jar interproscan-5.jar $@ -u $USER_DIR
