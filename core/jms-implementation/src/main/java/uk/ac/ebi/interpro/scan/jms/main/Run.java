c87c3cd2e0341aebcd3925afd52a8b5aad11e24e
Matthias Blum <mat.blum@gmail.com>
2024-01-14 16:27:24 +0000

    Update version number

--- core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/main/Run.java
+++ core/jms-implementation/src/main/java/uk/ac/ebi/interpro/scan/jms/main/Run.java
@@ -141,7 +141,7 @@ public class Run extends AbstractI5Runner {
 
             ArrayList<String> analysesHelpInformation = new ArrayList<>();
 
-            String i5Version = "5.65-97.0";
+            String i5Version = "5.66-98.0";
             String i5BuildType = "64-Bit";
 
             if (parsedCommandLine.hasOption(I5Option.VERSION.getLongOpt())) {
