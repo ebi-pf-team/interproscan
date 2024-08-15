11a74baae2bd53d5e22792add1e1e6e99874425a
Matthias Blum <mat.blum@gmail.com>
2024-08-15 15:13:50 +0100

    Rename class

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/mobidb/ParseMobiDBOutputStep.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/mobidb/ParseMobiDBOutputStep.java
@@ -1,7 +1,6 @@
 package uk.ac.ebi.interpro.scan.management.model.implementations.mobidb;
 
 import uk.ac.ebi.interpro.scan.management.model.implementations.ParseAndPersistStep;
-import uk.ac.ebi.interpro.scan.management.model.implementations.ParseStep;
 import uk.ac.ebi.interpro.scan.model.MobiDBMatch;
 import uk.ac.ebi.interpro.scan.model.raw.MobiDBRawMatch;
 
@@ -11,6 +10,6 @@ import uk.ac.ebi.interpro.scan.model.raw.MobiDBRawMatch;
  * @since 1.0
  */
 
-public class ParseMobiDBOutputStepV2 extends ParseAndPersistStep <MobiDBRawMatch, MobiDBMatch> {
+public class ParseMobiDBOutputStep extends ParseAndPersistStep <MobiDBRawMatch, MobiDBMatch> {
 
 }
