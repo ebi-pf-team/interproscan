08d6830e7464b16343f66c5bd47a5cc867d07305
Matthias Blum <mat.blum@gmail.com>
2024-09-02 19:10:08 +0100

    Refactor Berkley DB sites creation

--- core/precalcmatches/berkeley/src/main/java/uk/ac/ebi/interpro/scan/precalc/berkeley/model/SimpleLookupSite.java
+++ core/precalcmatches/berkeley/src/main/java/uk/ac/ebi/interpro/scan/precalc/berkeley/model/SimpleLookupSite.java
@@ -3,19 +3,19 @@ package uk.ac.ebi.interpro.scan.precalc.berkeley.model;
 import uk.ac.ebi.interpro.scan.util.Utilities;
 
 public class SimpleLookupSite {
-    //These indices go hand by hand with the 'lookup_tmp_tab' table
+    //These indices go hand by hand with the 'LOOKUP_SITE' table
 
-    public static final int COL_IDX_MD5 = 1;
-    public static final int COL_IDX_SIG_LIB_NAME = 2;
-    public static final int COL_IDX_SIG_LIB_RELEASE = 3;
-    public static final int COL_IDX_SIG_ACCESSION = 4;
-    public static final int COL_IDX_LOC_START = 5;
-    public static final int COL_IDX_LOC_END = 6;
-    public static final int COL_IDX_NUM_SITES = 7;
-    public static final int COL_IDX_RESIDUE = 8;
-    public static final int COL_IDX_RESIDUE_START = 9;
-    public static final int COL_IDX_RESIDUE_END = 10;
-    public static final int COL_IDX_DESCRIPTION = 11;
+    public static final int COL_IDX_MD5 = 0;
+    public static final int COL_IDX_SIG_LIB_NAME = 1;
+    public static final int COL_IDX_SIG_LIB_RELEASE = 2;
+    public static final int COL_IDX_SIG_ACCESSION = 3;
+    public static final int COL_IDX_LOC_START = 4;
+    public static final int COL_IDX_LOC_END = 5;
+    public static final int COL_IDX_NUM_SITES = 6;
+    public static final int COL_IDX_RESIDUE = 7;
+    public static final int COL_IDX_RESIDUE_START = 8;
+    public static final int COL_IDX_RESIDUE_END = 9;
+    public static final int COL_IDX_DESCRIPTION = 10;
 
 
     String proteinMD5;
@@ -34,7 +34,7 @@ public class SimpleLookupSite {
         //SFLD,4,SFLDS00029,5,347,3,C,105,105
         String [] lookupMatchTokens =  lookupMatch.split(",");
 
-        int columnOffSet = 2; // -1 as zero indexed and -1 as no proteinMD5 in sequence hit tokens
+        int columnOffSet = 1; // -1 as no proteinMD5 in sequence hit tokens
         this.proteinMD5 = proteinMD5;
         signatureLibraryName = lookupMatchTokens[COL_IDX_SIG_LIB_NAME - columnOffSet];
         sigLibRelease = lookupMatchTokens[COL_IDX_SIG_LIB_RELEASE - columnOffSet];
