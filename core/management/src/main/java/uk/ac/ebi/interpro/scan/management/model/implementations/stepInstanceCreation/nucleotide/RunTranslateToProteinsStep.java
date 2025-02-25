ad7d69a2d13eef3b2c92d831d3dc92faea1d305a
Matthias Blum <mat.blum@gmail.com>
2025-02-25 09:05:04 +0000

    Start cleaning getorf-related code

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/stepInstanceCreation/nucleotide/RunTranslateToProteinsStep.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/stepInstanceCreation/nucleotide/RunTranslateToProteinsStep.java
@@ -23,38 +23,14 @@ public class RunTranslateToProteinsStep extends RunBinaryStep {
 
     private String fullPathToTranslateBinary;
 
-    private String fullPathToGetorfBinary;
-
     /**
      * The path / file name for the OUTPUT FILE (protein sequence fasta file).
      */
     private String fastaFilePath;
 
-    private String mode = "translate";
-
-    /**
-     * Minimum nucleotide size of ORF to report (Any integer value). Default value is 50.
-     */
-    private String minSize;
-
-    /**
-     * Maximum nucleotide size of ORF to report (Any integer value). Default value is 1000000.
-     */
-    private String maxSize;
-
     public static final String SEQUENCE_FILE_PATH_KEY = "nucleic.seq.file.path";
 
     public static final String MIN_NUCLEOTIDE_SIZE = "minsize";
-
-    /**
-     * Path to getorf binary.
-     *
-     * @param fullPathToGetorfBinary
-     */
-    @Required
-    public void setFullPathToGetorfBinary(String fullPathToGetorfBinary) {
-        this.fullPathToGetorfBinary = fullPathToGetorfBinary;
-    }
 
     public void setFullPathToTranslateBinary(String fullPathToTranslateBinary) {
         this.fullPathToTranslateBinary = fullPathToTranslateBinary;
@@ -71,56 +47,13 @@ public class RunTranslateToProteinsStep extends RunBinaryStep {
         this.fastaFilePath = fastaFilePath;
     }
 
-    @Required
-    public void setMinSize(String minSize) {
-        this.minSize = minSize;
-    }
-
-    public void setMaxSize(String maxSize) {
-        this.maxSize = maxSize;
-    }
-
     @Override
     protected List<String> createCommand(StepInstance stepInstance, String temporaryFileDirectory) {
         final Map<String, String> parameters = stepInstance.getParameters();
         final String nucleicAcidSeqFilePath = parameters.get(SEQUENCE_FILE_PATH_KEY);
-        final String minSizeCommandLine = parameters.get(MIN_NUCLEOTIDE_SIZE);
-        if (minSizeCommandLine != null && minSizeCommandLine.length() > 0) {
-            setMinSize(minSizeCommandLine);
-        }
-        final String fastaFile = stepInstance.buildFullyQualifiedFilePath(temporaryFileDirectory, fastaFilePath);
         final List<String> command = new ArrayList<String>();
-        if (mode.equals("translate")){
-            command.add(fullPathToTranslateBinary);
-            command.add(nucleicAcidSeqFilePath);
-            //setOutputFileNameTemplate(fastaFile);
-        }else {
-            command.add(fullPathToGetorfBinary);
-            /*
-            Using the pearson format switch (undocumented feature!) means that getorf will
-            use all text up to the first space as the identifier.
-            This is essential for the short term nucletide header fix to work (IBU-2426)
-            TODO - consider removing this switch when the long-term fix is implemented
-             */
-            command.add("-sf");
-            command.add("pearson");
-            command.add("-sequence");
-            command.add(nucleicAcidSeqFilePath);
-            command.add("-outseq");
-            command.add(fastaFile);
-            if (this.minSize != null) {
-                command.add("-minsize");
-                command.add(this.minSize);
-            }
-            if (this.maxSize != null) {
-                command.add("-maxsize");
-                command.add(this.maxSize);
-            }
-            // Need to build binary switches.
-            // Need to have default minimum length (100?)
-            command.addAll(getBinarySwitchesAsList());
-        }
-
+        command.add(fullPathToTranslateBinary);
+        command.add(nucleicAcidSeqFilePath);
         Utilities.verboseLog(1100, "RunTranslateToProteinsStep: " + getCommandBuilder(command));
         return command;
     }
