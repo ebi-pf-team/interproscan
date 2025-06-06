cd07839203febf36e46dda2e78767393ef147151
Matthias Blum <mat.blum@gmail.com>
2025-06-06 21:35:13 +0100

    Add significant flag and sequence length

--- core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PirsfHmmer3RawMatch.java
+++ core/model/src/main/java/uk/ac/ebi/interpro/scan/model/raw/PirsfHmmer3RawMatch.java
@@ -1,19 +1,11 @@
 package uk.ac.ebi.interpro.scan.model.raw;
 
 import uk.ac.ebi.interpro.scan.model.SignatureLibrary;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Index;
 import javax.persistence.Table;
 
-/**
- * Specific raw match class for PIRSF.
- * Only required to ensure it gets it's own table.
- *
- * @author Matthew Fraser
- * @author Maxim Scheremetjew
- * @version $Id$
- * @since 1.0-SNAPSHOT
- */
 @Entity
 @Table(name = PirsfHmmer3RawMatch.TABLE_NAME, indexes = {
         @Index(name = "PIRSF3_RW_SEQ_IDX", columnList = RawMatch.COL_NAME_SEQUENCE_IDENTIFIER),
@@ -26,41 +18,36 @@ public class PirsfHmmer3RawMatch extends Hmmer3RawMatch {
 
     public static final String TABLE_NAME = "PIRSF_HMMER3_RAW_MATCH";
 
+    @Column(nullable = false)
+    private boolean significant;
+
+    @Column(nullable = false)
+    private int sequenceLength;
+
     protected PirsfHmmer3RawMatch() {
 
     }
 
-    /*
-       The pirsf.pl with "--outfmt i5" shall return the following in TSV format:
-       LOCATION_END
-       LOCATION_START
-       MODEL_ID
-       SEQUENCE_ID
-       EVALUE
-       HMM_BOUNDS
-       HMM_END
-       HMM_START
-       LOCATION_SCORE
-       SCORE
-       DOMAIN_BIAS
-       DOMAIN_CE_VALUE
-       DOMAIN_IE_VALUE
-       ENVELOPE_END
-       ENVELOPE_START
-       EXPECTED_ACCURACY
-       FULL_SEQUENCE_BIAS
-     */
-
     public PirsfHmmer3RawMatch(int locationEnd, int locationStart, String model, String sequenceIdentifier, double evalue,
-                               String hmmBounds, int hmmEnd, int hmmStart, double locationScore, double score,
-                               double domainBias, double domainCeValue, double domainIeValue, int envelopeEnd,
-                               int envelopeStart, double expectedAccuracy, double fullSequenceBias,
-                               SignatureLibrary signatureLibrary, String signatureLibraryRelease) {
+                               String hmmBounds, int hmmStart, int hmmEnd, double locationScore, double score,
+                               double domainBias, double domainCeValue, double domainIeValue, int envelopeStart,
+                               int envelopeEnd, double expectedAccuracy, double fullSequenceBias,
+                               SignatureLibrary signatureLibrary, String signatureLibraryRelease, boolean significant) {
         super(sequenceIdentifier, model, signatureLibrary, signatureLibraryRelease, locationStart, locationEnd,
                 evalue, score, hmmStart, hmmEnd, hmmBounds, locationScore, envelopeStart, envelopeEnd, expectedAccuracy,
                 fullSequenceBias, domainCeValue, domainIeValue, domainBias);
-
-
+        this.significant = significant;
     }
 
+    public boolean isSignificant() {
+        return significant;
+    }
+
+    public int getSequenceLength() {
+        return sequenceLength;
+    }
+
+    public void setSequenceLength(int sequenceLength) {
+        this.sequenceLength = sequenceLength;
+    }
 }
