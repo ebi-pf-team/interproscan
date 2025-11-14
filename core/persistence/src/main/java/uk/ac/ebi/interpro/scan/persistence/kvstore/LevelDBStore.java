33d314b8b4d8b6a9243be268c6b130da99d46121
Tiago Grego <tgrego@ebi.ac.uk>
2025-11-14 10:29:56 +0000

    increase writeBufferSize

--- core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/kvstore/LevelDBStore.java
+++ core/persistence/src/main/java/uk/ac/ebi/interpro/scan/persistence/kvstore/LevelDBStore.java
@@ -36,7 +36,7 @@ public class LevelDBStore extends KVDBImpl implements AutoCloseable {
         dbType = "leveldb";
         Options options = new Options();
         options.createIfMissing(true);
-        options.writeBufferSize(2 * 1024 * 1024); // reduce memory usage
+        options.writeBufferSize(8 * 1024 * 1024); // reduce memory usage
         options.blockSize(2 * 1024);              // smaller block size
         options.maxOpenFiles(100);                // limit open files
         options.compressionType(org.iq80.leveldb.CompressionType.SNAPPY);
